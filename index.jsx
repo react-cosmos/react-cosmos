8b37ff7800e697735c713c3b8d71e2eeeb1e6159
Ovidiu Chereches <skidding@gmail.com>
2014-02-05 21:10:38 +0200

    Add url separate lib

--- index.jsx
+++ index.jsx
@@ -1,17 +1,1 @@
-/** @jsx React.DOM */
-
-function getUrlParams() {
-  var str = window.location.search.substr(1),
-      params = {};
-  if (str) {
-    var pairs = str.split('&'),
-        parts;
-    for (var i = 0; i < pairs.length; i++) {
-      parts = pairs[i].split('=');
-      params[parts[0]] = parts[1];
-    }
-  }
-  return params;
-}
-
-React.renderComponent(ApplicationController(getUrlParams()), document.body);
+React.renderComponent(ApplicationController(url.getParams()), document.body);
