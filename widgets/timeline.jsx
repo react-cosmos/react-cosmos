c652a7293ddca04756fbd92a8f738305d3f5e4d6
Ovidiu Chereches <skidding@gmail.com>
2014-02-08 15:49:32 +0200

    Load widget props from data in Timelines

--- widgets/timeline.jsx
+++ widgets/timeline.jsx
@@ -11,12 +11,11 @@ fresh.widgets.Timeline = React.createClass({
   mixins: [fresh.mixins.SetIntervalMixin,
            fresh.mixins.DataManagerMixin],
   render: function() {
-    var itemWidget = fresh.getWidgetByName(this.props.item);
     return (
       <ul className="Timeline">
         {this.state.data.map(function(item, index) {
-          var props = {state: {data: item}};
-          return <li key={index}>{itemWidget(props)}</li>
+          var itemWidget = fresh.getWidgetByName(item.widget);
+          return <li key={index}>{itemWidget(item)}</li>
         })}
       </ul>
     );
