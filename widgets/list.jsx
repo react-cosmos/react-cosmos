783e2f6ba87195fa7aa6ca01eaed0225cced1452
Ovidiu Chereches <skidding@gmail.com>
2014-02-10 19:06:44 +0200

    Correctly name List widget file

--- widgets/list.jsx
+++ widgets/list.jsx
@@ -14,7 +14,7 @@ Fresh.widgets.List = React.createClass({
       <ul className="List">
         {this.state.data.map(function(item, index) {
           var itemWidget = Fresh.getWidgetByName(item.widget);
-          return <li key={index}>{itemWidget(item)}</li>
+          return <li key={index}>{itemWidget(_.clone(item))}</li>
         })}
       </ul>
     );
