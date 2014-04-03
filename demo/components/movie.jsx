c21265e581f6c594308944e319e72558e93b3f40
Ovidiu Chereches <skidding@gmail.com>
2014-04-03 20:30:52 +0300

    Make transitions between Movies seamless #14
    Show image from props before fetching movie content

--- demo/components/movie.jsx
+++ demo/components/movie.jsx
@@ -6,41 +6,47 @@ Cosmos.components.Movie = React.createClass({
            Cosmos.mixins.PersistState],
   defaultClass: 'movie full-background',
   render: function() {
-    if (_.isEmpty(this.state.data)) {
-      return (
-        <div className={this.getClassName()}>
-          <p className="loading">loading...</p>
-        </div>
-      );
-    }
-    var backgroundStyle = {
-          // We load the SD version first, since we already have it in cache
-          // from the thumbnail leading to this movie
-          backgroundImage: 'url(' + this.getUrlForBackgroundImage(780) + '),' +
-                           'url(' + this.getUrlForBackgroundImage(154) + ')'
-        },
-        similarMovies = this.getSimilarMovies();
+    var backgroundStyle = this.getBackgroundStyle();
     return (
       <div className={this.getClassName()} style={backgroundStyle}>
         <div className="full-background-content">
-          <div className="content-wrapper">
-            <h1 className="movie-title">
-              {this.state.data.title + ' '}
-              <span className="year">({this.getReleaseYear()})</span>
-            </h1>
-            <Cosmos component="MovieCredits"
-                    cast={this.state.data.credits.cast}
-                    crew={this.state.data.credits.crew} />
-            <p className="movie-plot">{this.state.data.overview}</p>
-            <p className="similar-to">similar to...</p>
-            <Cosmos component="List"
-                    itemComponent="MovieThumbnail"
-                    class="movie-list"
-                    state={{data: this.getSimilarMovies()}} />
-          </div>
+          {this.getContentDOM()}
         </div>
       </div>
     );
+  },
+  getContentDOM: function() {
+    if (_.isEmpty(this.state.data)) {
+      return null;
+    }
+    return (
+      <div className="content-wrapper">
+        <h1 className="movie-title">
+          {this.state.data.title + ' '}
+          <span className="year">({this.getReleaseYear()})</span>
+        </h1>
+        <Cosmos component="MovieCredits"
+                cast={this.state.data.credits.cast}
+                crew={this.state.data.credits.crew} />
+        <p className="movie-plot">{this.state.data.overview}</p>
+        <p className="similar-to">similar to...</p>
+        <Cosmos component="List"
+                itemComponent="MovieThumbnail"
+                class="movie-list"
+                state={{data: this.getSimilarMovies()}} />
+      </div>
+    );
+  },
+  getBackgroundStyle: function() {
+    if (!this.getPosterPath()) {
+      return {};
+    }
+    return {
+      // We load the SD version first, since we already have it in cache
+      // from the thumbnail leading to this movie
+      backgroundImage: 'url(' + this.getUrlForBackgroundImage(780) + '),' +
+                       'url(' + this.getUrlForBackgroundImage(154) + ')'
+    }
   },
   getReleaseYear: function() {
     return new Date(this.state.data.release_date).getFullYear();
