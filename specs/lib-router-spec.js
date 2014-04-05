69ff198800824b499c3a17839db3ec14e580b4fc
Ovidiu Chereches <skidding@gmail.com>
2014-04-05 04:51:51 +0300

    Remove RouterHistory and transitions from project #17

--- specs/lib-router-spec.js
+++ specs/lib-router-spec.js
@@ -12,11 +12,6 @@ describe("Cosmos.Router", function() {
     // Use window.history
     spyOn(Cosmos.Router.prototype, '_replaceHistoryState');
     spyOn(Cosmos.Router.prototype, '_pushHistoryState');
-    // Uses window.location
-    spyOn(Cosmos.Router.prototype, '_replaceInitialState');
-    // Methods using jQuery
-    spyOn(Cosmos.Router.prototype, '_resetContainer');
-    spyOn(Cosmos.Router.prototype, '_createComponentContainer');
     // Out of Router scope
     spyOn(Cosmos.url, 'isPushStateSupported').andReturn(true);
   });
@@ -63,11 +58,6 @@ describe("Cosmos.Router", function() {
         var router = new Cosmos.Router({container: '<body>'});
         expect(router.container).toEqual('<body>');
       });
-
-      it("should create a new RouterHistory instance", function() {
-        var router = new Cosmos.Router();
-        expect(router.history).toEqual(jasmine.any(Cosmos.RouterHistory));
-      });
   });
 
   describe("should render new Components", function() {
@@ -113,109 +103,6 @@ describe("Cosmos.Router", function() {
     });
   });
 
-  describe("on PopState events", function() {
-
-    beforeEach(function() {
-      spyOn(Cosmos, 'render');
-    });
-
-    it("should reset history if not in current history", function() {
-      var router = new Cosmos.Router({
-        props: {
-          component: 'List',
-          dataUrl: 'users.json'
-        }
-      });
-      router._onPopState({
-        state: {
-          component: 'User',
-          dataUrl: 'user.json'
-        }
-      });
-      expect(router.history.length).toBe(1);
-      expect(router.history[router.history.index].props).toEqual({
-        component: 'User',
-        dataUrl: 'user.json'
-      });
-    });
-
-    it("should reset history if not direct neighbor of current history entry", function() {
-      var router = new Cosmos.Router({
-        props: {
-          component: 'List',
-          dataUrl: 'users.json'
-        }
-      });
-      router.goTo('?component=User&dataUrl=user.json');
-      router.goTo('?component=Picture&dataUrl=picture.jpg');
-      router._onPopState({
-        state: {
-          component: 'List',
-          dataUrl: 'users.json'
-        }
-      });
-      expect(router.history.length).toBe(1);
-      expect(router.history[router.history.index].props).toEqual({
-        component: 'List',
-        dataUrl: 'users.json'
-      });
-    });
-
-    it("should not alter history if same as current history entry", function() {
-      var router = new Cosmos.Router({
-        props: {
-          component: 'List',
-          dataUrl: 'users.json'
-        }
-      });
-      router._onPopState({
-        state: {
-          component: 'List',
-          dataUrl: 'users.json'
-        }
-      });
-      expect(router.history.length).toBe(1);
-    });
-
-    it("should continue history if left of current history entry", function() {
-      var router = new Cosmos.Router({
-        props: {
-          component: 'List',
-          dataUrl: 'users.json'
-        }
-      });
-      router.goTo('?component=User&dataUrl=user.json');
-      router._onPopState({
-        state: {
-          component: 'List',
-          dataUrl: 'users.json'
-        }
-      });
-      expect(router.history.length).toBe(2);
-      expect(router.history.index).toBe(0);
-    });
-
-    it("should continue history if right of current history entry", function() {
-      var router = new Cosmos.Router({
-        props: {
-          component: 'List',
-          dataUrl: 'users.json'
-        }
-      });
-      router.goTo('?component=User&dataUrl=user.json');
-      // Move router history to first entry
-      router.history.index = 0;
-      router._onPopState({
-        state: {
-          component: 'User',
-          dataUrl: 'user.json'
-        }
-      });
-      expect(router.history.length).toBe(2);
-      expect(router.history.index).toBe(1);
-    });
-  });
-
   it("should cache latest snapshot of previous Component", function() {
     var ComponentSpec = {
           mixins: [Cosmos.mixins.PersistState],
@@ -239,7 +126,8 @@ describe("Cosmos.Router", function() {
     // Before routing to a new Component configuration, the previous one
     // shouldn't been update with our changes
     router.goTo('?component=User&dataUrl=user.json');
-    expect(router.history[0].props).toEqual({
+    expect(router._replaceHistoryState.callCount).toEqual(1);
+    expect(router._replaceHistoryState.mostRecentCall.args[0]).toEqual({
       component: 'List',
       dataUrl: null,
       someNumber: 555,
