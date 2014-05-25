4a291d4064de75b16c095198c0a6366c237e680e
Ovidiu Chereches <skidding@gmail.com>
2014-05-25 15:58:54 +0300

    Add Component for Tetriminos #35

---
+++ demo/components/tetrimino.jsx
@@ -0,0 +1,69 @@
+/** @jsx React.DOM */
+
+Cosmos.components.Tetrimino = React.createClass({
+  /**
+   * A tetromino is a geometric shape composed of four squares, connected
+   * orthogonally. Read more at http://en.wikipedia.org/wiki/Tetromino
+   */
+  getDefaultProps: function() {
+    return {
+      color: 'red'
+    };
+  },
+  getInitialState: function() {
+    return {
+      matrix: [
+        [0, 1, 0],
+        [1, 1, 1],
+        [0, 0, 0]
+      ]
+    };
+  },
+  mixins: [Cosmos.mixins.PeristState],
+  render: function() {
+    return (
+      <ul className="tetrimino">
+        {this.renderSquareBlocks()}
+      </ul>
+    );
+  },
+  rotate: function() {
+    var matrix = [],
+        rows = this.state.matrix.length,
+        cols = this.state.matrix[0].length,
+        row,
+        col;
+    for (row = 0; row < rows; row++) {
+      matrix[row] = [];
+      for (col = 0; col < cols; col++) {
+        matrix[row][col] = this.state.matrix[cols-1-col][row];
+      }
+    }
+    this.setState({matrix: matrix});
+  },
+  renderSquareBlocks: function() {
+    // Function inspired by http://stackoverflow.com/a/2800033/128816
+    var blocks = [],
+        rows = this.state.matrix.length,
+        cols = this.state.matrix[0].length,
+        row,
+        col;
+    for (row = 0; row < rows; row++) {
+      for (col = 0; col < cols; col++) {
+        if (this.state.matrix[row][col]) {
+          blocks.push(
+            <li className="positioned-square-block"
+                style={{
+                  top: (row * 25) + '%',
+                  left: (col * 25) + '%'
+                }}>
+              <Cosmos component="SquareBlock"
+                      color={this.props.color} />
+            </li>
+          );
+        }
+      }
+    }
+    return blocks;
+  }
+});
