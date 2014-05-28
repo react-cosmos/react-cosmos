79dc9479e51d7e28bd37fa8e7e3ed2136c6474cc
Ovidiu Chereches <skidding@gmail.com>
2014-05-28 12:06:39 +0300

    Refactor recursive snapshots to only affect initial render #35

--- mixins/persist-state.js
+++ mixins/persist-state.js
@@ -17,8 +17,7 @@ Cosmos.mixins.PersistState = {
         props = {},
         value,
         state,
-        children = {},
-        childrenProps;
+        children = {};
     for (var key in this.props) {
       value = this.props[key];
       // Ignore "system" props
@@ -33,25 +32,19 @@ Cosmos.mixins.PersistState = {
       }
       props[key] = value;
     }
-    state = _.clone(this.state);
-    if (!_.isEmpty(state)) {
-      props.state = state;
-    }
+    props.state = _.clone(this.state) || {};
     if (recursive) {
       _.each(this.refs, function(instance, ref) {
         // The child component needs to implement the PeristState mixin to be
-        // able to serialize its current state
+        // able to serialize its children recursively as well
         if (typeof(instance.generateSnapshot) == 'function') {
-          children[ref] = instance.generateSnapshot(true);
+          children[ref] = instance.generateSnapshot(true).state;
         } else {
-          childrenProps = this.getChildProps(ref);
-          if (!_.isEmpty(childrenProps)) {
-            children[ref] = childrenProps;
-          }
+          children[ref] = _.clone(instance.state) || {};
         }
-      }.bind(this));
+      });
       if (!_.isEmpty(children)) {
-        props.children = children;
+        props.state.children = children;
       }
     }
     return props;
@@ -60,34 +53,41 @@ Cosmos.mixins.PersistState = {
     return Cosmos(this.getChildProps(ref));
   },
   getChildProps: function(ref) {
-    var props = {};
-    // A tree of props can be embeded inside a single (root) Component
-    // input, trickling down recursively all the way to the tree leaves. Child
-    // props are set inside the .children prop of the parent Component, as a
-    // hash with keys corresponding to Component *refs*. These pre-defined
-    // props will be overriden with those generated at run-time.
-    if (this.props.children && this.props.children[ref]) {
-      _.extend(props, this.props.children[ref]);
-    }
     // The .children object on a Component class contains a hash of functions.
     // Keys in this hash correspond with *refs* of child Components and their
     // values are functions that return props for each of those child Components.
-    if (this.children && typeof(this.children[ref]) == 'function') {
-      props = this.children[ref].call(this, props);
+    var props = this.children[ref].call(this);
+    // A tree of states can be embeded inside a single (root) Component input,
+    // trickling down recursively all the way to the tree leaves. Child states
+    // are set inside the .children key of the parent Component's state, as a
+    // hash with keys corresponding to Component *refs*. These preset states
+    // will be overriden with those generated at run-time.
+    if (this._childSnapshots && this._childSnapshots[ref]) {
+      props.state = this._childSnapshots[ref];
+      // Child snapshots are only used for first render after which organic
+      // states are formed
+      delete this._childSnapshots[ref];
     }
     props.ref = ref;
     return props;
   },
+  loadStateSnapshot: function(state) {
+    if (state.children) {
+      this._childSnapshots = state.children;
+      delete state.children;
+    }
+    this.replaceState(state);
+  },
   componentWillMount: function() {
     // Allow passing a serialized snapshot of a state through the props
     if (this.props.state) {
-      this.replaceState(this.props.state);
+      this.loadStateSnapshot(this.props.state);
     }
   },
   componentWillReceiveProps: function(nextProps) {
     // A Component can have its configuration replaced at any time
     if (nextProps.state) {
-      this.replaceState(nextProps.state);
+      this.loadStateSnapshot(nextProps.state);
     }
   }
 };
