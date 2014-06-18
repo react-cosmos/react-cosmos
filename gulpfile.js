6d7ec05045c81a0f058e7c3c7aff0ad79376df07
Ovidiu Chereches <skidding@gmail.com>
2014-06-19 01:46:32 +0300

    Remove demo entirely
    Flatris has a new home: https://github.com/skidding/flatris

--- gulpfile.js
+++ gulpfile.js
@@ -5,42 +5,28 @@ var gulp = require('gulp'),
     gulpif = require('gulp-if'),
     react = require('gulp-react');
 
-var paths = {
-  core: [
-    'wrapper-header.js',
-    'cosmos.js',
-    'lib/**/*.js',
-    'mixins/**/*.js',
-    'components/**/*.jsx',
-    'wrapper-footer.js'],
-  demo: [
-    'demo/components/**/*.jsx',
-    'demo/flatris.js'
-  ]
-};
+var paths = [
+  'wrapper-header.js',
+  'cosmos.js',
+  'lib/**/*.js',
+  'mixins/**/*.js',
+  'components/**/*.jsx',
+  'wrapper-footer.js'
+];
 
 gulp.task('build', function() {
-  // Cosmos bundle
-  gulp.src(paths.core)
+  gulp.src(paths)
     .pipe(gulpif(/\.jsx$/, react()))
     .pipe(concat('cosmos.js'))
     .pipe(gulp.dest('build'))
     .pipe(uglify())
     .pipe(rename('cosmos.min.js'))
     .pipe(gulp.dest('build'));
-  // Demo bundle
-  gulp.src(paths.demo)
-    .pipe(react())
-    .pipe(concat('demo.js'))
-    .pipe(gulp.dest('build'))
-    .pipe(uglify())
-    .pipe(rename('demo.min.js'))
-    .pipe(gulp.dest('build'));
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function () {
-  gulp.watch(paths.core.concat(paths.demo), ['build']);
+  gulp.watch(paths.core, ['build']);
 });
 
 gulp.task('default', ['build', 'watch']);
