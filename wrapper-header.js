f21ec83d718210fe6335c56b79d4a51452d9332f
Ovidiu Cherecheș <skidding@gmail.com>
2014-09-17 14:59:06 +0300

    Fix module dependencies #61
    Previous commit was invalid

--- wrapper-header.js
+++ wrapper-header.js
@@ -5,9 +5,11 @@
   } else if (typeof exports === 'object') {
     // Node. Does not work with strict CommonJS, but only CommonJS-like
     // environments that support module.exports, like Node
-    module.exports = factory(require('react'), require('lodash'));
+    module.exports = factory(require('react'),
+                             require('lodash'),
+                             require('query'));
   } else {
     // Browser globals (root is window)
-    root.Cosmos = factory(root.React, root._);
+    root.Cosmos = factory(root.React, root._, root.$);
   }
-}(this, function(React, _) {
+}(this, function(React, _, $) {
