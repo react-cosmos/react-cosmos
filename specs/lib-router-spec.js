aa9838b326cc4fd94417be6f8f1459e0e8234a37
Ovidiu Chereches <skidding@gmail.com>
2015-01-24 17:10:43 +0200

    Redesign Router tests for new expectations #86

--- specs/lib-router-spec.js
+++ specs/lib-router-spec.js
@@ -8,7 +8,12 @@ describe("Cosmos.Router", function() {
   // in test cases as well.
   var React,
       utils,
-      Cosmos;
+      Cosmos,
+      propsFromQueryString,
+      ComponentClass,
+      componentElement,
+      componentInstance,
+      componentCallback;
 
   beforeEach(function() {
     global.window = jsdom.jsdom().createWindow('<html><body></body></html>');
@@ -19,116 +24,183 @@ describe("Cosmos.Router", function() {
     utils = React.addons.TestUtils;
     Cosmos = require('../build/cosmos.js');
 
-    // Ignore external methods
+    // Ignore native APIs
     spyOn(Cosmos.Router.prototype, '_bindPopStateEvent');
     spyOn(Cosmos.Router.prototype, '_replaceHistoryState');
     spyOn(Cosmos.Router.prototype, '_pushHistoryState');
-    spyOn(Cosmos.url, 'isPushStateSupported').and.returnValue(true);
 
     // The Cosmos.url lib is already tested in isolation
-    spyOn(Cosmos.url, 'getParams').and.returnValue({
+    propsFromQueryString = {
       component: 'List',
       data: 'users.json'
+    };
+    spyOn(Cosmos.url, 'getParams').and.returnValue(propsFromQueryString);
+    spyOn(Cosmos.url, 'isPushStateSupported').and.returnValue(true);
+
+    // We just want a valid instance to work with, the Router props won't be
+    // taken into consideration
+    spyOn(Cosmos, 'render').and.callFake(function(props, container, callback) {
+      componentCallback = callback;
+      return componentInstance;
+    });
+
+    // Clean up previous component setups
+    ComponentClass = null;
+    componentElement = null;
+    componentInstance = null;
+  });
+
+  it("should use props from URL query string", function() {
+    var router = new Cosmos.Router();
+
+    expect(Cosmos.render.calls.mostRecent().args[0])
+          .toEqual(propsFromQueryString);
+  });
+
+  it("should extend default props", function() {
+    var router = new Cosmos.Router({
+      component: 'DefaultComponent',
+      defaultProp: true
+    });
+
+    expect(Cosmos.render.calls.mostRecent().args[0]).toEqual({
+      component: 'List',
+      data: 'users.json',
+      // The props for the url didn't override this prop
+      defaultProp: true
     });
   });
 
-  describe("new instance", function() {
+  it("should default to document.body as container", function() {
+    var router = new Cosmos.Router();
 
-      beforeEach(function() {
-        spyOn(Cosmos, 'render');
-      });
-
-      it("should default to URL query string", function() {
-        var router = new Cosmos.Router();
-
-        expect(router.options.props).toEqual(Cosmos.url.getParams());
-      });
-
-      it("shouldn't use default props when props aren't empty", function() {
-        var router = new Cosmos.Router({defaultProps: {
-          component: 'DefaultComponent'
-        }});
-
-        expect(router.options.props).toEqual(Cosmos.url.getParams());
-      });
-
-      it("should default to document.body as container", function() {
-        var router = new Cosmos.Router();
-
-        expect(router.options.container).toBe(document.body);
-      });
-
-      it("should save a reference to the DOM container", function() {
-        var container = React.DOM.span();
-            router = new Cosmos.Router({container: '<span>'});
-
-        expect(router.container).toEqual('<span>');
-      });
+    expect(Cosmos.render.calls.mostRecent().args[1]).toBe(document.body);
   });
 
-  describe("should render new Components", function() {
+  it("should use props from .goTo method", function() {
+    var router = new Cosmos.Router();
+    router.goTo('?component=List&dataUrl=users.json');
 
-    beforeEach(function() {
-      spyOn(Cosmos, 'render');
+    expect(Cosmos.render.calls.count()).toEqual(2);
+    expect(Cosmos.render.calls.mostRecent().args[0]).toEqual({
+      component: 'List',
+      dataUrl: 'users.json'
     });
+  });
 
-    it("with constructor props and container", function() {
-      var router = new Cosmos.Router({
-        props: {component: 'List', dataUrl: 'users.json'},
-        container: '<span>'
-      });
-
-      expect(Cosmos.render.calls.count()).toEqual(1);
-      expect(Cosmos.render.calls.mostRecent().args[0]).toEqual({
-        component: 'List', dataUrl: 'users.json'
-      });
-      expect(Cosmos.render.calls.mostRecent().args[1]).toEqual('<span>');
+  it("should extend default props when using .goTo method", function() {
+    var router = new Cosmos.Router({
+      component: 'DefaultComponent',
+      defaultProp: true
     });
+    router.goTo('?component=List&dataUrl=users.json');
 
-    it("with props extracted from query string on .goTo", function() {
-      var router = new Cosmos.Router({});
-      router.goTo('?component=List&dataUrl=users.json');
+    expect(Cosmos.render.calls.count()).toEqual(2);
+    expect(Cosmos.render.calls.mostRecent().args[0]).toEqual({
+      component: 'List',
+      dataUrl: 'users.json',
+      // The props for the url didn't override this prop
+      defaultProp: true
+    });
+  });
 
-      expect(Cosmos.render.calls.count()).toEqual(2);
-      expect(Cosmos.render.calls.mostRecent().args[0]).toEqual({
+  it("should push component snapshot to state when using .goTo method",
+     function() {
+    ComponentClass = React.createClass({
+      mixins: [Cosmos.mixins.PersistState],
+      render: function() {
+        return React.DOM.span();
+      }
+    });
+    componentElement = React.createElement(ComponentClass, {
+      component: 'List',
+      dataUrl: 'users.json'
+    });
+    componentInstance = utils.renderIntoDocument(componentElement);
+
+    // Simulate some state addition in the component
+    spyOn(componentInstance, 'generateSnapshot').and.callFake(function() {
+      return {
         component: 'List',
-        dataUrl: 'users.json'
-      });
-    });
-
-    it("with props from event state on PopState event", function() {
-      var router = new Cosmos.Router({});
-      router._onPopState({
+        dataUrl: 'users.json',
         state: {
-          component: 'List',
-          dataUrl: 'users.json'
+          haveISeenThings: 'yes'
         }
-      });
+      };
+    });
 
-      expect(Cosmos.render.calls.count()).toEqual(2);
-      expect(Cosmos.render.calls.mostRecent().args[0]).toEqual({
+    var router = new Cosmos.Router();
+    router.goTo('?component=List&dataUrl=users.json');
+
+    // Simulate React.render callback call
+    componentCallback.call(componentInstance, 'testx');
+
+    // The snapshot shouldn't been extracted from the component two times
+    // 1. For previous component
+    // 2. For just loaded component
+    expect(componentInstance.generateSnapshot.calls.count()).toEqual(2);
+
+    // It's a bit difficult to mock the native functions so we mocked the
+    // private methods that wrap those calls
+    expect(router._pushHistoryState.calls.count()).toEqual(1);
+    expect(router._pushHistoryState.calls.mostRecent().args[0]).toEqual({
+      component: 'List',
+      dataUrl: 'users.json',
+      state: {
+        haveISeenThings: 'yes'
+      }
+    });
+  });
+
+  it("should use props from PopState event state", function() {
+    var router = new Cosmos.Router();
+    router.onPopState({
+      state: {
         component: 'List',
         dataUrl: 'users.json'
-      });
+      }
+    });
+
+    expect(Cosmos.render.calls.count()).toEqual(2);
+    expect(Cosmos.render.calls.mostRecent().args[0]).toEqual({
+      component: 'List',
+      dataUrl: 'users.json'
+    });
+  });
+
+  it("shouldn't extend default props when using PopState event", function() {
+    var router = new Cosmos.Router({
+      component: 'DefaultComponent',
+      defaultProp: true
+    });
+    router.onPopState({
+      state: {
+        component: 'List',
+        dataUrl: 'users.json'
+      }
+    });
+
+    expect(Cosmos.render.calls.count()).toEqual(2);
+    expect(Cosmos.render.calls.mostRecent().args[0]).toEqual({
+      component: 'List',
+      dataUrl: 'users.json'
     });
   });
 
   it("should cache latest snapshot of previous Component", function() {
-    var ComponentClass = React.createClass({
-          mixins: [Cosmos.mixins.PersistState],
-          render: function() {
-            return React.DOM.span();
-          }
-        }),
-        props = {component: 'List', dataUrl: 'users.json'},
-        componentElement = React.createElement(ComponentClass, props)
-        componentInstance = utils.renderIntoDocument(componentElement);
-
-    // We just want a valid instance to work with, the Router props won't be
-    // taken into consideration
-    spyOn(Cosmos, 'render').and.callFake(function(props) {
-      return componentInstance;
+    /* Note: This is not a pure unit test, it depends on the internal logic
+       of React components */
+    ComponentClass = React.createClass({
+      mixins: [Cosmos.mixins.PersistState],
+      render: function() {
+        return React.DOM.span();
+      }
     });
+    componentElement = React.createElement(ComponentClass, {
+      component: 'List',
+      dataUrl: 'users.json'
+    });
+    componentInstance = utils.renderIntoDocument(componentElement);
 
     var router = new Cosmos.Router();
     // We alter the current instance while it's bound to the current history
@@ -140,6 +212,8 @@ describe("Cosmos.Router", function() {
     // shouldn't been updated with our changes
     router.goTo('?component=User&dataUrl=user.json');
 
+    // It's a bit difficult to mock the native functions so we mocked the
+    // private methods that wrap those calls
     expect(router._replaceHistoryState.calls.count()).toEqual(1);
     expect(router._replaceHistoryState.calls.mostRecent().args[0]).toEqual({
       component: 'List',
