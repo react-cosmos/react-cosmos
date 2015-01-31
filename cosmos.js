6e68c49924a81772c7d94982548223ef3b4d6937
Ovidiu Chereches <skidding@gmail.com>
2015-01-31 11:36:29 +0200

    Improve styling with spaces #88

--- cosmos.js
+++ cosmos.js
@@ -11,22 +11,28 @@ _.extend(Cosmos, {
   start: function(defaultProps, container) {
     return new this.Router(defaultProps, container);
   },
+
   render: function(props, container, callback) {
     var componentInstance = this.createElement(props);
+
     if (container) {
       return React.render(componentInstance, container, callback);
     } else {
       return React.renderToString(componentInstance);
     }
   },
+
   createElement: function(props) {
     var ComponentClass = this.getComponentByName(props.component,
                                                  props.componentLookup);
+
     if (!_.isFunction(ComponentClass)) {
       throw new Error('Invalid component: ' + props.component);
     }
+
     return React.createElement(ComponentClass, props);
   },
+
   getComponentByName: function(name, componentLookup) {
     if (_.isFunction(componentLookup)) {
       var ComponentClass = componentLookup(name);
