5cf1f83085c71dbf7dc641725eab4d5e25b6cae1
Ovidiu Chereches <skidding@gmail.com>
2015-02-26 09:12:07 +0200

    Add Mocha tests for ComponentPlayground #101

---
+++ tests/components/component-playground/state.js
@@ -0,0 +1,55 @@
+var $ = require('jquery'),
+    Cosmos = require('../../../cosmos.js'),
+    renderComponent = require('../../helpers/render-component.js'),
+    ComponentPlayground = require('../../../components/component-playground.jsx');
+
+
+describe("ComponentPlayground component", function() {
+
+  var component,
+      $component,
+      props;
+
+  // Alow tests to extend fixture before rendering
+  function render() {
+    component = renderComponent(ComponentPlayground, props);
+    $component = $(component.getDOMNode());
+  };
+
+  beforeEach(function() {
+    // Don't render any children
+    sinon.stub(Cosmos, 'createElement');
+
+    // Allow tests to extend the base fixture
+    props = {
+      fixtures: {
+        FirstComponent: {},
+        SecondComponent: {
+          'simple-state': {}
+        }
+      }
+    };
+  });
+
+  afterEach(function() {
+    Cosmos.createElement.restore();
+  })
+
+  describe("state", function() {
+
+    it("should default to no expanded components", function() {
+      render();
+
+      expect(component.state.expandedComponents.length).to.equal(0);
+    });
+
+    it("should expand component from selected fixture", function() {
+      props.fixturePath = 'SecondComponent/simple-state';
+
+      render();
+
+      expect(component.state.expandedComponents.length).to.equal(1);
+      expect(component.state.expandedComponents[0]).to.equal('SecondComponent');
+    });
+  });
+});
---
+++ tests/helpers/render-component.js
@@ -0,0 +1,8 @@
+var React = require('react/addons'),
+    utils = React.addons.TestUtils;
+
+
+module.exports = function(componentClass, props) {
+  var componentElement = React.createElement(componentClass, props);
+  return utils.renderIntoDocument(componentElement);
+};
