c32b0f6124933233aa98e0681d2d607675b51b43
Ovidiu Chereches <skidding@gmail.com>
2015-03-30 22:27:57 +0300

    Indent component normally #114

--- examples/component-playground/components/simple-button.jsx
+++ examples/component-playground/components/simple-button.jsx
@@ -3,6 +3,8 @@ var React = require('react/addons');
 module.exports = React.createClass({
   render: function() {
     return <button type="button"
-                   disabled={this.props.disabled}>Click and let click</button>;
+                   disabled={this.props.disabled}>
+      Click and let click
+    </button>;
   }
 });
