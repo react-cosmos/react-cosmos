d8805ca5c64cfa8802d64e38fdb0f931470c8b40
Ovidiu Chereches <skidding@gmail.com>
2015-04-30 23:23:57 +0300

    Upgrade router and playground #125

--- component-playground/entry.js
+++ component-playground/entry.js
@@ -1,11 +1,11 @@
 require('./global-reset.css');
 
-var ReactMinimalRouter = require('react-minimal-router'),
+var ReactQuerystringRouter = require('react-querystring-router'),
     ComponentPlayground = require('react-component-playground'),
     getComponentFixtureTree = require('./lib/get-component-fixture-tree.js'),
     getTitleForFixture = require('./lib/get-title-for-fixture.js');
 
-module.exports = new ReactMinimalRouter.Router({
+module.exports = new ReactQuerystringRouter.Router({
   container: document.getElementById('component-playground'),
   defaultProps: {
     components: getComponentFixtureTree()
