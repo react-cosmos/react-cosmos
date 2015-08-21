f2663ef08468c1bff3fc85f19a94dedcb6a300a1
Ovidiu Cherecheș <skidding@gmail.com>
2015-08-21 14:32:35 +0300

    Namespace webpack aliases #159

--- component-playground/config.js
+++ component-playground/config.js
@@ -41,8 +41,8 @@ config.webpack = {
   ],
   resolve: {
     alias: {
-      components: resolvePath(argv.componentsPath || 'components'),
-      fixtures: resolvePath(argv.fixturesPath || 'fixtures')
+      __cosmos_components: resolvePath(argv.componentsPath || 'components'),
+      __cosmos_fixtures: resolvePath(argv.fixturesPath || 'fixtures')
     },
     extensions: ['', '.js', '.jsx']
   },
