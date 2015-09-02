a2057aad08154cea976e5749889489d7b86d7be2
George Bonner <georgebonnr@gmail.com>
2015-09-02 10:19:58 -0700

    adds similar handling for fixture files

--- component-playground/lib/get-component-fixture-tree.js
+++ component-playground/lib/get-component-fixture-tree.js
@@ -22,7 +22,7 @@ module.exports = function() {
       };
     }
 
-    fixtures[componentName].fixtures[fixtureName] = requireFixture(fixturePath);
+    fixtures[componentName].fixtures[fixtureName] = requireFixture(fixturePath).default;
   });
 
   return fixtures;
