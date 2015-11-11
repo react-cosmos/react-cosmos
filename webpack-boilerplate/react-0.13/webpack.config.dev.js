02ba3a1b83efa30e40b867ebde2a9d4e10ed0482
Ovidiu Chereches <skidding@gmail.com>
2015-11-11 22:48:34 +0200

    "build" is more appropriate when you're webpacking #169

--- webpack-boilerplate/react-0.13/webpack.config.dev.js
+++ webpack-boilerplate/react-0.13/webpack.config.dev.js
@@ -8,7 +8,7 @@ module.exports = {
     './src/index'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
     publicPath: '/static/'
   },
