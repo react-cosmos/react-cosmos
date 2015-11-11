a79ecb0320b2fb7a36aeef28f71dad0f26349b3f
Ovidiu Chereches <skidding@gmail.com>
2015-11-11 23:09:08 +0200

    Whitelist routes #9

--- webpack-boilerplate/react-0.13/devServer.js
+++ webpack-boilerplate/react-0.13/devServer.js
@@ -22,7 +22,7 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler));
 
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
