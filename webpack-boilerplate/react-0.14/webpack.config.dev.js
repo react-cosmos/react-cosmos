226a7486caf1a89cfc59915f4b55228e8da3b45d
catalinmiron <mironcatalin@gmail.com>
2016-07-30 10:19:33 +0300

    Upgrade babel and boilerplate

--- webpack-boilerplate/react-0.14/webpack.config.dev.js
+++ webpack-boilerplate/react-0.14/webpack.config.dev.js
@@ -22,17 +22,10 @@ module.exports = {
       loader: 'babel',
       include: path.join(__dirname, 'src'),
       query: {
-        plugins: ['react-transform'],
-        extra: {
-          'react-transform': {
-            transforms: [{
-              transform: 'react-transform-hmr',
-              imports: ['react'],
-              locals: ['module']
-            }, {
-              transform: 'react-transform-catch-errors',
-              imports: ['react', 'redbox-react']
-            }]
+        presets: ['es2015', 'stage-0', 'react'],
+        env: {
+          development: {
+            presets: ['react-hmre']
           }
         }
       }
