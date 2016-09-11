bddc5d86331fe88630e852ca11028bc42a57d0c8
Ovidiu Cherecheș <skidding@gmail.com>
2016-09-11 15:48:13 +0300

    Dedupe test-related rules
    cc @flaviusone @bogdanstefan @RadValentin

---
+++ test/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    mocha: true,
+  },
+  rules: {
+    // Allow require-ing inside describe cases
+    "global-require": "off",
+    // For some reason this confuses ESLint:
+    //  ({ container, component, $component } = render(fixture));
+    "no-unused-vars": "off",
+    // Allow chai assertions like:
+    //   expect(component.render).to.not.have.been.called;
+    "no-unused-expressions": "off",
+    // Allow magical module paths resolved by webpack (both rules). E.g.
+    //   require('helpers/render-component')
+    "import/no-unresolved": "off",
+    "import/no-extraneous-dependencies": "off",
+  },
+  globals: {
+    expect: true,
+    sinon: true,
+  }
+};
