7a670c4bcdc7c4f9c8dfa6bae9df60ebfbe10a92
Ovidiu Cherecheș <skidding@gmail.com>
2016-10-14 18:30:18 +0300

    Add warning message and notes for prune-react-copies script #212

--- scripts/prune-react-copies.js
+++ scripts/prune-react-copies.js
@@ -1,11 +1,15 @@
-const path = require('path');
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable prefer-arrow-callback, no-console */
+
+// Child packages should keep React in peerDeps instead of deps. Sometimes
+// deps of child packages mess this up so we must make sure to keep a single
+// React (and affiliated modules) copy for the entire repo.
+// TODO: Fix ubervu-react-split-pane deps.
+
+const glob = require('glob');
 const rimraf = require('rimraf');
 
-rimraf(path.join(__dirname, '../packages/*/node_modules/react{,-dom}'), (error) => {
-  if (error) {
-    throw error;
-  }
-  /* eslint-disable no-console */
-  console.log('React copies from packages removed successfully.');
-});
+glob.sync('./packages/*/node_modules/react{,-dom}').forEach(
+  function removeUnwantedReactCopies(reactCopyPath) {
+    rimraf.sync(reactCopyPath);
+    console.log('WARNING: Removed unwanted React copy', reactCopyPath);
+  });
