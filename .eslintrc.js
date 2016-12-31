080a5c6cb91fe9f5466a16f323650c53fb154c70
ovidiubute <ovidiu.bute@gmail.com>
2016-12-31 10:28:58 +0200

    Adapt AirBnB eslint config for react-cosmos.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,11 @@ module.exports = {
   rules: {
     // React Cosmos is designed to work with React >= 0.13 and stateless
     // functions were invented in 0.14
-    "react/prefer-stateless-function": 0
+    "react/prefer-stateless-function": 0,
+    // Dynamic require is needed in Playground
+    "import/no-dynamic-require": 0,
+    // Object and Array are forbidden by default too
+    "react/forbid-prop-types": [2, { "forbid": [ "any" ] }]
   },
   parserOptions: {
     ecmaFeatures: {
