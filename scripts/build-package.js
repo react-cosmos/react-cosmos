a6f2d1dfad5b869fd7883e650f7463464b7c11c1
ovidiubute <ovidiu.bute@gmail.com>
2017-01-03 21:49:09 +0200

    Obligatory linting.

--- scripts/build-package.js
+++ scripts/build-package.js
@@ -1,29 +1,35 @@
 const glob = require('glob');
 const path = require('path');
 const npmRun = require('npm-run');
+const yargs = require('yargs');
 
-function _runBuild(packageName) {
+/**
+ * Runs the build-babel or build-webpack npm target for the specified package.
+ * @param packageName Name of the package as listed in ./packages/
+ */
+function runBuildTask(packageName) {
   const npmTask = packageName !== 'react-component-playground' ? 'build-babel' :
       'build-webpack';
   const stdout = npmRun.execSync(`PACKAGE=${packageName} npm run ${npmTask}`, {
     cwd: __dirname,
   });
 
+  /* eslint-disable no-console */
   console.log(stdout.toString('utf-8'));
 }
 
 glob('./packages/*react-*', null, (err, files) => {
   const allPackages = files.map(f => path.basename(f));
-  const argv = require('yargs')
+  const argv = yargs
       .usage('Usage: $0 <package>')
       .help()
       .argv;
   const targetPackage = argv._[0];
 
   if (!targetPackage) {
-    allPackages.forEach(pkg => (_runBuild(pkg)));
+    allPackages.forEach(pkg => (runBuildTask(pkg)));
   } else if (allPackages.includes(targetPackage)) {
-    _runBuild(targetPackage);
+    runBuildTask(targetPackage);
   } else {
     throw new Error(`Invalid package! Can only build the following packages: ${allPackages}`);
   }
