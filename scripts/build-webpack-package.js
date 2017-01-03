a9dee31feb3a1a0d9ca65286643d4faf55ef4b97
ovidiubute <ovidiu.bute@gmail.com>
2017-01-03 20:37:01 +0200

    Add first draft of generic build script.

---
+++ scripts/build-webpack-package.js
@@ -0,0 +1,46 @@
+const glob = require('glob');
+const path = require('path');
+const { spawn } = require('child_process');
+
+function _runBuild(packageName) {
+  const npmTask = packageName !== 'react-component-playground' ? 'build-babel' :
+      'build-webpack';
+  const child = spawn('npm', ['run', npmTask], {
+    env: {
+      $PACKAGE: packageName,
+    },
+  });
+
+  child.stdout.on('data', (data) => {
+    console.log(data);
+  });
+
+  child.stderr.on('data', (data) => {
+    console.error(data);
+  });
+
+  child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+
+  child.on('error', (err) => {
+    console.error(`Failed to start child process ${err}`);
+  });
+}
+
+glob('./packages/*react-*', null, (err, files) => {
+  const allPackages = files.map(f => path.basename(f));
+  const argv = require('yargs')
+      .usage('Usage: $0 <package>')
+      .help()
+      .argv;
+  const targetPackage = argv._[0];
+
+  if (!targetPackage) {
+    allPackages.forEach(pkg => (_runBuild(pkg)));
+  } else if (allPackages.includes(targetPackage)) {
+    _runBuild(targetPackage);
+  } else {
+    throw new Error(`Invalid package! Can only build the following packages: ${allPackages}`);
+  }
+});
