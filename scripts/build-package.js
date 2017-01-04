339d0c279c6f4b7765cace4692b7b9214ece7caf
ovidiubute <ovidiu.bute@gmail.com>
2017-01-04 21:45:54 +0200

    Add watch support and use async spawn for processes.

--- scripts/build-package.js
+++ scripts/build-package.js
@@ -1,48 +1,75 @@
 const glob = require('glob');
 const path = require('path');
 const npmRun = require('npm-run');
-const yargs = require('yargs');
+const argv = require('yargs').argv;
 
 /**
  * Runs the build-babel or build-webpack npm target for the specified package.
- * @param packageName Name of the package as listed in ./packages/
- * @param npmTask Name of the npm task, defaults to build-babel
  */
-function runBuildTask(packageName, npmTask) {
-  const task = npmTask || 'build-babel';
-  const stdout = npmRun.execSync(`PACKAGE=${packageName} npm run ${task}`, {
+function runBuildTask(options) {
+  const task = options.npmTask || 'build-babel';
+  const args = ['run', task];
+  if (options.watch) {
+    args.push('--', '--watch');
+  }
+
+  const child = npmRun.spawn('npm', args, {
     cwd: __dirname,
+    env: Object.assign({}, process.env, {
+      PACKAGE: options.packageName,
+    }),
   });
 
-  /* eslint-disable no-console */
-  console.log(stdout.toString('utf-8'));
+  child.stdout.on('data', (data) => {
+    process.stdout.write(data);
+  });
+
+  child.stderr.on('data', (data) => {
+    process.stderr.write(data);
+  });
+
+  child.on('close', (code) => {
+    if (code) {
+      process.stderr.write(`${options.packageName} exited with code ${code}`);
+    }
+  });
 }
 
 /**
  * Run the build for Component Playground, since it's a webpack build it
  * requires all other components to be built first.
  * @param packageNames List of package names
+ * @param watch Whether to apply watch argument to the running task
  */
-function runBuildPlayground(packageNames) {
+function runBuildPlayground(packageNames, watch) {
   packageNames
       .filter(pkg => pkg !== 'react-component-playground')
-      .forEach(pkg => (runBuildTask(pkg)));
-  runBuildTask('react-component-playground', 'build-webpack');
+      .forEach(packageName => (
+          runBuildTask({
+            packageName,
+          })
+        ));
+  runBuildTask({
+    packageName: 'react-component-playground',
+    npmTask: 'build-webpack',
+    watch,
+  });
 }
+
+const [targetPackage] = argv._;
+const applyWatch = Boolean(argv.watch);
 
 glob('./packages/react-*', null, (err, files) => {
   const allPackagesNames = files.map(f => path.basename(f));
-  const argv = yargs
-      .usage('Usage: $0 <package>')
-      .help()
-      .argv;
-  const targetPackage = argv._[0];
 
   if (!targetPackage || targetPackage === 'react-component-playground') {
     // NOTE: The Playground needs to be built after everything else
-    runBuildPlayground(allPackagesNames);
+    runBuildPlayground(allPackagesNames, applyWatch);
   } else if (allPackagesNames.includes(targetPackage)) {
-    runBuildTask(targetPackage);
+    runBuildTask({
+      packageName: targetPackage,
+      watch: applyWatch,
+    });
   } else {
     throw new Error(`Invalid package! Can only build the following packages: ${allPackagesNames}`);
   }
