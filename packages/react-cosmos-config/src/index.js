9b53c716acc0ac18f996fa249cb4c956c2e017b7
John Collins <johnlamontcollins@gmail.com>
2017-02-05 16:04:10 -0500

    default output path

--- packages/react-cosmos-config/src/index.js
+++ packages/react-cosmos-config/src/index.js
@@ -18,7 +18,7 @@ const defaults = {
   port: 8989,
   proxies: [],
   webpackConfigPath: 'webpack.config',
-  outputPath: 'dist'
+  outputPath: 'cosmos-export'
 };
 
 export default function getCosmosConfig(configPath = 'cosmos.config') {
