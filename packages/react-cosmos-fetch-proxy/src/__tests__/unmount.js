f95ac5a7e90a7f641d5154bc13f3957cd17618e3
Ovidiu Cherecheș <skidding@gmail.com>
2017-09-05 16:08:52 +0200

    Add extra test to fetch proxy #420

--- packages/react-cosmos-fetch-proxy/src/__tests__/unmount.js
+++ packages/react-cosmos-fetch-proxy/src/__tests__/unmount.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import fetchMock from 'fetch-mock';
 import { shallow } from 'enzyme';
 import createFetchProxy from '../';
 
@@ -45,4 +46,8 @@ beforeEach(() => {
 test('unmounting reverts to original fetch', () => {
   // The original window.fetch is also a mock from jest.config.js #inceptionmock
   expect(fetch).toBe('__GLOBAL_FETCH_MOCK__');
+});
+
+test('clear alls mocks', () => {
+  expect(fetchMock.routes).toHaveLength(0);
 });
