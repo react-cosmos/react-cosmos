1bc59876536c8e250d43f5c8ba19e2a4350176d4
Max Salven <max@salven.com>
2017-09-26 12:10:42 +0800

    Move scroll-into-view fixture into Tree

---
+++ packages/react-cosmos-component-playground/src/components/Tree/__fixtures__/scroll-into-view.js
@@ -0,0 +1,49 @@
+const children = [];
+for (let i = 0; i < 100; i++) {
+  children.push({
+    name: `fixture${i}`,
+    urlParams: {
+      component: 'ComponentA',
+      fixture: `fixture${i}`
+    }
+  });
+}
+
+export default {
+  props: {
+    nodeArray: [
+      {
+        name: 'ComponentA',
+        expanded: true,
+        children
+      },
+      {
+        name: 'ComponentB',
+        expanded: true,
+        children: [
+          {
+            name: 'baz',
+            urlParams: {
+              component: 'ComponentB',
+              fixture: 'baz'
+            }
+          },
+          {
+            name: 'qux',
+            urlParams: {
+              component: 'ComponentB',
+              fixture: 'qux'
+            }
+          }
+        ]
+      }
+    ],
+    searchText: '',
+    selected: {
+      component: 'ComponentB',
+      fixture: 'baz'
+    },
+    onSelect: node => console.log('Selected node: ', node),
+    onToggle: (node, expanded) => console.log('Toggled node: ', node, expanded)
+  }
+};
