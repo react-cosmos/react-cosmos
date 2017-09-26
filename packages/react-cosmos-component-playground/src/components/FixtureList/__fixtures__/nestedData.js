694f6c81994bdf66200df552f8770e92a7aa293f
Max Salven <max@salven.com>
2017-09-26 09:53:43 +0800

    Add nested fixture to FixtureList

---
+++ packages/react-cosmos-component-playground/src/components/FixtureList/__fixtures__/nestedData.js
@@ -0,0 +1,12 @@
+export default {
+  props: {
+    fixtures: {
+      'dirA/Component1': ['fixtureA', 'fixtureB'],
+      'dirB/Component2': ['fixtureA', 'fixtureB'],
+      'dirB/Component3': ['fixtureA', 'fixtureB'],
+      'dirB/subdirA/Component4': ['fixtureA', 'fixtureB']
+    },
+    urlParams: {},
+    onUrlChange: () => {}
+  }
+};
