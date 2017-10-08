e390ddd84bb2f003a3bcbef808072f1cb14bce63
Max Salven <max@salven.com>
2017-10-08 15:59:20 +0800

    Add basic filter test and enhance filter function argument names

--- packages/react-cosmos-component-playground/src/components/FixtureList/filter.js
+++ packages/react-cosmos-component-playground/src/components/FixtureList/filter.js
@@ -9,26 +9,26 @@ const matcher = (filterText, node) => {
   return match(matchText, filterText).length > 0;
 };
 
-const findNode = (node, filter) =>
-  matcher(filter, node) || // i match
+const findNode = (node, filterText) =>
+  matcher(filterText, node) || // i match
   (node.children && // or i have decendents and one of them match
     node.children.length &&
-    Boolean(node.children.find(childNode => findNode(childNode, filter))));
+    Boolean(node.children.find(childNode => findNode(childNode, filterText))));
 
-const filterNode = (node, filter) => {
+const filterNode = (node, filterText) => {
   // If im an exact match then all my children get to stay
-  if (matcher(filter, node) || !node.children) {
+  if (matcher(filterText, node) || !node.children) {
     return { ...node, expanded: true };
   }
   // If not then only keep the ones that match or have matching descendants
   // eslint-disable-next-line no-use-before-define
-  const filteredChildren = filterNodeArray(node.children, filter);
+  const filteredChildren = filterNodeArray(node.children, filterText);
   return { ...node, expanded: true, children: filteredChildren };
 };
 
-const filterNodeArray = (nodeArray, filter) =>
+const filterNodeArray = (nodeArray, filterText) =>
   nodeArray
-    .filter(node => findNode(node, filter))
-    .map(node => filterNode(node, filter));
+    .filter(node => findNode(node, filterText))
+    .map(node => filterNode(node, filterText));
 
 export default filterNodeArray;
