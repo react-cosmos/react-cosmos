5a60c74aee6f29468355a7672a2a26a36635c91d
Ovidiu Cherecheș <skidding@gmail.com>
2017-10-16 19:07:44 +0200

    Improve custom path config test case

--- packages/react-cosmos-config/src/__tests__/custom-path/index.js
+++ packages/react-cosmos-config/src/__tests__/custom-path/index.js
@@ -3,7 +3,7 @@
 import path from 'path';
 import getCosmosConfig from '../../';
 
-jest.mock('yargs', () => ({ argv: { config: 'cozmos.config' } }));
+jest.mock('yargs', () => ({ argv: { config: 'nested/cosmos.config' } }));
 
 const mocksPath = path.join(__dirname, '__fsmocks__');
 
@@ -15,7 +15,7 @@ describe('when no config exists', () => {
   it('gets extended server defaults', () => {
     const config = getCosmosConfig();
     expect(config).toMatchObject({
-      globalImports: [path.join(mocksPath, 'some-polyfill')],
+      globalImports: [path.join(mocksPath, 'nested/some-polyfill')],
       hostname: '127.0.0.1',
       port: 9000,
       publicUrl: '/loader/',
@@ -23,20 +23,29 @@ describe('when no config exists', () => {
     });
   });
 
+  it('gets root path', () => {
+    const config = getCosmosConfig();
+    expect(config.rootPath).toBe(path.join(mocksPath, 'nested'));
+  });
+
   it('gets default proxies path', () => {
     const config = getCosmosConfig();
-    expect(config.proxiesPath).toBe(path.join(mocksPath, 'cosmos.proxies'));
+    expect(config.proxiesPath).toBe(
+      path.join(mocksPath, 'nested/cosmos.proxies')
+    );
   });
 
   it('gets default webpack config path', () => {
     const config = getCosmosConfig();
     expect(config.webpackConfigPath).toBe(
-      path.join(mocksPath, 'webpack.config')
+      path.join(mocksPath, 'nested/webpack.config')
     );
   });
 
   it('gets default output path', () => {
     const config = getCosmosConfig();
-    expect(config.outputPath).toBe(path.join(mocksPath, 'cosmos-export'));
+    expect(config.outputPath).toBe(
+      path.join(mocksPath, 'nested/cosmos-export')
+    );
   });
 });
