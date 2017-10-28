9b9a2aa82e263f11645f11433da5f7fec296bb5e
Ovidiu Cherecheș <skidding@gmail.com>
2017-10-28 16:14:18 +0200

    Move ErrorCatchProxy styles to own file

--- packages/react-cosmos-loader/src/components/ErrorCatchProxy/index.js
+++ packages/react-cosmos-loader/src/components/ErrorCatchProxy/index.js
@@ -1,6 +1,7 @@
 // @flow
 
 import React, { Component } from 'react';
+import { rootStyles, codeStyles } from './styles';
 
 import type { ProxyProps } from 'react-cosmos-shared/src/react/types';
 
@@ -8,8 +9,6 @@ type State = {
   hasError: boolean,
   errorMessage: string
 };
-
-const fontFamily = `'BlinkMacSystemFont', 'Lucida Grande', 'Segoe UI', Ubuntu, Cantarell, sans-serif`;
 
 // Returning a class creator to be consistent with the other proxies and to be
 // able to add options in the future without breaking API
@@ -31,23 +30,12 @@ export default function createErrorCatchProxy() {
 
     renderError() {
       return (
-        <div style={{ margin: 12, fontFamily }}>
+        <div style={rootStyles}>
           <div>
             <strong>Ouch, something wrong!</strong>
           </div>
           <div>
-            <pre
-              style={{
-                display: 'inline-block',
-                padding: '4px 8px',
-                background: 'rgba(255, 0, 0, 0.2)',
-                fontFamily: 'FiraCode-Light, monospace',
-                lineHeight: '1.5em',
-                whiteSpace: 'pre-wrap'
-              }}
-            >
-              {this.state.errorMessage}
-            </pre>
+            <pre style={codeStyles}>{this.state.errorMessage}</pre>
           </div>
           <div>Check console for more info.</div>
         </div>
