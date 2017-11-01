c1eba815c4d852aa308b97cbc547c2d10c6f8911
Ovidiu Cherecheș <skidding@gmail.com>
2017-11-01 20:44:49 +0100

    Use cheap-module-source-map to enable stacktraces

--- packages/react-cosmos-playground/webpack.config.js
+++ packages/react-cosmos-playground/webpack.config.js
@@ -34,6 +34,9 @@ if (env === 'production') {
 }
 
 module.exports = {
+  // Besides other advantages, cheap-module-source-map is compatible with
+  // React.componentDidCatch https://github.com/facebook/react/issues/10441
+  devtool: 'cheap-module-source-map',
   entry: src,
   output: {
     libraryTarget: 'umd',
