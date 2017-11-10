4a116d3339d7431ea0477e96065b6da992ca46d9
Max Salven <max@salven.com>
2017-11-10 12:32:41 +0800

    Test for httpProxy config option

---
+++ packages/react-cosmos/src/server/__tests__/server/http-proxy.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import startServer from '../../server';
+import httpProxyMiddleware from 'http-proxy-middleware';
+const mockRootPath = __dirname;
+
+jest.mock('react-cosmos-config', () => () => ({
+  rootPath: mockRootPath,
+  port: 9999,
+  hostname: '127.0.0.1',
+  httpProxy: { context: '/api', target: 'http://127.0.0.1:4000/api' },
+  globalImports: [],
+  componentPaths: []
+}));
+
+const getCbs = {};
+const mockGet = jest.fn((path, cb) => {
+  getCbs[path] = cb;
+});
+const mockUse = jest.fn();
+const mockListen = jest.fn();
+
+jest.mock('express', () => {
+  const mockExpress = jest.fn(() => ({
+    get: mockGet,
+    use: mockUse,
+    listen: mockListen
+  }));
+  mockExpress.static = jest.fn();
+  return mockExpress;
+});
+
+jest.mock('webpack', () => jest.fn(() => 'MOCK_WEBPACK_COMPILER'));
+
+jest.mock('webpack-dev-middleware', () => jest.fn(() => 'MOCK_DEV_MIDDLEWARE'));
+jest.mock('webpack-hot-middleware', () => jest.fn(() => 'MOCK_HOT_MIDDLEWARE'));
+
+jest.mock('http-proxy-middleware', () =>
+  jest.fn(() => 'MOCK_HTTP_PROXY_MIDDLEWARE')
+);
+
+jest.mock('../../extend-webpack-config', () =>
+  jest.fn(() => 'MOCK_WEBPACK_CONFIG')
+);
+
+beforeEach(() => {
+  jest.clearAllMocks();
+  startServer();
+});
+
+it('sends httpProxy context urls to http-proxy-middleware', () => {
+  expect(mockUse.mock.calls[0][0]).toBe('/api');
+  expect(mockUse.mock.calls[0][1]).toBe('MOCK_HTTP_PROXY_MIDDLEWARE');
+  expect(httpProxyMiddleware.mock.calls[0][0]).toBe(
+    'http://127.0.0.1:4000/api'
+  );
+});
