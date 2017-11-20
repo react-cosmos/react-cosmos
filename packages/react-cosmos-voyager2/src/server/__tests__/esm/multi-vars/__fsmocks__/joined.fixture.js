97e02096e45eef48f425c1f5e8faa9468471036e
Ovidiu Cherecheș <skidding@gmail.com>
2017-11-20 16:14:51 +0100

    Add suport for exporting fixture vars (#525)

---
+++ packages/react-cosmos-voyager2/src/server/__tests__/esm/multi-vars/__fsmocks__/joined.fixture.js
@@ -0,0 +1,17 @@
+// @flow
+
+import { Italic } from './Italic';
+
+const f1 = {
+  component: Italic,
+  props: {}
+};
+
+const f2 = {
+  component: Italic,
+  props: {}
+};
+
+const fixtures = [f1, f2];
+
+export default fixtures;
