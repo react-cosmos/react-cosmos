1e18951a050ef3d7ba121fa490b9a7afaa072456
Ovidiu Cherecheș <skidding@gmail.com>
2017-11-29 17:55:02 +0100

    Mock prop callbacks automatically in Jest env

--- packages/react-cosmos-enzyme/src/index.js
+++ packages/react-cosmos-enzyme/src/index.js
@@ -1,5 +1,6 @@
 // @flow
 
+import traverse from 'traverse';
 import { mount as mountEnzyme } from 'enzyme';
 import { importModule } from 'react-cosmos-shared';
 import { moduleExists } from 'react-cosmos-shared/lib/server';
@@ -11,6 +12,7 @@ import type { Config } from 'react-cosmos-config/src';
 import type {
   Wrapper,
   Proxy,
+  Fixture,
   Renderer,
   ContextArgs,
   ContextFunctions
@@ -34,11 +36,14 @@ type EnzymeContextFunctions = ContextFunctions & {
 };
 
 export function createContext(args: EnzymeContextArgs): EnzymeContextFunctions {
-  const { proxies = detectUserProxies(args.cosmosConfigPath) } = args;
+  const { fixture, proxies = detectUserProxies(args.cosmosConfigPath) } = args;
+  const decoratedFixture = decorateFixture(fixture);
+
   const context = createGenericContext({
     ...args,
     renderer: mountEnzyme,
-    proxies
+    proxies,
+    fixture: decoratedFixture
   });
   const { getWrapper } = context;
 
@@ -70,4 +75,41 @@ function detectUserProxies(cosmosConfigPath): Array<Proxy> {
     : [];
 
   return userProxies;
+}
+
+function decorateFixture(fixture: Fixture): Fixture {
+  if (!inJestEnv() || !fixture.props) {
+    return fixture;
+  }
+
+  return {
+    ...fixture,
+    props: traverse.map(fixture.props, addJestWrapper)
+  };
+}
+
+function inJestEnv() {
+  try {
+    jest.fn(); // eslint-disable-line no-undef
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+function addJestWrapper(fn: Function): Function {
+  // HIGHLY EXPERIMENTAL: This is likely to go away if it causes problems. But,
+  // in the meantime, as Louie would say, "Weeeee!". This makes it possible to
+  // do expect(fixture.props.*).toHaveBeenCalled in Jest without wrapping any
+  // callback with jest.fn() by had.
+  // eslint-disable-next-line no-undef
+  return isFunctionButNotClass(fn) ? jest.fn(fn) : fn;
+}
+
+function isFunctionButNotClass(fn: Function): boolean {
+  // Inspired from https://stackoverflow.com/a/32235930
+  return (
+    typeof fn === 'function' &&
+    !/^(?:class\s+|function\s+(?:_class|_default|[A-Z]))/.test(fn.toString())
+  );
 }
