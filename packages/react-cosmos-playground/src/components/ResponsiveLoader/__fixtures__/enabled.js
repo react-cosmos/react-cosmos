99b0b27447417dd9a26b93f1d7b9d5665a3958f9
Max Salven <max@salven.com>
2017-12-17 18:57:07 +0800

    Update fixture

--- packages/react-cosmos-playground/src/components/ResponsiveLoader/__fixtures__/enabled.js
+++ packages/react-cosmos-playground/src/components/ResponsiveLoader/__fixtures__/enabled.js
@@ -7,6 +7,10 @@ export default {
     src: '/mock/loader/index.html',
     showResponsiveControls: true,
     onFixtureUpdate: args => console.log(args),
-    fixture: { width: 1000, height: 800 }
+    fixture: { width: 1000, height: 800 },
+    devices: [
+      { label: 'iPhone 5', width: 320, height: 568 },
+      { label: 'iPhone 6', width: 375, height: 667 }
+    ]
   }
 };
