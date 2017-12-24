422005456b0070b14279faa3ecc6bfb0381d8230
Max Salven <max@salven.com>
2017-12-24 18:01:06 +0800

    Add fixtures to ComponentPlayground

---
+++ packages/react-cosmos-playground/src/components/ComponentPlayground/__fixtures__/selected-responsive.js
@@ -0,0 +1,30 @@
+import ComponentPlayground from '../index';
+import { routerProps, init } from './_shared';
+
+export default {
+  component: ComponentPlayground,
+
+  props: {
+    options: {
+      loaderUri: '/mock/loader/index.html',
+      projectKey: 'test',
+      responsiveDevices: [
+        { label: 'iPhone 5', width: 320, height: 568 },
+        { label: 'iPhone 6', width: 375, height: 667 }
+      ]
+    },
+    router: routerProps,
+    component: 'ComponentA',
+    fixture: 'foo',
+    responsive: true
+  },
+
+  fetch: [
+    {
+      matcher: 'end:/mock/loader/index.html',
+      response: 200
+    }
+  ],
+
+  init
+};
