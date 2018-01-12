e9cdb04d950b0160606e96fa29003318610cc8a4
xavcz <xavier.cazalot@gmail.com>
2018-01-12 15:13:23 +0100

    feat: simplify API, remove schema link (#445)

--- examples/apollo/cosmos.proxies.js
+++ examples/apollo/cosmos.proxies.js
@@ -3,18 +3,8 @@ import createApolloProxy from 'react-cosmos-apollo-proxy';
 // see more examples at:
 // https://github.com/react-cosmos/react-cosmos#react-apollo-graphql
 
-// most simple config for real data
 export default [
   createApolloProxy({
     endpoint: 'https://1jzxrj179.lp.gql.zone/graphql'
   })
 ];
-
-// most simple config for mocked data
-// import schema from './local-schema';
-
-// export default [
-//   createApolloProxy({
-//     schema
-//   })
-// ];
--- packages/react-cosmos-apollo-proxy/src/index.js
+++ packages/react-cosmos-apollo-proxy/src/index.js
@@ -2,19 +2,16 @@ import React, { Component } from 'react';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-import { SchemaLink } from 'apollo-link-schema';
 import { ApolloProvider } from 'react-apollo';
 import { proxyPropTypes } from 'react-cosmos-shared/lib/react';
 import { createFixtureLink } from './fixtureLink';
 
 const defaults = {
-  fixtureKey: 'apollo',
-  context: {},
-  rootValue: {}
+  fixtureKey: 'apollo'
 };
 
 export default function createApolloProxy(options) {
-  const { fixtureKey, endpoint, client, schema, context, rootValue } = {
+  const { fixtureKey, endpoint, client } = {
     ...defaults,
     ...options
   };
@@ -23,14 +20,13 @@ export default function createApolloProxy(options) {
     constructor(props) {
       super(props);
 
-      if (!endpoint && !client && !schema) {
+      if (!endpoint && !client) {
         throw new Error(
           `
 It looks the Apollo Proxy is not configured! 
 Give it:
 - a GraphQL endpoint to send GraphQL operations to;
 - a configured Apollo Client (maybe the one you use in your app?);
-- a local schema built with graphql-tools or a link pointing to a GraphQL endpoint.
 Read more at: https://github.com/react-cosmos/react-cosmos#react-apollo-graphql.`
         );
       }
@@ -41,13 +37,7 @@ Read more at: https://github.com/react-cosmos/react-cosmos#react-apollo-graphql.
         client ||
         new ApolloClient({
           cache: new InMemoryCache(),
-          link: endpoint
-            ? new HttpLink({ uri: endpoint })
-            : new SchemaLink({
-                schema,
-                context,
-                rootValue
-              })
+          link: new HttpLink({ uri: endpoint })
         });
 
       if (resolveWith || failWith) {
