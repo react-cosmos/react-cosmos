763ccd495533a3ff54b525bcf1f589644909187d
xavcz <xavier.cazalot@gmail.com>
2018-03-13 07:27:27 +0100

    feat: test local state example with the test api

---
+++ examples/local-state/components/Counter/Counter.test.js
@@ -0,0 +1,29 @@
+import createTestContext from 'react-cosmos-test/enzyme';
+import until from 'async-until';
+import fiveClicksFixture from './__fixtures__/five-clicks';
+
+const { mount, get, getWrapper } = createTestContext({
+  fixture: fiveClicksFixture
+});
+
+beforeEach(mount);
+
+describe('local state example', () => {
+  it('has been clicked five times', () => {
+    expect(get('state')).toEqual({ value: 5 });
+  });
+
+  it('is an awesome counter', () => {
+    expect(get('props')).toEqual(
+      expect.objectContaining({ name: 'Awesome Counter' })
+    );
+  });
+
+  it('increments the counter when the button is clicked', async () => {
+    getWrapper()
+      .find('button')
+      .simulate('click');
+
+    await until(() => get('state').value === 6);
+  });
+});
