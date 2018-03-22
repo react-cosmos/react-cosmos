5082edf772ab039ab0b56d7ebe1e596d79a1cc41
scottsd <scottsd@us.ibm.com>
2018-03-22 15:46:33 -0500

    fix(bin/cosmos*.js): check the local process.env variables before defaulting to 'development' or 'production'

--- packages/react-cosmos/bin/cosmos-export.js
+++ packages/react-cosmos/bin/cosmos-export.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
+process.env.BABEL_ENV = process.env.BABEL_ENV || 'production';
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 const startExport = require('../lib/server/export').default;
 
