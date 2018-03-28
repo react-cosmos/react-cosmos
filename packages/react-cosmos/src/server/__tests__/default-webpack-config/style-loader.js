0eff793173d8ef5bfa8970b61b2fc7f8877a3e96
Xavier Cazalot <xavier.cazalot@gmail.com>
2018-03-28 17:12:06 +0200

    fix: allow parsing css from node_modules by default #631 (#632)

--- packages/react-cosmos/src/server/__tests__/default-webpack-config/style-loader.js
+++ packages/react-cosmos/src/server/__tests__/default-webpack-config/style-loader.js
@@ -17,11 +17,21 @@ jest.mock('import-from', () => ({
   }
 }));
 
-it('includes style-loader', () => {
+it('parses personal css with style-loader', () => {
   const config = getDefaultWebpackConfig('/foo/path');
   expect(config.module.rules).toContainEqual({
     test: /\.css$/,
     loader: '/style/path',
     exclude: /node_modules/
+  });
+});
+
+it('parses 3rd party css with style-loader', () => {
+  const config = getDefaultWebpackConfig('/foo/path');
+
+  expect(config.module.rules).toContainEqual({
+    test: /\.css$/,
+    loader: '/style/path',
+    include: /node_modules/
   });
 });
