ffc9d93bcc02f363d2f2c59f4648e3e085fcfd6e
Ovidiu Cherecheș <skidding@gmail.com>
2018-04-03 22:20:50 +0700

    Export all config APIs

--- packages/react-cosmos-config/index.js
+++ packages/react-cosmos-config/index.js
@@ -1,1 +1,3 @@
 exports.getCosmosConfig = require('./lib').getCosmosConfig;
+exports.hasUserCosmosConfig = require('./lib').hasUserCosmosConfig;
+exports.generateCosmosConfig = require('./lib').generateCosmosConfig;
