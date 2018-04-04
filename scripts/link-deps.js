e0565f97c8684e17be7b6bbc154699196c995c99
Ovidiu Cherecheș <skidding@gmail.com>
2018-04-04 13:44:03 +0700

    Create script for linking package entry points to src or lib

---
+++ scripts/link-deps.js
@@ -0,0 +1,55 @@
+// @flow
+
+import { argv } from 'yargs';
+import {
+  globAsync,
+  readFileAsync,
+  writeFileAsync,
+  getNodePackages
+} from './shared';
+
+const SRC_DIR = 'src';
+const DIST_DIR = 'lib';
+
+type TargetDir = typeof SRC_DIR | typeof DIST_DIR;
+
+run();
+
+async function run() {
+  const targetDir = getTargetDir();
+  const nodePackages = await getNodePackages();
+  const entryPoints = await getPackageEntryPoints(nodePackages);
+  await Promise.all(entryPoints.map(f => linkFileRequiresToDir(f, targetDir)));
+
+  // TODO: Prettify console output
+  console.log(`DONE Linked entry points to {${targetDir}}.`);
+}
+
+async function linkFileRequiresToDir(filePath, targetDir: TargetDir) {
+  // NOTE: Use Babel transform + Prettier if future requires it.
+  // For now this is JustFine™
+  const prevContents = await readFileAsync(filePath, 'utf8');
+  const regExp = new RegExp(`require\\('./(${SRC_DIR}|${DIST_DIR})`, 'g');
+  const nextContents = prevContents.replace(regExp, `require('./${targetDir}`);
+
+  writeFileAsync(filePath, nextContents, 'utf8');
+}
+
+async function getPackageEntryPoints(packages) {
+  return globAsync(`./packages/{${packages.join(',')}}/*.js`);
+}
+
+function getTargetDir(): TargetDir {
+  const targetDir: void | ?string = argv._[0];
+
+  if (
+    typeof targetDir === 'string' &&
+    (targetDir === SRC_DIR || targetDir === DIST_DIR)
+  ) {
+    return targetDir;
+  }
+
+  throw new Error(
+    `Invalid target dir! Available options are "${SRC_DIR}" and "${DIST_DIR}"`
+  );
+}
---
+++ scripts/shared.js
@@ -0,0 +1,47 @@
+// @flow
+
+import { readFile, writeFile } from 'fs';
+import path from 'path';
+import glob from 'glob';
+
+type PackageNames = Array<string>;
+
+export const globAsync = asyncify(glob);
+export const readFileAsync = asyncify(readFile);
+export const writeFileAsync = asyncify(writeFile);
+
+const AS_IS_PACKAGES = ['react-cosmos-flow'];
+const BROWSER_PACKAGES = ['react-cosmos-playground'];
+
+export async function getNodePackages(): Promise<PackageNames> {
+  const allPackages = await getAllPackages();
+  const exclude = [...AS_IS_PACKAGES, ...BROWSER_PACKAGES];
+
+  return allPackages.filter(p => exclude.indexOf(p) === -1);
+}
+
+export async function getBrowserPackages(): Promise<PackageNames> {
+  const allPackages = await getAllPackages();
+
+  return allPackages.filter(p => BROWSER_PACKAGES.indexOf(p) !== -1);
+}
+
+export async function getAllPackages(): Promise<PackageNames> {
+  const files = await globAsync('./packages/react-*');
+
+  return files.map(f => path.basename(f));
+}
+
+function asyncify(fn: Function) {
+  return (...args: Array<*>): Promise<*> => {
+    return new Promise((resolve, reject) => {
+      fn(...args, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  };
+}
