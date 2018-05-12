f07b2e0d7b41cf8e4cd7d2c8e811bddfe37fb3fd
Ovidiu Cherecheș <skidding@gmail.com>
2018-05-12 19:05:23 +0700

    Document pitfall of isComponentClass function

--- packages/react-cosmos-loader/src/utils/is-component-class.js
+++ packages/react-cosmos-loader/src/utils/is-component-class.js
@@ -5,6 +5,9 @@ export function isComponentClass(componentType) {
   return (
     componentType.prototype &&
     // ES6 Class
+    // Warning: This will return false is the component is extending a
+    // different copy of React than the one used by Cosmos. This is relevant
+    // when running Cosmos from an external location instead of node_modules.
     (componentType.prototype instanceof Component ||
       // React.createClass
       componentType.prototype.getInitialState !== undefined) &&
