b0ab73776fbc0a1f892a191ef3bd9a3ef9cb05f2
maxsalven <max@salven.com>
2018-06-04 23:18:56 +0200

    Remove old /lib code

--- packages/react-cosmos-config/lib/config-templates.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', {
-  value: true
-});
-var CRA_COSMOS_CONFIG = (exports.CRA_COSMOS_CONFIG =
-  "module.exports = {\n  containerQuerySelector: '#root',\n  webpackConfigPath: 'react-scripts/config/webpack.config.dev',\n  publicPath: 'public',\n  // Optional: Create this file when you begin adding proxies\n  proxiesPath: 'src/cosmos.proxies'\n};\n");
