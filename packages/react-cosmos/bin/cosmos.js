977987f047c0da560e1be5330a2cd97038fd6d86
Ovidiu Cherecheș <skidding@gmail.com>
2018-06-14 13:20:57 +0300

    Brevity

--- packages/react-cosmos/bin/cosmos.js
+++ packages/react-cosmos/bin/cosmos.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-// Do this as the first thing so that any code reading it knows the right env.
+// Set the env before any code reads it
 process.env.BABEL_ENV = process.env.BABEL_ENV || 'development';
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
