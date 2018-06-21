a3020b99b1979fea2dbb548ab0117ae321e793d7
Ovidiu Cherecheș <skidding@gmail.com>
2018-06-21 11:26:52 +0300

    Fix lint error

--- examples/create-react-native-app/App.cosmos.js
+++ examples/create-react-native-app/App.cosmos.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { CosmosNativeLoader } from 'react-cosmos-loader/native';
-/* eslint-disable-next-line import/no-unresolved */
+/* eslint-disable-next-line import/no-unresolved, import/extensions */
 import { options, getUserModules } from './cosmos.modules';
 
 export default class App extends Component {
