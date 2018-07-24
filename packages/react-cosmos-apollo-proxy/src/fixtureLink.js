ca53b2fcbc709cf2270a846ca73157ae0ca06d9d
Simeon Cheeseman <simeon@thehavens.co.nz>
2018-07-24 19:38:33 +0900

    feat: apollo-proxy support returning a promise (#760)
    resloveWith function should be able to handle returning a Promise.

--- packages/react-cosmos-apollo-proxy/src/fixtureLink.js
+++ packages/react-cosmos-apollo-proxy/src/fixtureLink.js
@@ -1,8 +1,33 @@
 import { ApolloLink, Observable } from 'apollo-link';
 
+function resolveFunctionOrPromise(result, observer) {
+  // test to see if the result "looks" like a promise
+  if (typeof result.then === 'function' && typeof result.catch === 'function') {
+    result
+      .then(data => {
+        observer.next({
+          data
+        });
+      })
+      .catch(errors => {
+        observer.next({
+          errors
+        });
+      })
+      .then(() => {
+        observer.complete();
+      });
+  } else {
+    observer.next({
+      data: result
+    });
+    observer.complete();
+  }
+}
+
 export function createFixtureLink({ apolloFixture, cache, fixture }) {
   return new ApolloLink(
-    ({ operationName, variables }) =>
+    ({ operationName, variables, ...others }) =>
       new Observable(observer => {
         const { failWith, resolveWith } =
           apolloFixture[operationName] || apolloFixture;
@@ -11,19 +36,25 @@ export function createFixtureLink({ apolloFixture, cache, fixture }) {
           observer.error(failWith);
         }
 
-        observer.next({
-          data:
-            typeof resolveWith === 'function'
-              ? resolveWith({ cache, variables, fixture })
-              : resolveWith.data
-                ? resolveWith.data
-                : resolveWith,
-          errors:
-            (typeof resolveWith !== 'function' && resolveWith.errors) ||
-            undefined
-        });
+        if (typeof resolveWith === 'function') {
+          resolveFunctionOrPromise(
+            resolveWith({
+              cache,
+              variables,
+              fixture,
+              operationName,
+              ...others
+            }),
+            observer
+          );
+        } else {
+          observer.next({
+            data: resolveWith.data ? resolveWith.data : resolveWith,
+            errors: resolveWith.errors
+          });
 
-        observer.complete();
+          observer.complete();
+        }
       })
   );
 }
