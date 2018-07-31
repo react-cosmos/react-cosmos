51ad1b4d38b0511fc659736df342376ea6b366f2
Ovidiu Cherecheș <skidding@gmail.com>
2018-07-31 10:59:42 +0300

    Fix Native proxies import

--- packages/react-cosmos-loader/src/__tests__/native.js
+++ packages/react-cosmos-loader/src/__tests__/native.js
@@ -1,9 +1,12 @@
 // @flow
 
+import React from 'react';
 import until from 'async-until';
 import io from 'socket.io-client';
-import { create as renderer } from 'react-test-renderer';
-import createContext from 'react-cosmos-test/generic';
+// https://github.com/facebook/react-native/issues/19797
+// $FlowFixMe
+import { View, Text } from 'react-native';
+import { create as render } from 'react-test-renderer';
 import { CosmosNativeLoader } from '../native';
 
 const handlers = {};
@@ -29,26 +32,42 @@ jest.mock('socket.io-client', () =>
   })
 );
 
-const { mount } = createContext({
-  renderer,
-  fixture: {
-    component: CosmosNativeLoader,
-    props: {
-      options: {
-        port: 8080
-      },
-      modules: {
-        fixtureModules: {},
-        fixtureFiles: [],
-        proxies: []
-      }
-    }
-  }
-});
+const Proxy = props => {
+  const { nextProxy } = props;
+
+  return (
+    <View>
+      <Text>I am proxy</Text>
+      <nextProxy.value {...props} nextProxy={nextProxy.next()} />
+    </View>
+  );
+};
+const proxiesEsModule = { __esModule: true, default: [Proxy] };
+
+const Component = () => 'I am component';
+const fixture = { component: Component };
+const fixtureEsModule = { __esModule: true, default: fixture };
+
+let wrapper;
 
 beforeEach(async () => {
   jest.clearAllMocks();
-  await mount();
+  wrapper = render(
+    <CosmosNativeLoader
+      options={{
+        port: 8080
+      }}
+      modules={{
+        fixtureModules: { '/path/to/fixture': fixtureEsModule },
+        fixtureFiles: [{ filePath: '/path/to/fixture', components: [] }],
+        proxies: proxiesEsModule
+      }}
+    />
+  );
+});
+
+afterEach(() => {
+  wrapper.unmount();
 });
 
 it('calls Socket.IO client with port', async () => {
@@ -60,7 +79,7 @@ it('emits loaderReady', async () => {
   await until(() => mockEmit.mock.calls.length > 0);
   expect(mockEmit).toHaveBeenCalledWith('cosmos-cmd', {
     type: 'loaderReady',
-    fixtures: {}
+    fixtures: { Component: ['fixture'] }
   });
 });
 
@@ -73,6 +92,31 @@ it('emits loaderReady again on request', async () => {
   await until(() => mockEmit.mock.calls.length > 1);
   expect(mockEmit).nthCalledWith(2, 'cosmos-cmd', {
     type: 'loaderReady',
-    fixtures: {}
+    fixtures: { Component: ['fixture'] }
+  });
+});
+
+describe('on select', () => {
+  beforeEach(async () => {
+    await until(() => mockEmit.mock.calls.length > 0); // loaderReady
+    handlers['cosmos-cmd']({
+      type: 'fixtureSelect',
+      component: 'Component',
+      fixture: 'fixture'
+    });
+
+    await until(() => mockEmit.mock.calls.length > 1);
+    expect(mockEmit).nthCalledWith(2, 'cosmos-cmd', {
+      type: 'fixtureLoad',
+      fixtureBody: {}
+    });
+  });
+
+  it('renders proxy', () => {
+    expect(wrapper.toJSON().children[0].children).toContain('I am proxy');
+  });
+
+  it('renders selected fixture component', async () => {
+    expect(wrapper.toJSON().children).toContain('I am component');
   });
 });
