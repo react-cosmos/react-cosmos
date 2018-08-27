cff758a8a772784009cc6d79c41648834fb5922e
Ovidiu Cherecheș <skidding@gmail.com>
2018-08-28 00:13:02 +0300

    Fix slash helper

--- packages/react-cosmos-shared/src/server/slash-path.js
+++ packages/react-cosmos-shared/src/server/slash-path.js
@@ -5,5 +5,5 @@ import { default as _slash } from 'slash';
 
 // Convert Windows backslash paths to slash paths
 export function slash(...paths: string[]) {
-  return paths.length > 0 ? _slash(join(...paths)) : _slash(paths[0]);
+  return paths.length > 1 ? _slash(join(...paths)) : _slash(paths[0]);
 }
