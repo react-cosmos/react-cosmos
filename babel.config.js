27f5fb244e7ff0456ebf09a626bbafc40b96616f
Ovidiu Cherecheș <skidding@gmail.com>
2018-08-29 03:24:20 +0300

    Polyfill via babel runtime corejs2

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,6 @@ module.exports = {
   plugins: [
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-class-properties',
-    '@babel/transform-runtime'
+    ['@babel/transform-runtime', { corejs: 2 }]
   ]
 };
