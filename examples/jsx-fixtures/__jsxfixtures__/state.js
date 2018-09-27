10539d79880f28dbc6454deddce1cc2ae4b7dc6c
Ovidiu Cherecheș <skidding@gmail.com>
2018-09-27 12:49:35 +0300

    Use external component module in JSX example
    Works better with HMR

--- examples/jsx-fixtures/__jsxfixtures__/state.js
+++ examples/jsx-fixtures/__jsxfixtures__/state.js
@@ -1,21 +1,8 @@
 // @flow
 
-import React, { Component } from 'react';
+import React from 'react';
 import { StateMock } from '@react-mock/state';
-
-class Counter extends Component<{ suffix: string }, { count: number }> {
-  state = { count: 0 };
-
-  render() {
-    return (
-      <button
-        onClick={() => this.setState(({ count }) => ({ count: count + 1 }))}
-      >
-        {this.state.count} {this.props.suffix}
-      </button>
-    );
-  }
-}
+import { Counter } from '../components/Counter';
 
 export default (
   <StateMock state={{ count: 5 }}>
