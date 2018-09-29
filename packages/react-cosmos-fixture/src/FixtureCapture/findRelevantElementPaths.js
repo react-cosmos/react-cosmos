0dfb534737570ddf57473bb6997610f15507e73f
Ovidiu Cherecheș <skidding@gmail.com>
2018-09-29 13:45:21 +0300

    feat: Hide props/state of HTML and Styled elements

--- packages/react-cosmos-fixture/src/FixtureCapture/findRelevantElementPaths.js
+++ packages/react-cosmos-fixture/src/FixtureCapture/findRelevantElementPaths.js
@@ -7,11 +7,14 @@ import type { Children } from './childrenTree';
 export function findRelevantElementPaths(children: Children): string[] {
   const elPaths = findElementPaths(children);
 
-  // TODO: Ignore HTML elements
-  // TODO: Ignore StyledComponents
   return elPaths.filter(elPath => {
     const { type } = getExpectedElementAtPath(children, elPath);
 
-    return type.cosmosCapture !== false;
+    // TODO: Make this customizable
+    return (
+      typeof type !== 'string' &&
+      type.cosmosCapture !== false &&
+      type.name !== 'StyledComponent'
+    );
   });
 }
