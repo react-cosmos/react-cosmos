c76a8fc05565a10f08705a80d1916e04e8c8ddb0
Ovidiu Cherecheș <skidding@gmail.com>
2018-11-04 14:03:38 +0200

    Add full screen button

--- packages/react-cosmos-playground2/src/plugins/Nav/index.js
+++ packages/react-cosmos-playground2/src/plugins/Nav/index.js
@@ -6,39 +6,76 @@ import { register, Plugin, Plug } from 'react-plugin';
 import { PlaygroundContext } from '../../PlaygroundContext';
 import { FixtureTree } from './FixtureTree';
 
+import type { Node } from 'react';
+
+type Props = {
+  children: Node
+};
+
+class Root extends Component<Props> {
+  static contextType = PlaygroundContext;
+
+  render() {
+    const { children } = this.props;
+    const {
+      urlParams: { fullscreen }
+    } = this.context;
+
+    if (fullscreen) {
+      return (
+        <Container>
+          <Right key="right">{children}</Right>
+        </Container>
+      );
+    }
+
+    return (
+      <Container>
+        <Nav />
+        <Right key="right">{children}</Right>
+      </Container>
+    );
+  }
+}
+
 class Nav extends Component<{}> {
   static contextType = PlaygroundContext;
 
   render() {
     const {
+      urlParams: { fixture },
       uiState: { fixtures }
     } = this.context;
 
     return (
-      <FixtureTree fixtures={fixtures} onSelect={this.handleFixtureSelect} />
+      <Left>
+        {fixture && (
+          <Buttons>
+            <button onClick={this.handleGoHome}>home</button>
+            <button onClick={this.handleGoFullScreen}>fullscreen</button>
+          </Buttons>
+        )}
+        <FixtureTree fixtures={fixtures} onSelect={this.handleFixtureSelect} />
+      </Left>
     );
   }
 
-  handleFixtureSelect = (fixturePath: string) => {
-    const { setUrlParams } = this.context;
+  handleGoHome = () => {
+    this.context.setUrlParams({ fixture: undefined });
+  };
 
-    setUrlParams({ fixture: fixturePath });
+  handleFixtureSelect = (fixturePath: string) => {
+    this.context.setUrlParams({ fixture: fixturePath });
+  };
+
+  handleGoFullScreen = () => {
+    this.context.setUrlParams({ fullscreen: true });
   };
 }
 
 register(
   <Plugin name="Preview">
-    <Plug
-      slot="root"
-      render={({ children }) => (
-        <Container>
-          <Left>
-            <Nav />
-          </Left>
-          <Right>{children}</Right>
-        </Container>
-      )}
-    />
+    <Plug slot="root" render={Root} />
   </Plugin>
 );
 
@@ -59,4 +96,9 @@ const Right = styled.div`
   flex: 1;
   display: flex;
   flex-direction: column;
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
