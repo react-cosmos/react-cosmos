bbebddebce3c70dff97dcbe351f1b969bbd70c35
Ovidiu Cherecheș <skidding@gmail.com>
2018-11-28 11:21:01 +0200

    Add missing DEFAULT_DEVICES

--- packages/react-cosmos-playground2/src/plugins/ResponsivePreview/shared.js
+++ packages/react-cosmos-playground2/src/plugins/ResponsivePreview/shared.js
@@ -30,6 +30,15 @@ export type ResponsivePreviewState =
   | DisabledViewport
   | DisabledNoViewport;
 
+export const DEFAULT_DEVICES = [
+  { label: 'iPhone 5', width: 320, height: 568 },
+  { label: 'iPhone 6', width: 375, height: 667 },
+  { label: 'iPhone 6 Plus', width: 414, height: 736 },
+  { label: 'Medium', width: 1024, height: 768 },
+  { label: 'Large', width: 1440, height: 900 },
+  { label: '1080p', width: 1920, height: 1080 }
+];
+
 export const DEFAULT_VIEWPORT = {
   width: 320,
   height: 568
