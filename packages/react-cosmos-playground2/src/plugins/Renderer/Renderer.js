9dbd1fd4f37198149d63e6c15bb9772f5ec78aa8
Ovidiu Cherecheș <skidding@gmail.com>
2018-12-03 10:31:47 +0200

    Kept fixture state when selecting fixture

--- packages/react-cosmos-playground2/src/plugins/Renderer/Renderer.js
+++ packages/react-cosmos-playground2/src/plugins/Renderer/Renderer.js
@@ -100,11 +100,16 @@ export class Renderer extends Component<{}> {
   }
 
   handleSelectFixture = (fixturePath: string) => {
-    this.resetRendererState(() => {
-      this.forEachRenderer(rendererId =>
-        this.postSelectFixtureRequest(rendererId, fixturePath, null)
-      );
-    });
+    // NOTE: The fixture state used to be reset in the local renderer state
+    // before posting the "selectFixture" request, but that no longer happens.
+    // Resetting renderer state when selecting a fixture makes sense in
+    // abstract, but it creates an unnecessary flash of layout whenever
+    // reselecting the current fixture, or selecting a fixture of the same
+    // component. By keeping the fixture state until the new fixture state is
+    // received from the renderer, the transition between fixtures is smoother.
+    this.forEachRenderer(rendererId =>
+      this.postSelectFixtureRequest(rendererId, fixturePath, null)
+    );
   };
 
   handleUnselectFixture = () => {
