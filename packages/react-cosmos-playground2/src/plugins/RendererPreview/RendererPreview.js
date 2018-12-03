f931e4a5987554e8a87283821e4ce037f64cb2c2
Ovidiu Cherecheș <skidding@gmail.com>
2018-12-03 10:32:44 +0200

    TODOs

--- packages/react-cosmos-playground2/src/plugins/RendererPreview/RendererPreview.js
+++ packages/react-cosmos-playground2/src/plugins/RendererPreview/RendererPreview.js
@@ -54,7 +54,9 @@ export class RendererPreview extends Component<Props> {
   };
 
   handleWindowMsg = (msg: Object) => {
-    // TODO: Validate
+    // TODO: Validate message shape
+    // TODO: Filter out alien messages (maybe source: "cosmos")
+    // TODO: https://github.com/facebook/react-devtools/issues/812#issuecomment-308827334
     this.context.emitEvent('renderer.response', msg.data);
   };
 
