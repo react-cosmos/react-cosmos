d67ec24590bf85c16895535d32c6203eb34c7186
Ovidiu Cherecheș <skidding@gmail.com>
2018-12-07 21:03:16 +0200

    Fix Router selectors

--- packages/react-cosmos-playground2/src/plugins/Router/selectors.js
+++ packages/react-cosmos-playground2/src/plugins/Router/selectors.js
@@ -1,8 +1,8 @@
 // @flow
 
-import type { PluginContextValue } from '../../plugin';
-import type { UrlParams } from './shared';
+import type { IPluginContext } from 'react-plugin';
+import type { RouterState } from './shared';
 
-export function getUrlParams(context: PluginContextValue): UrlParams {
-  return context.getState('router').urlParams;
+export function getUrlParams(context: IPluginContext<{}, RouterState>) {
+  return context.getState().urlParams;
 }
