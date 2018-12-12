e63f90d0a80f3e10fd44c3261684c224a3d33461
Ovidiu Cherecheș <skidding@gmail.com>
2018-12-12 17:03:55 +0200

    Add list of disabled plugins

--- packages/react-cosmos-playground2/src/index.js
+++ packages/react-cosmos-playground2/src/index.js
@@ -3,12 +3,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { getDomContainer } from 'react-cosmos-shared2/dom';
+import { enablePlugin } from 'ui-plugin';
 import { loadPlugins, Slot } from 'react-plugin';
 import './register-plugins';
 
 import type { PlaygroundConfig } from './index.js.flow';
 
+// TODO: Read list of disabled plugins from user config
+const DISABLED_PLUGINS = ['controlPanel'];
+
 export default function mount(config: PlaygroundConfig) {
+  DISABLED_PLUGINS.forEach(pluginName => {
+    enablePlugin(pluginName, false);
+  });
   loadPlugins({ config });
   render(<Slot name="root" />, getDomContainer());
 }
