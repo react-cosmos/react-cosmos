3c6828d19f96623b4982534e73235a9dd85caf2e
Ovidiu Cherecheș <skidding@gmail.com>
2018-12-13 15:57:21 +0200

    Attach react-plugin API to window

--- packages/react-cosmos-playground2/src/index.js
+++ packages/react-cosmos-playground2/src/index.js
@@ -3,18 +3,25 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { getDomContainer } from 'react-cosmos-shared2/dom';
-import { enablePlugin, loadPlugins, Slot } from 'react-plugin';
+import * as ReactPlugin from 'react-plugin';
 import './register-plugins';
 
 import type { PlaygroundConfig } from './index.js.flow';
+
+// Enable external plugins to use a shared copy of react-plugin. Also enable
+// fiddling with plugins from browser console :D.
+global.ReactPlugin = ReactPlugin;
 
 // TODO: Read list of disabled plugins from user config
 const DISABLED_PLUGINS = ['controlPanel'];
 
 export default function mount(config: PlaygroundConfig) {
+  const { enablePlugin, loadPlugins, Slot } = ReactPlugin;
+
   DISABLED_PLUGINS.forEach(pluginName => {
     enablePlugin(pluginName, false);
   });
   loadPlugins({ config });
+
   render(<Slot name="root" />, getDomContainer());
 }
