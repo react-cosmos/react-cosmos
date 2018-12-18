a1de30f609c48da0bc0460877fd0a2e347c92b71
Ovidiu Cherecheș <skidding@gmail.com>
2018-12-18 11:18:55 +0200

    Import React devtools hook before anything ese

--- packages/react-cosmos/src/server/web/webpack/enhance-webpack-config.js
+++ packages/react-cosmos/src/server/web/webpack/enhance-webpack-config.js
@@ -109,7 +109,9 @@ export default function enhanceWebpackConfig({
 }
 
 function getEntry({ next, globalImports, hot }, shouldExport) {
-  let entry = [...globalImports];
+  // The React devtools hook needs to be imported before any other module which
+  // might import React
+  let entry = [resolveClientPath('react-devtools-hook'), ...globalImports];
 
   if (hot && !shouldExport) {
     entry = [
@@ -120,15 +122,14 @@ function getEntry({ next, globalImports, hot }, shouldExport) {
     ];
   }
 
-  // Load loader entry last
   return [
     ...entry,
-    require.resolve(
-      next
-        ? '../../../client/loader-entry-next'
-        : '../../../client/loader-entry'
-    )
+    resolveClientPath(next ? 'loader-entry-next' : 'loader-entry')
   ];
+}
+
+function resolveClientPath(p) {
+  return require.resolve(`../../../client/${p}`);
 }
 
 function getOutput({ outputPath, publicUrl }, shouldExport) {
