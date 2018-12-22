dceaaf526f3a9bd9f151bc78d073ecf29d5645b2
Ovidiu Cherecheș <skidding@gmail.com>
2018-12-22 11:08:19 +0200

    Update icons

--- packages/react-cosmos-playground2/src/shared/icons/index.js
+++ packages/react-cosmos-playground2/src/shared/icons/index.js
@@ -21,16 +21,41 @@ export const FolderIcon = () => (
   </Icon>
 );
 
-export const XIcon = () => (
+export const XCircleIcon = () => (
   <Icon>
-    <line x1="18" y1="6" x2="6" y2="18" />
-    <line x1="6" y1="6" x2="18" y2="18" />
+    <circle cx="12" cy="12" r="10" />
+    <line x1="15" y1="9" x2="9" y2="15" />
+    <line x1="9" y1="9" x2="15" y2="15" />
   </Icon>
 );
 
 export const MaximizeIcon = () => (
   <Icon>
     <path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3" />
+  </Icon>
+);
+
+export const SmartphoneIcon = () => (
+  <Icon>
+    <rect x="5" y="2" width="14" height="20" rx="2" ry="2" />
+    <line x1="12" y1="18" x2="12" y2="18" />
+  </Icon>
+);
+
+export const Minimize2Icon = () => (
+  <Icon>
+    <polyline points="4 14 10 14 10 20" />
+    <polyline points="20 10 14 10 14 4" />
+    <line x1="14" y1="10" x2="21" y2="3" />
+    <line x1="3" y1="21" x2="10" y2="14" />
+  </Icon>
+);
+
+export const RefreshCwIcon = () => (
+  <Icon>
+    <polyline points="23 4 23 10 17 10" />
+    <polyline points="1 20 1 14 7 14" />
+    <path d="M3.51 9a9 9 0 0 1 14.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0 0 20.49 15" />
   </Icon>
 );
 
