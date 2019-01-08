dc2595d7edd7305c091da7d6142c7558c9d85793
Ovidiu Cherecheș <skidding@gmail.com>
2019-01-08 14:53:05 +0200

    Improve deep element comparision

--- packages/react-cosmos-fixture/src/FixtureCapture/childrenTree/areChildrenEqual.js
+++ packages/react-cosmos-fixture/src/FixtureCapture/childrenTree/areChildrenEqual.js
@@ -1,36 +1,32 @@
 // @flow
 
-import { isEqual, pick } from 'lodash';
+import { isEqual, pick, mapValues } from 'lodash';
 import { isElement } from 'react-is';
 
 import type { Element } from 'react';
 import type { Children } from './shared';
 
 export function areChildrenEqual(a: Children, b: Children): boolean {
-  return isEqual(stripInternalNodeAttrs(a), stripInternalNodeAttrs(b));
+  return isEqual(stripInternalElementAttrs(a), stripInternalElementAttrs(b));
 }
 
 // Don't compare private element attrs like _owner and _store, which hold
 // internal details and have auto increment-type attrs
-function stripInternalNodeAttrs(node: Children) {
-  if (Array.isArray(node)) {
-    return node.map(n => stripInternalNodeAttrs(n));
+function stripInternalElementAttrs(children: Children) {
+  if (Array.isArray(children)) {
+    return children.map(c => stripInternalElementAttrs(c));
   }
 
-  if (isElement(node)) {
+  if (isElement(children)) {
     // $FlowFixMe Flow can't get cues from react-is package
-    const el: Element<any> = node;
-    const { props } = el;
-    const { children } = props;
+    const el: Element<any> = children;
 
     return {
       ...pick(el, 'type', 'key', 'ref'),
-      props: {
-        ...props,
-        children: children && stripInternalNodeAttrs(children)
-      }
+      // children and other props can contain Elements
+      props: mapValues(el.props, prop => stripInternalElementAttrs(prop))
     };
   }
 
-  return node;
+  return children;
 }
