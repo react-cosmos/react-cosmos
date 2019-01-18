a5efafd425910dd7b556ef3be8e3b8ef9a22edd3
Ovidiu Cherecheș <skidding@gmail.com>
2019-01-18 16:23:45 +0200

    Remove superfluous method

--- packages/react-cosmos-playground2/src/plugins/RendererPreview/index.js
+++ packages/react-cosmos-playground2/src/plugins/RendererPreview/index.js
@@ -4,7 +4,6 @@ import { registerPlugin } from 'react-plugin';
 import { checkRendererStatus } from './checkRendererStatus';
 import { createRendererRequestHandler } from './onRendererRequest';
 import { handleWindowMessages } from './handleWindowMessages';
-import { isVisible } from './isVisible';
 import { RendererPreview } from './RendererPreview';
 
 import type { RendererCoordinatorConfig } from '../RendererCoordinator';
@@ -15,7 +14,7 @@ export type { UrlStatus, RuntimeStatus, RendererPreviewState } from './shared';
 export function register() {
   const { onRendererRequest, setIframeRef } = createRendererRequestHandler();
 
-  const { init, on, method, plug } = registerPlugin<{}, RendererPreviewState>({
+  const { init, on, plug } = registerPlugin<{}, RendererPreviewState>({
     name: 'rendererPreview',
     initialState: {
       urlStatus: 'unknown',
@@ -24,10 +23,6 @@ export function register() {
   });
 
   on('rendererCoordinator.request', onRendererRequest);
-
-  // This method lets plugins that overlay the "rendererPreview" slot know when
-  // to get out of the way
-  method('isVisible', isVisible);
 
   init(context => {
     const rendererUrl = getRendererUrl(context);
@@ -61,4 +56,23 @@ function getRendererUrl({ getConfigOf }) {
   );
 
   return webUrl;
+}
+
+function isVisible(context) {
+  const { runtimeStatus } = context.getState();
+  const rendererConnected = runtimeStatus === 'connected';
+  const rendererBroken = runtimeStatus === 'error';
+
+  // Here's a scenario when it's required to show the renderer preview iframe
+  // even if no fixture is loaded: Say a run-time error occurs in the renderer
+  // code (user build), inside a fixture or a global import. The Cosmos-related
+  // renderer code is no longer able to report the list of fixture because its
+  // host runtime already crashed. Without showing the renderer iframe
+  // immediately, users would be stuck with a blank Playground UI state, with
+  // no information on what's going on inside the renderer iframe.
+  return (rendererConnected && isFixtureSelected(context)) || rendererBroken;
+}
+
+function isFixtureSelected({ getStateOf }) {
+  return getStateOf('router').urlParams.fixturePath !== undefined;
 }
