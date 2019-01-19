a7445774fda317f1568371d87187c891dc30eeb8
Ovidiu Cherecheș <skidding@gmail.com>
2019-01-19 14:05:50 +0200

    Remove unused import

--- packages/react-cosmos-playground2/src/plugins/ContentOverlay/ContentOverlay.js
+++ packages/react-cosmos-playground2/src/plugins/ContentOverlay/ContentOverlay.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import {
-  BlankCanvasIllustration,
   EmptyIllustration,
   DreamerIllustration
 } from '../../shared/illustrations';
