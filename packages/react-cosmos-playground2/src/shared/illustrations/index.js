4349b88eaf11d91d57c97d43b2f6c3b0ec4885ee
Ovidiu Cherecheș <skidding@gmail.com>
2019-01-19 14:01:49 +0200

    Remove unused illustration

--- packages/react-cosmos-playground2/src/shared/illustrations/index.js
+++ packages/react-cosmos-playground2/src/shared/illustrations/index.js
@@ -5,4 +5,3 @@ export { EmptyIllustration } from './Empty';
 export { DreamerIllustration } from './Dreamer';
 export { ArtificialIntelligenceIllustration } from './ArtificialIntelligence';
 export { AstronautIllustration } from './Astronaut';
-export { OuterSpaceIllustration } from './OuterSpace';
