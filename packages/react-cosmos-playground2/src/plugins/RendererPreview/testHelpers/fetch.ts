d5c19d604774478db991cb81f3c965fa70cbcc07
Ovidiu Cherecheș <skidding@gmail.com>
2019-02-07 21:10:19 +0200

    Upgrade deps

--- packages/react-cosmos-playground2/src/plugins/RendererPreview/testHelpers/fetch.ts
+++ packages/react-cosmos-playground2/src/plugins/RendererPreview/testHelpers/fetch.ts
@@ -1,3 +1,3 @@
 export function fakeFetchResponseStatus(status: number) {
-  window.fetch = jest.fn(() => Promise.resolve({ status }));
+  (window as any).fetch = jest.fn(() => Promise.resolve({ status }));
 }
