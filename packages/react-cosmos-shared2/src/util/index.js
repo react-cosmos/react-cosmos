c8e8c540122ef87580e1f79928c445da7e67de55
Ovidiu Cherecheș <skidding@gmail.com>
2019-02-07 21:19:57 +0200

    Fix Flow error

--- packages/react-cosmos-shared2/src/util/index.js
+++ packages/react-cosmos-shared2/src/util/index.js
@@ -52,7 +52,10 @@ export function removeItem<T>(items: $ReadOnlyArray<T>, item: T): Array<T> {
   return [...items.slice(0, index), ...items.slice(index + 1)];
 }
 
-export function updateState<T: {}>(prevState: T, updater: StateUpdater<T>): T {
+export function updateState<T: null | {}>(
+  prevState: T,
+  updater: StateUpdater<T>
+): T {
   return typeof updater === 'function' ? updater(prevState) : updater;
 }
 
