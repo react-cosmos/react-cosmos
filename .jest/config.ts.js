6357e1e3c815f2606e07ef17eb689242323eb133
Ovidiu Cherecheș <skidding@gmail.com>
2019-02-07 20:41:20 +0200

    Update Jest configs

--- .jest/config.ts.js
+++ .jest/config.ts.js
@@ -5,5 +5,15 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['**/__tests__/**/*.{ts,tsx}', '**/?(*.)test.{ts,tsx}'],
-  setupTestFrameworkScriptFile: '<rootDir>/.jest/setup-framework.ts'
+  setupTestFrameworkScriptFile: '<rootDir>/.jest/setup-framework.ts',
+  collectCoverageFrom: [
+    '**/src/**/*.{ts,tsx}',
+    '!**/{__fixtures__,__jsxfixtures__}/**',
+    '!**/*.{fixture,jsxfixture}.{js,ts,tsx}',
+    '!**/cosmos.decorator.{js,ts,tsx}',
+    '!**/testHelpers/**',
+    '!**/react-cosmos-playground2/src/shared/illustrations/**',
+    // Ignore coverage from dark launched APIs
+    '!**/react-cosmos-playground2/src/plugins/ControlPanel/**'
+  ]
 };
