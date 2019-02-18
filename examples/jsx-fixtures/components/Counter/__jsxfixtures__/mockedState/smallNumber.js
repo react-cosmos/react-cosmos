a9e011f51d699645679998448c88bb1730f11006
Ovidiu Cherecheș <skidding@gmail.com>
2019-02-18 12:49:34 +0200

    Add multi JSX fixture example

--- examples/jsx-fixtures/components/Counter/__jsxfixtures__/mockedState/smallNumber.js
+++ examples/jsx-fixtures/components/Counter/__jsxfixtures__/mockedState/smallNumber.js
@@ -4,8 +4,15 @@ import React from 'react';
 import { StateMock } from '@react-mock/state';
 import { Counter } from '../..';
 
-export default (
-  <StateMock state={{ count: 5 }}>
-    <Counter suffix="times" />
-  </StateMock>
-);
+export default {
+  five: (
+    <StateMock state={{ count: 5 }}>
+      <Counter suffix="times" />
+    </StateMock>
+  ),
+  fifyFive: (
+    <StateMock state={{ count: 55 }}>
+      <Counter suffix="times" />
+    </StateMock>
+  )
+};
