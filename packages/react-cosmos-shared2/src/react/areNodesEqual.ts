9e9e3846b5501c9bd51652797a7da32c90dc8829
Ovidiu Cherecheș <skidding@gmail.com>
2019-03-12 22:31:40 +0200

    Update areNodesEqual.ts

--- packages/react-cosmos-shared2/src/react/areNodesEqual.ts
+++ packages/react-cosmos-shared2/src/react/areNodesEqual.ts
@@ -8,22 +8,20 @@ export function areNodesEqual(a: React.ReactNode, b: React.ReactNode): boolean {
 
 // Don't compare private element attrs like _owner and _store, which hold
 // internal details and have auto increment-type attrs
-function stripInternalElementAttrs(node: React.ReactNode): React.ReactNode {
+function stripInternalElementAttrs(node: React.ReactNode): {} {
   if (Array.isArray(node)) {
     return node.map(n => stripInternalElementAttrs(n));
   }
-
-  if (isElement(node)) {
-    const el = node as React.ReactElement<any>;
-
-    return {
-      ...pick(el, 'type', 'key', 'ref'),
-      // children and other props can contain Elements
-      props: mapValues(el.props, propValue =>
-        stripInternalElementAttrs(propValue)
-      )
-    };
+  if (!isElement(node)) {
+    return node;
   }
 
-  return node;
+  const el = node as React.ReactElement<any>;
+  return {
+    ...pick(el, 'type', 'key', 'ref'),
+    // children and other props can contain Elements
+    props: mapValues(el.props, propValue =>
+      stripInternalElementAttrs(propValue)
+    )
+  };
 }
