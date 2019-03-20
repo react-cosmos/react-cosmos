9bcfc9ebc0119e11a933d6cd64231b6e1c62d163
Ovidiu Cherecheș <skidding@gmail.com>
2019-03-20 17:59:19 +0200

    Export fixture state types (#938)

--- packages/react-cosmos-fixture/src/index.ts
+++ packages/react-cosmos-fixture/src/index.ts
@@ -4,6 +4,8 @@ export { PostMessage } from './FixtureConnect/PostMessage';
 export { WebSockets } from './FixtureConnect/WebSockets';
 
 // Use in decorators
+export { FixtureState } from 'react-cosmos-shared2/fixtureState';
+export { SetFixtureState } from './shared';
 export { FixtureContext } from './FixtureContext';
 
 // Advanced: Use in fixtures to capture elements in render callback
