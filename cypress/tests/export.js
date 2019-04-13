afcbcf1611a24b1812ae971186ca0fd684d1c384
Ovidiu Cherecheș <skidding@gmail.com>
2019-04-13 16:16:02 +0300

    Reuse Cypress tests

--- cypress/tests/export.js
+++ cypress/tests/export.js
@@ -1,4 +1,9 @@
 import { clearTreeViewState } from '../support/localStorage';
+import {
+  homepageTests,
+  navTests,
+  selectFixtureTests
+} from '../support/testBlocks';
 
 describe('Export', () => {
   // WARNING: These tests are serial and share state
@@ -6,57 +11,7 @@ describe('Export', () => {
     return clearTreeViewState().then(() => cy.visit('http://localhost:5001'));
   });
 
-  context('homepage', () => {
-    it('has document title', () => {
-      cy.title().should('include', 'React Cosmos');
-    });
-
-    it('displays welcome message', () => {
-      cy.contains('Welcome to Cosmos Next');
-    });
-
-    it('shows renderer connected notification', () => {
-      cy.contains('Renderer connected');
-    });
-  });
-
-  context('nav', () => {
-    it('renders tree view root items', () => {
-      cy.contains('Counter');
-      cy.contains('WelcomeMessage');
-      cy.contains('hello world');
-    });
-
-    it('expands tree view items', () => {
-      cy.contains('Counter').click();
-      cy.contains('small numbers').click();
-      cy.contains('fifty');
-      cy.contains('fifty five');
-    });
-  });
-
-  context('select fixture', () => {
-    it('renders selected fixtures', () => {
-      cy.contains('hello world').click();
-      getRendererBody()
-        .find('#root')
-        .should('have.text', 'Hello World!');
-    });
-
-    it('renders updated fixture', () => {
-      cy.contains('large number').click();
-      getRendererBody()
-        .find('button')
-        .should('have.text', '555555555 times');
-      getRendererBody()
-        .find('button')
-        .click()
-        .click()
-        .should('have.text', '555555557 times');
-    });
-  });
+  homepageTests();
+  navTests();
+  selectFixtureTests();
 });
-
-function getRendererBody() {
-  return cy.get('iframe').then($iframe => $iframe.contents().find('body'));
-}
