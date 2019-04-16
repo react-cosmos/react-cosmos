269a0d90271e05792b07aeddfa8143cc5c0aa90d
Ovidiu Cherecheș <skidding@gmail.com>
2019-04-16 22:32:07 +0300

    Gracefully degrade clipboard-write permission check

--- packages/react-cosmos-playground2/src/plugins/RendererRemote/RemoteButton/copyToClipboard.ts
+++ packages/react-cosmos-playground2/src/plugins/RendererRemote/RemoteButton/copyToClipboard.ts
@@ -11,8 +11,18 @@ interface IExtendedNavigator extends Navigator {
 export async function copyToClipboard(text: string): Promise<void> {
   const { permissions, clipboard } = navigator as IExtendedNavigator;
 
-  const { state } = await permissions.query({ name: 'clipboard-write' });
-  if (state !== 'granted' && state !== 'prompt') {
+  let permissionDenied = false;
+  try {
+    const { state } = await permissions.query({ name: 'clipboard-write' });
+    permissionDenied = state !== 'granted' && state !== 'prompt';
+  } catch (e) {
+    // Some browsers (eg. Firefox 66) don't support the 'clipboard-write'
+    // PermissionDescriptor but support clipboard.writeText. So unless
+    // permission is explicitly denied, we try to copy to clipboard even if the
+    // permission check failed.
+  }
+
+  if (permissionDenied) {
     throw new Error('Permission denied to write to clipboard');
   }
 
