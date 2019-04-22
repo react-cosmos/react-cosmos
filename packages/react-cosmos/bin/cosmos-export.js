1f97af8203580e51aa37959bfb4882d92a23d858
Ovidiu Cherecheș <skidding@gmail.com>
2019-04-22 18:48:22 +0300

    Clean up unused env var

--- packages/react-cosmos/bin/cosmos-export.js
+++ packages/react-cosmos/bin/cosmos-export.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 // Set the env before any code reads it
-process.env.BABEL_ENV = process.env.BABEL_ENV || 'production';
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 const { generateWebExport } = require('../dist/webExport');
--- packages/react-cosmos/bin/cosmos-native.js
+++ packages/react-cosmos/bin/cosmos-native.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 // Set the env before any code reads it
-process.env.BABEL_ENV = process.env.BABEL_ENV || 'development';
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const { startNativeServer } = require('../dist/nativeServer');
