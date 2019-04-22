350c271c7cd6128b9b07f0531a0d86fd9d1ffdc3
Ovidiu Cherecheș <skidding@gmail.com>
2019-04-22 16:23:55 +0300

    Throw if explicit webpack config path is invalid

--- packages/react-cosmos/src/plugins/webpack/cosmosConfig/webpack.ts
+++ packages/react-cosmos/src/plugins/webpack/cosmosConfig/webpack.ts
@@ -1,4 +1,6 @@
+import path from 'path';
 import { CosmosConfig, resolvePath } from '../../../config';
+import { fileExists } from '../../../shared/fs';
 
 type WebpackCosmosConfig = {
   configPath: null | string;
@@ -20,10 +22,26 @@ export function createWebpackCosmosConfig(
 }
 
 function getWebpackConfigPath(
-  { configPath = 'webpack.config.js' }: WebpackCosmosConfigInput,
+  { configPath }: WebpackCosmosConfigInput,
   rootDir: string
 ) {
-  return configPath && resolvePath(rootDir, configPath);
+  if (typeof configPath === 'undefined') {
+    return resolvePath(rootDir, 'webpack.config.js');
+  }
+
+  // User can choose to prevent automatical use of an existing webpack.config.js
+  // file (relative to the root dir) by setting configPath to null
+  if (!configPath) {
+    return null;
+  }
+
+  const absPath = resolvePath(rootDir, configPath);
+  if (!fileExists(absPath)) {
+    const relPath = path.relative(process.cwd(), absPath);
+    throw new Error(`webpack config not found at path: ${relPath}`);
+  }
+
+  return absPath;
 }
 
 function getHotReload({ hotReload = true }: WebpackCosmosConfigInput) {
