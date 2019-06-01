aa55fea46a7a8bf96c0a0daa3555820c60463953
Ovidiu Cherecheș <skidding@gmail.com>
2019-06-01 14:38:10 +0300

    Test active fullscreen button

---
+++ packages/react-cosmos-playground2/src/plugins/FullScreenButton/__tests__/selectedFixture.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import { Slot, loadPlugins } from 'react-plugin';
+import { cleanup } from '../../../testHelpers/plugin';
+import { mockRouter, mockRendererCore } from '../../../testHelpers/pluginMocks';
+import { register } from '..';
+
+afterEach(cleanup);
+
+function registerTestPlugins() {
+  register();
+  mockRendererCore({
+    isRendererConnected: () => true,
+    isValidFixtureSelected: () => true
+  });
+}
+
+function loadTestPlugins() {
+  loadPlugins();
+  return render(<Slot name="rendererActions" />);
+}
+
+it('renders fullscreen button', async () => {
+  registerTestPlugins();
+
+  const selectFixture = jest.fn();
+  mockRouter({
+    getSelectedFixtureId: () => ({ path: 'foo', name: null }),
+    selectFixture
+  });
+
+  const { getByText } = loadTestPlugins();
+  fireEvent.click(getByText(/fullscreen/i));
+
+  expect(selectFixture).toBeCalledWith(
+    expect.any(Object),
+    { path: 'foo', name: null },
+    true
+  );
+});
