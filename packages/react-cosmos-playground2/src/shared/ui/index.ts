1ae91f617681618ab275f41553c85ad23ec79d97
Ovidiu Cherecheș <skidding@gmail.com>
2019-06-01 20:48:32 +0300

    Use TreeView in Props/ClassState panels

--- packages/react-cosmos-playground2/src/shared/ui/index.ts
+++ packages/react-cosmos-playground2/src/shared/ui/index.ts
@@ -1,4 +1,4 @@
 export { Button } from './Button';
 export { TreeNode, TreeNodeDirs, TreeExpansion, TreeView } from './TreeView';
-export { ValueInput } from './ValueInput';
+export { ValueInputTree } from './ValueInputTree';
 export { useDrag } from './useDrag';
