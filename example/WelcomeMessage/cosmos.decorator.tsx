e534926f99d28636821315bf37fc647e73d844fc
Ovidiu Cherecheș <skidding@gmail.com>
2019-06-14 01:11:14 +0300

    Convert example files to TS

--- example/WelcomeMessage/cosmos.decorator.tsx
+++ example/WelcomeMessage/cosmos.decorator.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default ({ children }) => (
+export default ({ children }: { children: React.ReactNode }) => (
   <div
     style={{ border: '2px dashed #AAA', padding: '0 16px', borderRadius: 8 }}
   >
