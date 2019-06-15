c6a9bf38712ce1d35b13e8aaac44007550a2efa5
Ovidiu Cherecheș <skidding@gmail.com>
2019-06-15 23:46:07 +0300

    Show error notifications when fixture file fails to open

--- packages/react-cosmos-playground2/src/plugins/EditFixtureButton/index.test.tsx
+++ packages/react-cosmos-playground2/src/plugins/EditFixtureButton/index.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { loadPlugins, ArraySlot } from 'react-plugin';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, wait, RenderResult } from '@testing-library/react';
 import { register } from '.';
 import { cleanup } from '../../testHelpers/plugin';
-import { mockCore, mockRouter } from '../../testHelpers/pluginMocks';
+import {
+  mockCore,
+  mockRouter,
+  mockNotifications
+} from '../../testHelpers/pluginMocks';
 import { mockFetch } from './testHelpers';
 
 afterEach(cleanup);
@@ -19,12 +23,18 @@ async function loadTestPlugins() {
   return render(<ArraySlot name="fixtureActions" />);
 }
 
+function clickButton({ getByTitle }: RenderResult) {
+  const editBtn = getByTitle(/open fixture source/i);
+  fireEvent.click(editBtn);
+}
+
 it(`doesn't render button when dev server is off`, async () => {
   register();
   mockSelectedFixtureId();
   mockCore({
     isDevServerOn: () => false
   });
+  mockNotifications();
 
   const { queryByTitle } = await loadTestPlugins();
   expect(queryByTitle(/open fixture source/i)).toBeNull();
@@ -36,24 +46,94 @@ it('renders button', async () => {
   mockCore({
     isDevServerOn: () => true
   });
+  mockNotifications();
 
   const { getByTitle } = await loadTestPlugins();
   getByTitle(/open fixture source/i);
 });
 
 it('calls server endpoint on button click', async () => {
-  await mockFetch(async fetchMock => {
+  await mockFetch(200, async fetchMock => {
     register();
     mockSelectedFixtureId();
     mockCore({
       isDevServerOn: () => true
     });
+    mockNotifications();
 
-    const { getByTitle } = await loadTestPlugins();
-    const editBtn = getByTitle(/open fixture source/i);
-    fireEvent.click(editBtn);
+    const renderer = await loadTestPlugins();
+    clickButton(renderer);
 
     const openFileUrl = '/_open?filePath=foo.js';
     expect(fetchMock).toBeCalledWith(openFileUrl, expect.any(Object));
+  });
+});
+
+it('shows 400 error notification', async () => {
+  await mockFetch(400, async () => {
+    register();
+    mockSelectedFixtureId();
+    mockCore({
+      isDevServerOn: () => true
+    });
+    const { pushTimedNotification } = mockNotifications();
+
+    const renderer = await loadTestPlugins();
+    clickButton(renderer);
+
+    await wait(() =>
+      expect(pushTimedNotification).toBeCalledWith(expect.any(Object), {
+        id: expect.any(String),
+        type: 'error',
+        title: 'Failed to open fixture',
+        info: 'This looks like a bug. Let us know please!'
+      })
+    );
+  });
+});
+
+it('shows 404 error notification', async () => {
+  await mockFetch(404, async () => {
+    register();
+    mockSelectedFixtureId();
+    mockCore({
+      isDevServerOn: () => true
+    });
+    const { pushTimedNotification } = mockNotifications();
+
+    const renderer = await loadTestPlugins();
+    clickButton(renderer);
+
+    await wait(() =>
+      expect(pushTimedNotification).toBeCalledWith(expect.any(Object), {
+        id: expect.any(String),
+        type: 'error',
+        title: 'Failed to open fixture',
+        info: 'File is missing. Weird!'
+      })
+    );
+  });
+});
+
+it('shows 500 error notification', async () => {
+  await mockFetch(500, async () => {
+    register();
+    mockSelectedFixtureId();
+    mockCore({
+      isDevServerOn: () => true
+    });
+    const { pushTimedNotification } = mockNotifications();
+
+    const renderer = await loadTestPlugins();
+    clickButton(renderer);
+
+    await wait(() =>
+      expect(pushTimedNotification).toBeCalledWith(expect.any(Object), {
+        id: expect.any(String),
+        type: 'error',
+        title: 'Failed to open fixture',
+        info: 'Does your OS know to open source files with your code editor?'
+      })
+    );
   });
 });
--- packages/react-cosmos-playground2/src/plugins/EditFixtureButton/index.tsx
+++ packages/react-cosmos-playground2/src/plugins/EditFixtureButton/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createPlugin } from 'react-plugin';
 import { CoreSpec } from '../Core/public';
 import { RouterSpec } from '../Router/public';
+import { NotificationsSpec } from '../Notifications/public';
 import { EditFixtureButton } from './EditFixtureButton';
 import { EditFixtureButtonSpec } from './public';
 
@@ -9,13 +10,30 @@ const { plug, register } = createPlugin<EditFixtureButtonSpec>({
   name: 'editFixtureButton'
 });
 
+const ERORR_TITLE = 'Failed to open fixture';
+
 plug('fixtureActions', ({ pluginContext: { getMethodsOf } }) => {
   const core = getMethodsOf<CoreSpec>('core');
   const router = getMethodsOf<RouterSpec>('router');
+
+  const notifications = getMethodsOf<NotificationsSpec>('notifications');
+  const { pushTimedNotification } = notifications;
+  const onError = React.useCallback(
+    info =>
+      pushTimedNotification({
+        id: 'edit-fixture',
+        type: 'error',
+        title: ERORR_TITLE,
+        info
+      }),
+    [pushTimedNotification]
+  );
+
   return (
     <EditFixtureButton
       devServerOn={core.isDevServerOn()}
       selectedFixtureId={router.getSelectedFixtureId()}
+      onError={onError}
     />
   );
 });
