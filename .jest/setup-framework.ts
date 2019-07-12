f9fd363cbc60a6caf972bb23c5e56a8d0b0e1126
Ovidiu Cherecheș <skidding@gmail.com>
2019-07-12 18:12:14 +0300

    Upgrade major dep versions

--- .jest/setup-framework.ts
+++ .jest/setup-framework.ts
@@ -1,3 +1,3 @@
-import 'jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect';
 
 jest.setTimeout(20000);
--- packages/react-cosmos-playground2/src/@types/jest-dom.d.ts
+++ packages/react-cosmos-playground2/src/@types/jest-dom.d.ts
@@ -1,1 +1,1 @@
-import 'jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect';
