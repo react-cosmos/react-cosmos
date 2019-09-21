fa58f9b8c75aa4efabe569a971773ea11118ae4c
Ovidiu Cherecheș <skidding@gmail.com>
2019-09-21 12:18:57 +0300

    Update StringItem.tsx

--- packages/react-cosmos-playground2/src/shared/ui/valueInputTree/ValueInputTreeItem/StringItem.tsx
+++ packages/react-cosmos-playground2/src/shared/ui/valueInputTree/ValueInputTreeItem/StringItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { blue, grey248, grey8 } from '../../colors';
 import {
   TextContainer,
   TextField,
@@ -7,7 +8,6 @@ import {
 } from '../../inputs/shared';
 import { useFocus } from '../../useFocus';
 import { Label, ValueContainer } from './shared';
-import { grey8, blue } from '../../colors';
 
 type Props = {
   id: string;
@@ -47,7 +47,7 @@ export function StringItem({ id, label, value, onChange }: Props) {
               id={id}
               value={value}
               focused={focused}
-              color="var(--grey7)"
+              color={grey248}
               onChange={onInputChange}
               onFocus={onFocus}
               onBlur={onBlur}
