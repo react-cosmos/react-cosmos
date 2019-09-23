53704207f515912ce73dea901b5746d3e6a89ae3
Ovidiu Cherecheș <skidding@gmail.com>
2019-09-23 22:11:29 +0300

    Update inception.fixture.tsx

--- packages/react-cosmos-playground2/src/plugins/inception.fixture.tsx
+++ packages/react-cosmos-playground2/src/plugins/inception.fixture.tsx
@@ -2,6 +2,5 @@ import React from 'react';
 import { Slot } from 'react-plugin';
 
 export default () => {
-  // Force plugins to reload on HMR
-  return <Slot key={Date.now()} name="root" />;
+  return <Slot name="root" />;
 };
