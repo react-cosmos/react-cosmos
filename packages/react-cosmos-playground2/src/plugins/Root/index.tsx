0d033ac7740b57e31192dc63b2520e92eee1c0f5
Ovidiu Cherecheș <skidding@gmail.com>
2019-09-30 14:18:16 +0300

    s/Layout/Root

--- packages/react-cosmos-playground2/src/plugins/Root/index.tsx
+++ packages/react-cosmos-playground2/src/plugins/Root/index.tsx
@@ -4,12 +4,12 @@ import { CoreSpec } from '../Core/public';
 import { RendererCoreSpec } from '../RendererCore/public';
 import { RouterSpec } from '../Router/public';
 import { StorageSpec } from '../Storage/public';
-import { Root } from './Layout';
 import { isNavOpen, openNav } from './navOpen';
 import { getNavWidthApi } from './navWidth';
 import { isPanelOpen, openPanel } from './panelOpen';
 import { getPanelWidthApi } from './panelWidth';
 import { RootSpec } from './public';
+import { Root } from './Root';
 import { RootContext } from './shared';
 
 const { onLoad, plug, register } = createPlugin<RootSpec>({
