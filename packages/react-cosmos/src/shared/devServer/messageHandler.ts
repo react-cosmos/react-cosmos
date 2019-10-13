49d905a3fa94bca7454fa4c8644cdc8f4a83a2c0
Ovidiu Cherecheș <skidding@gmail.com>
2019-10-13 16:58:15 +0300

    Remove (unused) debug package

--- packages/react-cosmos/src/shared/devServer/messageHandler.ts
+++ packages/react-cosmos/src/shared/devServer/messageHandler.ts
@@ -1,5 +1,4 @@
 import http from 'http';
-import debug from 'debug';
 import ioServer from 'socket.io';
 import {
   BuildMessage,
@@ -7,25 +6,19 @@ import {
 } from 'react-cosmos-shared2/build';
 import { RENDERER_MESSAGE_EVENT_NAME } from 'react-cosmos-shared2/renderer';
 
-const d = debug('cosmos:message');
-
 export function createMessageHandler(httpServer: http.Server) {
-  d('init');
   const io = ioServer(httpServer);
 
   io.on('connection', socket => {
-    d('connection');
     // Forward commands between connected clients. Parties involved can be the
     // - The Cosmos UI, which acts as a remote control
     // - The web iframe or the React Native component renderers
     socket.on(RENDERER_MESSAGE_EVENT_NAME, msg => {
-      d('on renderer message %o', msg);
       socket.broadcast.emit(RENDERER_MESSAGE_EVENT_NAME, msg);
     });
   });
 
   function sendMessage(msg: BuildMessage) {
-    d('send server message %o', msg);
     io.emit(SERVER_MESSAGE_EVENT_NAME, msg);
   }
 
