59dafdbd207b3076cf733428a74e2e6082d1cab8
Ovidiu Cherecheș <skidding@gmail.com>
2019-10-21 17:08:02 +0300

    Make default renderer background work well with bright designs (#1076)

--- packages/react-cosmos-playground2/src/global/style.ts
+++ packages/react-cosmos-playground2/src/global/style.ts
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 import { fontFamily } from '../shared/ui/vars';
+import { grey8, white3 } from './../shared/ui/colors';
 
 export const GlobalStyle = createGlobalStyle`
   html {
@@ -22,4 +23,29 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   a {}
+`;
+
+export const CosmosGlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${grey8};
+    /* Black checkerboard effect on background */
+    background-image: linear-gradient(
+        45deg,
+        ${white3} 25%,
+        transparent 25%,
+        transparent 75%,
+        ${white3} 75%,
+        ${white3} 100%
+      ),
+      linear-gradient(
+        45deg,
+        ${white3} 25%,
+        transparent 25%,
+        transparent 75%,
+        ${white3} 75%,
+        ${white3} 100%
+      );
+    background-size: 32px 32px;
+    background-position: 0 0, 16px 16px;
+  }
 `;
--- packages/react-cosmos-playground2/src/plugins/RendererPreview/RendererPreview.tsx
+++ packages/react-cosmos-playground2/src/plugins/RendererPreview/RendererPreview.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Slot } from 'react-plugin';
 import styled from 'styled-components';
-import { grey8, white3 } from '../../shared/ui/colors';
 
 export type OnIframeRef = (elRef: null | HTMLIFrameElement) => void;
 
@@ -35,26 +34,8 @@ export const RendererPreview = React.memo(function RendererPreview({
 const Container = styled.div`
   width: 100%;
   height: 100%;
-  background-color: ${grey8};
-  /* Checkerboard effect on background */
-  background-image: linear-gradient(
-      45deg,
-      ${white3} 25%,
-      transparent 25%,
-      transparent 75%,
-      ${white3} 75%,
-      ${white3} 100%
-    ),
-    linear-gradient(
-      45deg,
-      ${white3} 25%,
-      transparent 25%,
-      transparent 75%,
-      ${white3} 75%,
-      ${white3} 100%
-    );
-  background-size: 32px 32px;
-  background-position: 0 0, 16px 16px;
+  background-color: #fff;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%23f1f1f1' fill-opacity='0.4'%3E%3Cpath opacity='.5' d='M96 95h4v1h-4v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9zm-1 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9z'/%3E%3Cpath d='M6 5V0H5v5H0v1h5v94h1V6h94V5H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
 `;
 
 const Iframe = styled.iframe`
