0dddbdf324a68d4deae1b2ceb3bb9836011fc152
LogKal <Lkaltz@ltu.edu>
2019-10-23 10:16:22 -0500

    Added optional advanced httpProxy config options

--- packages/react-cosmos/src/plugins/httpProxy.ts
+++ packages/react-cosmos/src/plugins/httpProxy.ts
@@ -7,13 +7,28 @@ import { DevServerPluginArgs } from '../shared/devServer';
 // Supporting all httpProxy options isn't as easy anymore, because the Cosmos
 // config is now JSON. We can either support a serializable subset of useful
 // options (like "pathRewrite") or optionally support an external config module.
-type HttpProxyConfig = { [context: string]: string };
+
+type HttpProxyConfig = {
+  [context: string]:
+    | string
+    | {
+        target: string;
+        secure?: boolean;
+        pathRewrite?: { [rewrite: string]: string };
+        logLevel?: string;
+      };
+};
 
 export function httpProxy({ cosmosConfig, expressApp }: DevServerPluginArgs) {
   const httpProxyConfig = getHttpProxyCosmosConfig(cosmosConfig);
   Object.keys(httpProxyConfig).forEach(context => {
-    const target = httpProxyConfig[context];
-    expressApp.use(context, httpProxyMiddleware(context, { target }));
+    if (typeof httpProxyConfig[context] === 'string') {
+      const target = httpProxyConfig[context] as string;
+      expressApp.use(context, httpProxyMiddleware(context, { target: target }));
+    } else if (typeof httpProxyConfig[context] === 'object') {
+      const proxy = httpProxyConfig[context] as object;
+      expressApp.use(context, httpProxyMiddleware(proxy));
+    }
   });
 }
 
