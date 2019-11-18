5d44fbce5925f28557ef9c95820bc2364a557018
Ovidiu Cherecheș <skidding@gmail.com>
2019-11-18 20:58:48 +0200

    Create website webpack config and index.html

---
+++ website/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const src = path.join(__dirname, 'src');
+
+const env = process.env.NODE_ENV || 'development';
+
+module.exports = {
+  mode: env,
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, include: src, loader: 'babel-loader' },
+      { test: /\.css$/, include: src, loader: 'style-loader!css-loader' }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(src, 'index.html')
+    })
+  ]
+};
