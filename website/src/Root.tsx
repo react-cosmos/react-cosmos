0a814c832220614302dbe157ba2ef5e227b72ee2
Ovidiu Cherecheș <skidding@gmail.com>
2019-11-27 23:12:35 +0200

    Style "open platform" feature

--- website/src/Root.tsx
+++ website/src/Root.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { ComponentLibraryPreview } from './ComponentLibraryPreview';
 import { Header } from './Header/Header';
-import { HEADER_HEIGHT_PX, MAX_CONTENT_WIDTH_PX } from './shared';
+import { MAX_CONTENT_WIDTH_PX } from './shared';
 import { HEADER_SCROLL_LENGTH_PX, useHeaderScroll } from './useHeaderScroll';
 import { useWindowViewport } from './useWindowViewport';
 import { useWindowYScroll } from './useWindowYScroll';
@@ -22,65 +22,67 @@ export function Root() {
         minimizeRatio={minimizeRatio}
       />
       <Content>
-        <Features style={{ opacity: showContent ? 1 : 0 }}>
-          <Feature>
-            <FeaturePreviewContainer>
-              <VisualTddPreview />
-            </FeaturePreviewContainer>
-            <FeatureTextOverlay>
-              <FeatureTitle>Visual TDD</FeatureTitle>
-              <FeatureDescription>
-                Develop one component at a time. Isolate the UI you&apos;re
-                working on and iterate quickly. Reloading your whole app on
-                every change is slowing you down!
-              </FeatureDescription>
-            </FeatureTextOverlay>
-          </Feature>
-          <Feature>
-            <FeaturePreviewContainer>
-              <ComponentLibraryPreview />
-            </FeaturePreviewContainer>
-            <FeatureTextOverlay>
-              <FeatureTitle>Component library</FeatureTitle>
-              <FeatureDescription>
-                From blank states to edge cases, define component states to come
-                back to. Your component library keeps you organized and provides
-                a solid foundation of test cases.
-              </FeatureDescription>
-            </FeatureTextOverlay>
-          </Feature>
-          <Feature>
-            <FeaturePreviewContainer></FeaturePreviewContainer>
-            <FeatureTextOverlay>
-              <FeatureTitle>Open platform</FeatureTitle>
-              <FeatureDescription>
-                React Cosmos can be used in powerful ways. Snapshot and visual
-                regression tests are possible, as well as custom integrations
-                tailored to your needs.
-              </FeatureDescription>
-            </FeatureTextOverlay>
-          </Feature>
-        </Features>
+        <Center>
+          <Features style={{ opacity: showContent ? 1 : 0 }}>
+            <Feature>
+              <FeaturePreviewContainer>
+                <VisualTddPreview />
+              </FeaturePreviewContainer>
+              <DarkFeatureTextOverlay>
+                <FeatureTitle>Visual TDD</FeatureTitle>
+                <FeatureDescription>
+                  Develop one component at a time. Isolate the UI you&apos;re
+                  working on and iterate quickly. Reloading your whole app on
+                  every change is slowing you down!
+                </FeatureDescription>
+              </DarkFeatureTextOverlay>
+            </Feature>
+            <Feature>
+              <FeaturePreviewContainer>
+                <ComponentLibraryPreview />
+              </FeaturePreviewContainer>
+              <DarkFeatureTextOverlay>
+                <FeatureTitle>Component library</FeatureTitle>
+                <FeatureDescription>
+                  From blank states to edge cases, define component states to
+                  come back to. Your component library keeps you organized and
+                  provides a solid foundation of test cases.
+                </FeatureDescription>
+              </DarkFeatureTextOverlay>
+            </Feature>
+            <Feature>
+              <OpenPlatformPreview y={(yScroll / 2) % 200} />
+              <LightFeatureTextOverlay>
+                <FeatureTitle>Open platform</FeatureTitle>
+                <FeatureDescription>
+                  React Cosmos can be used in powerful ways. Snapshot and visual
+                  regression tests are possible, as well as custom integrations
+                  tailored to your needs.
+                </FeatureDescription>
+              </LightFeatureTextOverlay>
+            </Feature>
+          </Features>
+        </Center>
       </Content>
     </Container>
   );
 }
 
-const SECTION_PADDING_PX = 128;
-
 const Container = styled.div<{ white: boolean }>`
   padding-top: ${HEADER_SCROLL_LENGTH_PX}px;
-  padding-bottom: 50vh;
   background: ${props => (props.white ? '#fff' : '#093556')};
   color: #0a2e46;
 `;
 
 const Content = styled.div`
-  max-width: ${MAX_CONTENT_WIDTH_PX}px;
   box-sizing: border-box;
-  margin: 0 auto;
-  padding: ${HEADER_HEIGHT_PX + 256}px 0 0 0;
+  padding: 50vh 0;
   line-height: 1.5em;
+`;
+
+const Center = styled.div`
+  max-width: ${MAX_CONTENT_WIDTH_PX}px;
+  margin: 0 auto;
 `;
 
 const Features = styled.div`
@@ -93,7 +95,7 @@ const Features = styled.div`
 const Feature = styled.div`
   width: 100%;
   height: 600px;
-  margin-bottom: ${SECTION_PADDING_PX}px;
+  margin-bottom: 10vh;
   position: relative;
 `;
 
@@ -103,13 +105,23 @@ const FeaturePreviewContainer = styled.div`
   background-color: #e1e1e1;
 `;
 
-const FeatureTextOverlay = styled.div`
+const OpenPlatformPreview = styled(FeaturePreviewContainer)<{ y: number }>`
+  background-color: #093556;
+  background-image: url('/space-pattern.png');
+  background-repeat: repeat;
+  background-position: center center;
+  background-size: 200px 200px;
+  background-attachment: fixed;
+`;
+
+const DarkFeatureTextOverlay = styled.div`
   position: absolute;
   bottom: 0;
   left: 0;
   right: 0;
   background: rgba(0, 0, 0, 0.7);
   backdrop-filter: blur(20px);
+  color: #fff;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -117,10 +129,15 @@ const FeatureTextOverlay = styled.div`
   text-align: center;
 `;
 
+const LightFeatureTextOverlay = styled(DarkFeatureTextOverlay)`
+  background: rgba(255, 255, 255, 0.8);
+  backdrop-filter: blur(5px);
+  color: #0a2e46;
+`;
+
 const FeatureTitle = styled.h2`
   margin: 0;
   padding: 32px 0 8px 0;
-  color: #fff;
   font-size: 36px;
   line-height: 36px;
   font-weight: 600;
@@ -130,7 +147,6 @@ const FeatureTitle = styled.h2`
 const FeatureDescription = styled.div`
   max-width: 640px;
   padding: 0 24px 32px 24px;
-  color: #fff;
   font-size: 24px;
   line-height: 30px;
   opacity: 0.9;
