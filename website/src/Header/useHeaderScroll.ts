0054593c470865c67b39d224c3716bf9ae6ccea3
Ovidiu Cherecheș <skidding@gmail.com>
2019-12-05 20:58:52 +0200

    Optimize header animation (#1092)

--- website/src/Header/useHeaderScroll.ts
+++ website/src/Header/useHeaderScroll.ts
@@ -1,20 +1,20 @@
 import React from 'react';
 import { useWindowYScroll } from './useWindowYScroll';
 
-export const HEADER_SCROLL_LENGTH_PX = 512;
 const HEADER_SCROLL_STEP_PX = 4;
 
-export function useHeaderScroll() {
+export function useHeaderScroll(windowHeight: number) {
   const yScroll = useWindowYScroll();
   const roundedYScroll = yScroll - (yScroll % HEADER_SCROLL_STEP_PX);
   return React.useMemo(() => {
-    const scrollRatio = getScrollRatio(roundedYScroll);
+    const scrollRatio = getScrollRatio(roundedYScroll, windowHeight);
     const cropRatio = Math.min(1, scrollRatio * 2);
     const minimizeRatio = Math.max(0, scrollRatio - 0.5) * 2;
     return { cropRatio, minimizeRatio };
-  }, [roundedYScroll]);
+  }, [roundedYScroll, windowHeight]);
 }
 
-function getScrollRatio(yScroll: number) {
-  return Math.min(1, Math.max(0, yScroll / HEADER_SCROLL_LENGTH_PX));
+function getScrollRatio(yScroll: number, windowHeight: number) {
+  const threshold = windowHeight * 0.4;
+  return Math.min(1, Math.max(0, yScroll / threshold));
 }
