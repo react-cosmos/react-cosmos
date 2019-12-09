9faac352c651a92cdd8e0f17637fbca322796c58
Ovidiu Cherecheș <skidding@gmail.com>
2019-12-09 14:55:04 +0200

    Fix edge case

--- packages/react-cosmos-playground2/src/shared/fixtureTree/collapseSoloIndexes.ts
+++ packages/react-cosmos-playground2/src/shared/fixtureTree/collapseSoloIndexes.ts
@@ -17,11 +17,12 @@ export function collapseSoloIndexes(treeNode: FixtureNode): FixtureNode {
   const dirs: FixtureNodeDirs = {};
 
   forEach(treeNode.dirs, (dirNode, dirName) => {
-    const dirItems = dirNode.items || {};
+    const dirItems = dirNode.items;
     const containsSoloIndexItem =
       Object.keys(dirItems).length === 1 && dirItems.index;
+    const hasSubdirs = Object.keys(dirNode.dirs).length > 0;
 
-    if (containsSoloIndexItem && !items[dirName]) {
+    if (containsSoloIndexItem && !items[dirName] && !hasSubdirs) {
       items[dirName] = dirItems.index;
     } else {
       dirs[dirName] = collapseSoloIndexes(dirNode);
