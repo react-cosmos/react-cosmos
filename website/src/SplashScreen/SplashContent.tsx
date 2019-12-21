7a71071dc313b7da6f4ab62ad39089670d5ea97e
Ovidiu Cherecheș <skidding@gmail.com>
2019-12-21 19:15:44 +0200

    Add links to docs in header

--- website/src/SplashScreen/SplashContent.tsx
+++ website/src/SplashScreen/SplashContent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { ExternalLink } from '../shared/ExternalLink';
-import { slideInTransition } from '../shared/slideIn';
+import { getSlideInStyle, slideInTransition } from '../shared/slideIn';
 import { getCosmonautSize, Viewport } from '../shared/viewport';
 
 type Props = {
@@ -10,7 +10,10 @@ type Props = {
 };
 
 export function SplashContent({ windowViewport, gitHubStars }: Props) {
-  const containerStyle = getContainerStyle(windowViewport);
+  const containerStyle = React.useMemo(
+    () => getContainerStyle(windowViewport, gitHubStars),
+    [gitHubStars, windowViewport]
+  );
 
   return (
     <Container style={containerStyle}>
@@ -21,27 +24,30 @@ export function SplashContent({ windowViewport, gitHubStars }: Props) {
         Introducing <strong>React Cosmos 5</strong>
         <br />a tool for ambitious UI developers
       </Subtitle>
-      <CallToAction
-        href="https://github.com/react-cosmos/react-cosmos"
-        style={{
-          opacity: gitHubStars === null ? 0 : 1,
-          transform: `scale(${gitHubStars === null ? 0.8 : 1})`
-        }}
-      >
+      <CallToAction href="https://github.com/react-cosmos/react-cosmos">
         <strong>GitHub</strong>
         <Star />
         {gitHubStars}
       </CallToAction>
+      <DocsLink href="https://github.com/react-cosmos/react-cosmos/blob/master/README.md#table-of-contents">
+        <span>Docs</span>
+        <Chevron />
+      </DocsLink>
     </Container>
   );
 }
 
-function getContainerStyle(windowViewport: Viewport) {
+function getContainerStyle(
+  windowViewport: Viewport,
+  gitHubStars: null | number
+) {
   const cosmonautSize = Math.round(getCosmonautSize(windowViewport));
   const fontSize = Math.round(cosmonautSize / 18);
+  const slideInStyle = getSlideInStyle(gitHubStars !== null);
 
   if (isPortrait(windowViewport)) {
     return {
+      ...slideInStyle,
       bottom: cosmonautSize,
       left: 0,
       width: windowViewport.width,
@@ -51,6 +57,7 @@ function getContainerStyle(windowViewport: Viewport) {
   }
 
   return {
+    ...slideInStyle,
     bottom: 0,
     left: cosmonautSize,
     width: windowViewport.width - cosmonautSize,
@@ -70,7 +77,7 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   color: #fff;
-  transition: opacity 0.4s;
+  transition: ${slideInTransition};
 `;
 
 const Title = styled.h1`
@@ -107,12 +114,12 @@ const CallToAction = styled(ExternalLink)`
   background: #b1dcfd;
   color: #0a2e46;
   font-size: calc(10px + 1em);
+  font-weight: 400;
   line-height: 2.3em;
   display: flex;
   flex-direction: row;
   align-items: center;
   text-decoration: none;
-  transition: ${slideInTransition};
 
   strong {
     font-weight: 500;
@@ -136,7 +143,43 @@ const Star = () => {
 const StyledStar = styled.svg`
   width: 0.8em;
   height: 0.8em;
-  stroke-width: calc(2px + 0.01em);
+  stroke-width: calc(2px + 0.005em);
   margin: 0 0.1em 0 0.5em;
   transform: translate(0, 4%);
+`;
+
+const DocsLink = styled(ExternalLink)`
+  margin: 0.5em 0 0 0;
+  padding: 0 1em;
+  color: #b1dcfd;
+  font-size: calc(10px + 0.8em);
+  font-weight: 400;
+  line-height: 2.3em;
+  text-decoration: none;
+  white-space: nowrap;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const Chevron = () => {
+  return (
+    <StyledChevron
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="square"
+      strokeLinejoin="round"
+    >
+      <polyline points="9 18 15 12 9 6"></polyline>
+    </StyledChevron>
+  );
+};
+
+const StyledChevron = styled.svg`
+  width: 1em;
+  height: 1em;
+  stroke-width: calc(2px + 0.005em);
+  margin: 0 -0.4em 0 0em;
+  transform: translate(0, 9%);
 `;
