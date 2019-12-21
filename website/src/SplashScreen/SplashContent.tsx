6dc211ddf3e75f6e370277ddaf9c20d65495f3e8
Ovidiu Cherecheș <skidding@gmail.com>
2019-12-21 18:55:40 +0200

    Use CSS to scale Splash content

--- website/src/SplashScreen/SplashContent.tsx
+++ website/src/SplashScreen/SplashContent.tsx
@@ -9,100 +9,44 @@ type Props = {
   gitHubStars: null | number;
 };
 
-type FullScreenHeaderSizes = {
-  titleFontSize: number;
-  subtitleFontSize: number;
-  ctaMarginTop: number;
-  ctaHeight: number;
-  ctaPadding: number;
-  ctaFontSize: number;
-  starSize: number;
-  starStrokeWidth: number;
-  starLeftMargin: number;
-  starRightMargin: number;
-};
-
 export function SplashContent({ windowViewport, gitHubStars }: Props) {
   const containerStyle = getContainerStyle(windowViewport);
-  const {
-    titleFontSize,
-    subtitleFontSize,
-    ctaMarginTop,
-    ctaHeight,
-    ctaPadding,
-    ctaFontSize,
-    starSize,
-    starStrokeWidth,
-    starLeftMargin,
-    starRightMargin
-  } = getFullScreenHeaderSizes(windowViewport);
 
   return (
     <Container style={containerStyle}>
-      <Title style={{ fontSize: titleFontSize }}>
+      <Title>
         Build UIs at <em>scale</em>.
       </Title>
-      <Subtitle style={{ fontSize: subtitleFontSize }}>
+      <Subtitle>
         Introducing <strong>React Cosmos 5</strong>
         <br />a tool for ambitious UI developers
       </Subtitle>
       <CallToAction
         href="https://github.com/react-cosmos/react-cosmos"
         style={{
-          marginTop: ctaMarginTop,
-          padding: `0 ${ctaPadding}px`,
-          fontSize: ctaFontSize,
-          lineHeight: `${ctaHeight}px`,
           opacity: gitHubStars === null ? 0 : 1,
           transform: `scale(${gitHubStars === null ? 0.8 : 1})`
         }}
       >
         <strong>GitHub</strong>
-        <Star
-          size={starSize}
-          strokeWidth={starStrokeWidth}
-          leftMargin={starLeftMargin}
-          rightMargin={starRightMargin}
-        />
+        <Star />
         {gitHubStars}
       </CallToAction>
     </Container>
   );
 }
 
-function getFullScreenHeaderSizes(
-  windowViewport: Viewport
-): FullScreenHeaderSizes {
-  const cosmonautSize = getCosmonautSize(windowViewport);
-  const fontOffset = Math.round(cosmonautSize / 18);
-  const titleFontSize = 16 + Math.round(fontOffset * 2.4);
-  const subtitleFontSize = 10 + fontOffset;
-  const ctaFontSize = 10 + fontOffset;
-  const ctaHeight = Math.round(ctaFontSize * 2.3);
-
-  return {
-    titleFontSize,
-    subtitleFontSize,
-    ctaMarginTop: fontOffset * 4,
-    ctaPadding: ctaFontSize * 1,
-    ctaHeight,
-    ctaFontSize,
-    starSize: Math.round(subtitleFontSize * 0.75),
-    starStrokeWidth: Math.max(2, Math.ceil(subtitleFontSize / 30)),
-    starLeftMargin: Math.round(subtitleFontSize / 2),
-    starRightMargin: Math.round(subtitleFontSize / 10)
-  };
-}
-
 function getContainerStyle(windowViewport: Viewport) {
   const cosmonautSize = Math.round(getCosmonautSize(windowViewport));
+  const fontSize = Math.round(cosmonautSize / 18);
 
   if (isPortrait(windowViewport)) {
     return {
       bottom: cosmonautSize,
       left: 0,
       width: windowViewport.width,
-      height: windowViewport.height - cosmonautSize
+      height: windowViewport.height - cosmonautSize,
+      fontSize
     };
   }
 
@@ -110,7 +54,8 @@ function getContainerStyle(windowViewport: Viewport) {
     bottom: 0,
     left: cosmonautSize,
     width: windowViewport.width - cosmonautSize,
-    height: windowViewport.height
+    height: windowViewport.height,
+    fontSize
   };
 }
 
@@ -132,6 +77,7 @@ const Title = styled.h1`
   margin: 0;
   padding: 0;
   background: rgb(9, 53, 86, 0.8);
+  font-size: calc(16px + 2.4em);
   font-weight: 600;
   line-height: 1.5em;
   letter-spacing: -0.02em;
@@ -143,6 +89,7 @@ const Subtitle = styled.p`
   margin: 0;
   padding: 0;
   background: rgb(9, 53, 86, 0.8);
+  font-size: calc(10px + 1em);
   font-weight: 300;
   line-height: 1.6em;
   color: #b1dcfd;
@@ -155,8 +102,12 @@ const Subtitle = styled.p`
 `;
 
 const CallToAction = styled(ExternalLink)`
+  margin: 2.8em 0 0 0;
+  padding: 0 1em;
   background: #b1dcfd;
   color: #0a2e46;
+  font-size: calc(10px + 1em);
+  line-height: 2.3em;
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -168,28 +119,14 @@ const CallToAction = styled(ExternalLink)`
   }
 `;
 
-type StarProps = {
-  size: number;
-  strokeWidth: number;
-  leftMargin: number;
-  rightMargin: number;
-};
-
-const Star = ({ size, strokeWidth, leftMargin, rightMargin }: StarProps) => {
+const Star = () => {
   return (
     <StyledStar
-      xmlns="http://www.w3.org/2000/svg"
-      width={size}
-      height={size}
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth={strokeWidth}
       strokeLinecap="round"
       strokeLinejoin="round"
-      style={{
-        margin: `0 ${rightMargin}px 0 ${leftMargin}px`
-      }}
     >
       <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
     </StyledStar>
@@ -197,5 +134,9 @@ const Star = ({ size, strokeWidth, leftMargin, rightMargin }: StarProps) => {
 };
 
 const StyledStar = styled.svg`
-  transform: translate(0, 3%);
+  width: 0.8em;
+  height: 0.8em;
+  stroke-width: calc(2px + 0.01em);
+  margin: 0 0.1em 0 0.5em;
+  transform: translate(0, 4%);
 `;
