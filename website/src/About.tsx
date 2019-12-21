dda830d73ed62c87781ed5788c056082e798bd75
Ovidiu Cherecheș <skidding@gmail.com>
2019-12-21 14:45:20 +0200

    Redesign header actions

--- website/src/About.tsx
+++ website/src/About.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import { columnsWidthBreakpoint } from './shared/breakpoints';
 import { ExternalLink } from './shared/ExternalLink';
-import { Heart } from './shared/Heart';
 import { getSlideInStyle, slideInTransition } from './shared/slideIn';
 import { Center } from './shared/styledPrimitives';
 import { useViewportEnter } from './shared/useViewportEnter';
@@ -149,3 +148,11 @@ const CallToAction = styled(ExternalLink)`
     }
   }
 `;
+
+function Heart() {
+  return (
+    <svg viewBox="0 0 24 24" fill="currentColor">
+      <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
+    </svg>
+  );
+}
--- website/src/shared/Heart.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function Heart() {
-  return (
-    <svg viewBox="0 0 24 24">
-      <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
-    </svg>
-  );
-}
