2d573c9496bf8f0ec046fd4770b03d4e8e7520fb
Ovidiu Cherecheș <skidding@gmail.com>
2019-12-30 02:09:35 +0200

    Add version with link to releases in website header

--- website/src/Header/Header.tsx
+++ website/src/Header/Header.tsx
@@ -12,6 +12,7 @@ type Props = {
 };
 
 export function Header({ visible, fixed }: Props) {
+  const version = getVersion();
   return (
     <Container
       style={{
@@ -32,20 +33,30 @@ export function Header({ visible, fixed }: Props) {
             <ExternalLink href="https://github.com/react-cosmos/react-cosmos/blob/master/README.md#table-of-contents">
               Docs
             </ExternalLink>
-            <Separator>/</Separator>
+            <LinkSeparator>/</LinkSeparator>
             <InternalLink to="/demo">Demo</InternalLink>
-            <Separator>/</Separator>
+            <LinkSeparator>/</LinkSeparator>
             <ExternalLink href="https://github.com/sponsors/skidding">
               Sponsor
             </ExternalLink>
           </Links>
         </Body>
-        <GitHubButton href="https://github.com/react-cosmos/react-cosmos">
-          <GitHub />
-        </GitHubButton>
+        <RightSide>
+          <ReleasesLink href="https://github.com/react-cosmos/react-cosmos/releases">
+            v{version}
+          </ReleasesLink>
+          <GitHubLink href="https://github.com/react-cosmos/react-cosmos">
+            GitHub
+          </GitHubLink>
+        </RightSide>
       </Content>
     </Container>
   );
+}
+
+function getVersion(): string {
+  // @ts-ignore
+  return typeof RC_VERSION === 'string' ? RC_VERSION : '5.0.0';
 }
 
 const Container = styled.div`
@@ -86,37 +97,6 @@ const CosmonautButton = styled(InternalLink)`
   cursor: pointer;
 `;
 
-const GitHubButton = styled(ExternalLink)`
-  flex-shrink: 0;
-  width: 64px;
-  height: 64px;
-  margin: 0 8px 0 0;
-  color: rgba(10, 46, 70, 0.9);
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-
-  svg {
-    display: block;
-    width: 64px;
-    height: 64px;
-  }
-
-  @media (max-width: ${centerHeaderBreakpoint}px) {
-    display: none;
-  }
-`;
-
-export function GitHub() {
-  return (
-    <svg viewBox="0 0 24 24" fill="currentColor" stroke="none">
-      <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
-    </svg>
-  );
-}
-
 const Body = styled.div`
   flex: 1;
   height: 56px;
@@ -149,12 +129,10 @@ const Title = styled.h1`
 const Links = styled.div`
   display: flex;
   flex-direction: row;
-  font-size: 16px;
   line-height: 20px;
 
   a {
     color: inherit;
-    font-weight: 400;
     text-decoration: none;
     opacity: 0.9;
 
@@ -164,8 +142,49 @@ const Links = styled.div`
   }
 `;
 
-const Separator = styled.span`
+const LinkSeparator = styled.span`
   margin: 0 8px;
   font-weight: 300;
   opacity: 0.5;
+`;
+
+const RightSide = styled.div`
+  flex-shrink: 0;
+  width: 64px;
+  height: 56px;
+  margin: 0 8px 0 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+
+  @media (max-width: ${centerHeaderBreakpoint}px) {
+    display: none;
+  }
+`;
+
+const ReleasesLink = styled(ExternalLink)`
+  background: rgba(10, 46, 70, 0.08);
+  color: inherit;
+  border-radius: 3px;
+  margin: 0 0 8px 0;
+  padding: 0 8px;
+  text-decoration: none;
+  font-weight: 500;
+  line-height: 28px;
+
+  :hover {
+    text-decoration: underline;
+  }
+`;
+
+const GitHubLink = styled(ExternalLink)`
+  color: inherit;
+  line-height: 20px;
+  text-decoration: none;
+  opacity: 0.9;
+
+  :hover {
+    text-decoration: underline;
+  }
 `;
--- website/webpack.config.js
+++ website/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const src = path.join(__dirname, 'src');
 const dist = path.join(__dirname, 'dist');
 
 const env = process.env.NODE_ENV || 'development';
+const { version } = require('../lerna.json');
 
 module.exports = {
   mode: env,
@@ -21,13 +23,18 @@ module.exports = {
       { test: /\.tsx?$/, include: [src], loader: 'babel-loader' },
       { test: /\.css$/, include: src, loader: 'style-loader!css-loader' }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      RC_VERSION: JSON.stringify(version)
+    })
+  ]
 };
 
 if (env === 'development') {
-  module.exports.plugins = [
+  module.exports.plugins.push(
     new HtmlWebpackPlugin({
       template: path.join(src, 'index.dev.html')
     })
-  ];
+  );
 }
