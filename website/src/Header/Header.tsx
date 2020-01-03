a3f952604a0a89b6576cd12ec50b07c8146fea04
Joonas Kallunki <jkallunki@gmail.com>
2020-01-03 14:27:17 +0200

    Fix website accessibility issues (#1123)

--- website/src/Header/Header.tsx
+++ website/src/Header/Header.tsx
@@ -23,7 +23,7 @@ export function Header({ visible, fixed }: Props) {
     >
       <Content>
         <CosmonautContainer>
-          <CosmonautButton to="/" />
+          <CosmonautButton to="/" visuallyOnly />
         </CosmonautContainer>
         <Body>
           <Title>
