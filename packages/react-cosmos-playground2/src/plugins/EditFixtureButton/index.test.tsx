7865beffe95dfcbf352239626a9cef46cd824d71
Ovidiu Cherecheș <skidding@gmail.com>
2020-01-15 23:08:38 +0200

    Add custom notification when trying to edit fixture in static export (#1146)

--- packages/react-cosmos-playground2/src/plugins/EditFixtureButton/index.test.tsx
+++ packages/react-cosmos-playground2/src/plugins/EditFixtureButton/index.test.tsx
@@ -4,6 +4,7 @@ import { loadPlugins, resetPlugins } from 'react-plugin';
 import { register } from '.';
 import { RendererActionSlot } from '../../shared/slots/RendererActionSlot';
 import { mockCore, mockNotifications } from '../../testHelpers/pluginMocks';
+import { Commands } from '../Core/public';
 import { mockFetch } from './testHelpers';
 
 afterEach(resetPlugins);
@@ -29,6 +30,33 @@ it(`doesn't render button when dev server is off`, async () => {
 
   const { queryByTitle } = await loadTestPlugins();
   expect(queryByTitle(/open fixture source/i)).toBeNull();
+});
+
+it(`shows error notification when dev server is off`, async () => {
+  register();
+  let registeredCommands: Commands = {};
+  mockCore({
+    isDevServerOn: () => false,
+    registerCommands: (context, commands) => {
+      registeredCommands = commands;
+      return () => {
+        registeredCommands = {};
+      };
+    }
+  });
+  const { pushTimedNotification } = mockNotifications();
+
+  await loadTestPlugins();
+  registeredCommands.editFixture();
+
+  await wait(() =>
+    expect(pushTimedNotification).toBeCalledWith(expect.any(Object), {
+      id: expect.any(String),
+      type: 'error',
+      title: 'Failed to open fixture',
+      info: 'Static exports cannot access source files.'
+    })
+  );
 });
 
 it('renders button', async () => {
