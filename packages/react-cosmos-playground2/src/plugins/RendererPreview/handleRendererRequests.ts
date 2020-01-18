c0dedd67cfbbb241068d248053f580f0c7bcdf23
Ovidiu Cherecheș <skidding@gmail.com>
2020-01-18 19:01:11 +0200

    Clear notification when renderer iframe location reverts (#1147)

--- packages/react-cosmos-playground2/src/plugins/RendererPreview/handleRendererRequests.ts
+++ packages/react-cosmos-playground2/src/plugins/RendererPreview/handleRendererRequests.ts
@@ -46,14 +46,18 @@ export function createRendererRequestHandler() {
         onIframeLoad(e: Event) {
           const iframe = e.target as HTMLIFrameElement;
           const iframeWindow = iframe.contentWindow;
-          if (iframeWindow && iframeLocationChanged(iframeWindow, iframe.src)) {
+          if (iframeWindow) {
             const notifications = getNotificationMethods(pluginContext);
-            notifications.pushStickyNotification({
-              id: notificationId,
-              type: 'info',
-              title: 'Renderer iframe location changed',
-              info: `Reload or select another fixture to reset your preview.`
-            });
+            if (iframeLocationChanged(iframeWindow, iframe.src)) {
+              notifications.pushStickyNotification({
+                id: notificationId,
+                type: 'info',
+                title: 'Renderer iframe location changed',
+                info: `Reload or select another fixture to reset your preview.`
+              });
+            } else {
+              notifications.removeStickyNotification(notificationId);
+            }
           }
         }
       };
