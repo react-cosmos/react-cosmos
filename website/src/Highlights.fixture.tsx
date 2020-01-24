2df571d8983b0b04adfcc67f6ccdea8492ef6e6f
Ovidiu Cherecheș <skidding@gmail.com>
2020-01-25 00:00:44 +0200

    Add feature highlights to website (#1151)

---
+++ website/src/Highlights.fixture.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Highlights } from './Highlights';
+
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  min-height: 100%;
+  background: #fff;
+  padding: 64px 0;
+`;
+
+export default (
+  <Container>
+    <Highlights />
+  </Container>
+);
