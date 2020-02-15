5f5fbe50a658cd87f50b16ff6226d377d265d5a9
Ovidiu Cherecheș <skidding@gmail.com>
2020-02-15 20:10:38 +0200

    Collapse single child dirs recursively in fixture tree (#1164)

--- packages/react-cosmos-playground2/src/shared/fixtureTree/hideSingleChildDirs.ts
+++ packages/react-cosmos-playground2/src/shared/fixtureTree/hideSingleChildDirs.ts
@@ -1,13 +1,29 @@
+import { forEach } from 'lodash';
+import { FixtureId } from 'react-cosmos-shared2/renderer';
+import { TreeNodeDirs } from '../tree';
 import { FixtureNode } from './shared';
 
-export function hideSingleChildDirs(treeNode: FixtureNode): FixtureNode {
-  const { items, dirs } = treeNode;
-  const dirNames = Object.keys(dirs);
+export function hideSingleChildDirs(rootNode: FixtureNode): FixtureNode {
+  const { items, dirs } = rootNode;
 
-  const hasSingleChild = Object.keys(items).length == 0 && dirNames.length == 1;
-  if (hasSingleChild) {
+  const dirNames = Object.keys(dirs);
+  if (containsSingleDir(rootNode)) {
     return hideSingleChildDirs(dirs[dirNames[0]]);
   }
 
-  return { items, dirs };
+  const newDirs: TreeNodeDirs<FixtureId> = {};
+  forEach(dirs, (dir, dirName) => {
+    const subDirNames = Object.keys(dir.dirs);
+    if (containsSingleDir(dir) && !newDirs[subDirNames[0]]) {
+      newDirs[subDirNames[0]] = hideSingleChildDirs(dir.dirs[subDirNames[0]]);
+    } else {
+      newDirs[dirName] = hideSingleChildDirs(dir);
+    }
+  });
+
+  return { items, dirs: newDirs };
+}
+
+function containsSingleDir({ items, dirs }: FixtureNode) {
+  return Object.keys(items).length == 0 && Object.keys(dirs).length == 1;
 }
