815a43864896371d0a05db97a6c28d59ad0d05bc
Ovidiu Cherecheș <skidding@gmail.com>
2020-04-11 15:33:53 +0300

    Only sort tree nodes alphabetically

--- packages/react-cosmos-shared2/src/fixtureTree/getSortedNodeDirNames.ts
+++ packages/react-cosmos-shared2/src/fixtureTree/getSortedNodeDirNames.ts
@@ -1,22 +1,10 @@
-import { TreeNode, TreeNodes } from './shared/types';
+import { TreeNodes } from './shared/types';
 
 export function getSortedNodeDirNames(nodeDirs: TreeNodes<any>): string[] {
   return (
     Object.keys(nodeDirs)
       .slice()
-      // Sort alphabetically first
+      // Sort alphabetically
       .sort()
-      .sort((dirName1, dirName2) => {
-        return (
-          calcNodeDepth(nodeDirs[dirName2]) - calcNodeDepth(nodeDirs[dirName1])
-        );
-      })
   );
-}
-
-// Only differentiate between nodes with and without subdirs and ignore
-// depth level in the latter
-function calcNodeDepth(node: TreeNode<any>): 0 | 1 {
-  const hasDirs = Object.keys(node.dirs).length > 0;
-  return hasDirs ? 1 : 0;
 }
