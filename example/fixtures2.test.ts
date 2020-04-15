5477ff9931adb8651809740bbe215c1899658fb9
Ovidiu Cherecheș <skidding@gmail.com>
2020-04-15 14:40:33 +0300

    Fix inverted fixture URLs (#1183)

--- example/fixtures2.test.ts
+++ example/fixtures2.test.ts
@@ -10,11 +10,11 @@ it('returns fixture info', async () => {
       fileName: 'index',
       getElement: expect.any(Function),
       name: 'default',
-      relativeFilePath: 'Counter/index.fixture.tsx',
       playgroundUrl:
-        'http://localhost:5000/_renderer.html?_fixtureId=%7B%22path%22%3A%22Counter%2Findex.fixture.tsx%22%2C%22name%22%3A%22default%22%7D',
-      rendererUrl:
         'http://localhost:5000/?fixtureId=%7B%22path%22%3A%22Counter%2Findex.fixture.tsx%22%2C%22name%22%3A%22default%22%7D',
+      relativeFilePath: 'Counter/index.fixture.tsx',
+      rendererUrl:
+        'http://localhost:5000/_renderer.html?_fixtureId=%7B%22path%22%3A%22Counter%2Findex.fixture.tsx%22%2C%22name%22%3A%22default%22%7D',
       treePath: ['Counter', 'default']
     },
     {
@@ -22,11 +22,11 @@ it('returns fixture info', async () => {
       fileName: 'index',
       getElement: expect.any(Function),
       name: 'small number',
-      relativeFilePath: 'Counter/index.fixture.tsx',
       playgroundUrl:
-        'http://localhost:5000/_renderer.html?_fixtureId=%7B%22path%22%3A%22Counter%2Findex.fixture.tsx%22%2C%22name%22%3A%22small%20number%22%7D',
-      rendererUrl:
         'http://localhost:5000/?fixtureId=%7B%22path%22%3A%22Counter%2Findex.fixture.tsx%22%2C%22name%22%3A%22small%20number%22%7D',
+      relativeFilePath: 'Counter/index.fixture.tsx',
+      rendererUrl:
+        'http://localhost:5000/_renderer.html?_fixtureId=%7B%22path%22%3A%22Counter%2Findex.fixture.tsx%22%2C%22name%22%3A%22small%20number%22%7D',
       treePath: ['Counter', 'small number']
     },
     {
@@ -34,11 +34,11 @@ it('returns fixture info', async () => {
       fileName: 'index',
       getElement: expect.any(Function),
       name: 'large number',
-      relativeFilePath: 'Counter/index.fixture.tsx',
       playgroundUrl:
-        'http://localhost:5000/_renderer.html?_fixtureId=%7B%22path%22%3A%22Counter%2Findex.fixture.tsx%22%2C%22name%22%3A%22large%20number%22%7D',
-      rendererUrl:
         'http://localhost:5000/?fixtureId=%7B%22path%22%3A%22Counter%2Findex.fixture.tsx%22%2C%22name%22%3A%22large%20number%22%7D',
+      relativeFilePath: 'Counter/index.fixture.tsx',
+      rendererUrl:
+        'http://localhost:5000/_renderer.html?_fixtureId=%7B%22path%22%3A%22Counter%2Findex.fixture.tsx%22%2C%22name%22%3A%22large%20number%22%7D',
       treePath: ['Counter', 'large number']
     },
     {
@@ -46,11 +46,11 @@ it('returns fixture info', async () => {
       fileName: 'Hello World',
       getElement: expect.any(Function),
       name: null,
-      relativeFilePath: '__fixtures__/Hello World.ts',
       playgroundUrl:
-        'http://localhost:5000/_renderer.html?_fixtureId=%7B%22path%22%3A%22__fixtures__%2FHello%20World.ts%22%2C%22name%22%3Anull%7D',
-      rendererUrl:
         'http://localhost:5000/?fixtureId=%7B%22path%22%3A%22__fixtures__%2FHello%20World.ts%22%2C%22name%22%3Anull%7D',
+      relativeFilePath: '__fixtures__/Hello World.ts',
+      rendererUrl:
+        'http://localhost:5000/_renderer.html?_fixtureId=%7B%22path%22%3A%22__fixtures__%2FHello%20World.ts%22%2C%22name%22%3Anull%7D',
       treePath: ['Hello World']
     },
     {
@@ -59,9 +59,9 @@ it('returns fixture info', async () => {
       getElement: expect.any(Function),
       name: null,
       relativeFilePath: '__fixtures__/Props Playground.tsx',
-      playgroundUrl:
-        'http://localhost:5000/_renderer.html?_fixtureId=%7B%22path%22%3A%22__fixtures__%2FProps%20Playground.tsx%22%2C%22name%22%3Anull%7D',
       rendererUrl:
+        'http://localhost:5000/_renderer.html?_fixtureId=%7B%22path%22%3A%22__fixtures__%2FProps%20Playground.tsx%22%2C%22name%22%3Anull%7D',
+      playgroundUrl:
         'http://localhost:5000/?fixtureId=%7B%22path%22%3A%22__fixtures__%2FProps%20Playground.tsx%22%2C%22name%22%3Anull%7D',
       treePath: ['Props Playground']
     },
@@ -71,46 +71,46 @@ it('returns fixture info', async () => {
       getElement: expect.any(Function),
       name: null,
       relativeFilePath: '__fixtures__/Values Playground.tsx',
-      playgroundUrl:
-        'http://localhost:5000/_renderer.html?_fixtureId=%7B%22path%22%3A%22__fixtures__%2FValues%20Playground.tsx%22%2C%22name%22%3Anull%7D',
       rendererUrl:
+        'http://localhost:5000/_renderer.html?_fixtureId=%7B%22path%22%3A%22__fixtures__%2FValues%20Playground.tsx%22%2C%22name%22%3Anull%7D',
+      playgroundUrl:
         'http://localhost:5000/?fixtureId=%7B%22path%22%3A%22__fixtures__%2FValues%20Playground.tsx%22%2C%22name%22%3Anull%7D',
       treePath: ['Values Playground']
     },
     {
+      absoluteFilePath: `${__dirname}/CounterButton/index.fixture.tsx`,
       fileName: 'index',
       getElement: expect.any(Function),
-      absoluteFilePath: `${__dirname}/CounterButton/index.fixture.tsx`,
       name: null,
-      relativeFilePath: 'CounterButton/index.fixture.tsx',
       playgroundUrl:
-        'http://localhost:5000/_renderer.html?_fixtureId=%7B%22path%22%3A%22CounterButton%2Findex.fixture.tsx%22%2C%22name%22%3Anull%7D',
-      rendererUrl:
         'http://localhost:5000/?fixtureId=%7B%22path%22%3A%22CounterButton%2Findex.fixture.tsx%22%2C%22name%22%3Anull%7D',
+      relativeFilePath: 'CounterButton/index.fixture.tsx',
+      rendererUrl:
+        'http://localhost:5000/_renderer.html?_fixtureId=%7B%22path%22%3A%22CounterButton%2Findex.fixture.tsx%22%2C%22name%22%3Anull%7D',
       treePath: ['CounterButton']
     },
     {
+      absoluteFilePath: `${__dirname}/NestedDecorators/index.fixture.tsx`,
       fileName: 'index',
       getElement: expect.any(Function),
-      absoluteFilePath: `${__dirname}/NestedDecorators/index.fixture.tsx`,
       name: null,
-      relativeFilePath: 'NestedDecorators/index.fixture.tsx',
       playgroundUrl:
-        'http://localhost:5000/_renderer.html?_fixtureId=%7B%22path%22%3A%22NestedDecorators%2Findex.fixture.tsx%22%2C%22name%22%3Anull%7D',
-      rendererUrl:
         'http://localhost:5000/?fixtureId=%7B%22path%22%3A%22NestedDecorators%2Findex.fixture.tsx%22%2C%22name%22%3Anull%7D',
+      relativeFilePath: 'NestedDecorators/index.fixture.tsx',
+      rendererUrl:
+        'http://localhost:5000/_renderer.html?_fixtureId=%7B%22path%22%3A%22NestedDecorators%2Findex.fixture.tsx%22%2C%22name%22%3Anull%7D',
       treePath: ['NestedDecorators']
     },
     {
+      absoluteFilePath: `${__dirname}/WelcomeMessage/index.fixture.tsx`,
       fileName: 'index',
       getElement: expect.any(Function),
-      absoluteFilePath: `${__dirname}/WelcomeMessage/index.fixture.tsx`,
       name: null,
-      relativeFilePath: 'WelcomeMessage/index.fixture.tsx',
       playgroundUrl:
-        'http://localhost:5000/_renderer.html?_fixtureId=%7B%22path%22%3A%22WelcomeMessage%2Findex.fixture.tsx%22%2C%22name%22%3Anull%7D',
-      rendererUrl:
         'http://localhost:5000/?fixtureId=%7B%22path%22%3A%22WelcomeMessage%2Findex.fixture.tsx%22%2C%22name%22%3Anull%7D',
+      relativeFilePath: 'WelcomeMessage/index.fixture.tsx',
+      rendererUrl:
+        'http://localhost:5000/_renderer.html?_fixtureId=%7B%22path%22%3A%22WelcomeMessage%2Findex.fixture.tsx%22%2C%22name%22%3Anull%7D',
       treePath: ['WelcomeMessage']
     }
   ]);
