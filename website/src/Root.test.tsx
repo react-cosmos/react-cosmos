997c532a5fc7076e8f3d1739a0132976adf99816
Ovidiu Cherecheș <skidding@gmail.com>
2020-04-20 15:14:00 +0300

    Upgrade styled-components

--- website/src/Root.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { create } from 'react-test-renderer';
-import { Root } from './Root';
-
-it('renders correctly', () => {
-  const renderer = create(<Root />);
-  expect(renderer.toJSON()).toMatchSnapshot();
-});
