5dc155df5950f840808ba039cfdc6d1126665f28
Ovidiu Cherecheș <skidding@gmail.com>
2020-04-30 22:21:24 +0300

    Fix auto fixture file detection in Create React App (#1194)

--- packages/react-cosmos/src/plugins/webpack/webpackConfig/devServer.ts
+++ packages/react-cosmos/src/plugins/webpack/webpackConfig/devServer.ts
@@ -20,7 +20,8 @@ export async function getDevWebpackConfig(
     cosmosConfig,
     userWebpack
   );
-  return {
+
+  const webpackConfig = {
     ...baseWebpackConfig,
     entry: getEntry(cosmosConfig),
     output: getOutput(cosmosConfig),
@@ -31,6 +32,23 @@ export async function getDevWebpackConfig(
     resolve: resolveLocalReactDeps(cosmosConfig, baseWebpackConfig),
     plugins: getPlugins(cosmosConfig, baseWebpackConfig, userWebpack),
   };
+
+  // optimization.splitChunks.name = false breaks auto fixture file discovery.
+  // When the splitChunks.name is set to false, existing fixtures hot reload
+  // fine, but added or removed fixture files don't appear or disappear in the
+  // React Cosmos UI automatically — a page refresh is required. The webpack
+  // build updates correctly, but module.hot.accept isn't called on the client:
+  // https://github.com/react-cosmos/react-cosmos/blob/548e9b7e9ca9fbc66f3915861cf1ae9d60222b28/packages/react-cosmos/src/plugins/webpack/client/index.ts#L24-L29
+  // Create React App uses this setting:
+  // https://github.com/facebook/create-react-app/blob/37712374bcaa6ccb168eeaf4fe8bd52d120dbc58/packages/react-scripts/config/webpack.config.js#L286
+  // Apparently it's a webpack 4 bug:
+  // https://twitter.com/wSokra/status/1255925851557974016
+  if (webpackConfig.optimization?.splitChunks) {
+    const { name } = webpackConfig.optimization.splitChunks;
+    if (name === false) delete webpackConfig.optimization.splitChunks.name;
+  }
+
+  return webpackConfig;
 }
 
 function getEntry(cosmosConfig: CosmosConfig) {
