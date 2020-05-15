902a53f84113a2f73cd59a0f9bcbdef95a8d0147
Ovidiu Cherecheș <skidding@gmail.com>
2020-05-15 18:10:49 +0300

    Make spacing more consistent

--- packages/react-cosmos-playground2/src/shared/ui/valueInputTree/ValueInputTreeDir.tsx
+++ packages/react-cosmos-playground2/src/shared/ui/valueInputTree/ValueInputTreeDir.tsx
@@ -86,6 +86,7 @@ const ChevronContainer = styled.span`
   flex-shrink: 0;
   width: ${iconSize}px;
   height: ${iconSize}px;
+  margin: 0 0 0 -2px;
   padding: 0px 2px 0 0;
   color: ${grey160};
 `;
@@ -98,7 +99,7 @@ const Text = styled.span`
 
 const DirName = styled.span<{ disabled: boolean }>`
   color: ${disabledColors(grey224, grey128)};
-  padding: 0 0 0 ${props => (props.disabled ? 18 : 0)}px;
+  padding: 0 0 0 ${props => (props.disabled ? 16 : 0)}px;
 `;
 
 const ChildrenInfo = styled.span`
