74d7643135d9e53f33ac29a9101d899caaf23437
Ovidiu Cherecheș <skidding@gmail.com>
2020-05-20 02:35:10 +0300

    Update Stars.tsx

--- website/src/Cosmonaut/Stars.tsx
+++ website/src/Cosmonaut/Stars.tsx
@@ -6,7 +6,7 @@ export const Stars = React.memo(function Stars() {
     <>
       <defs>
         <radialGradient id="starGlow" r={0.5}>
-          <stop offset="20%" stopColor="#bde0f6" />
+          <stop offset="30%" stopColor="#bde0f6" />
           <stop offset="100%" stopColor="#2b7fc0" stopOpacity={0} />
         </radialGradient>
       </defs>
@@ -15,9 +15,8 @@ export const Stars = React.memo(function Stars() {
           key={index}
           cx={star.x}
           cy={star.y}
-          r={star.r + 1}
+          r={star.r}
           fill="url(#starGlow)"
-          opacity={(Math.min(2.5, star.r) / 2.5) * 0.7 + 0.3}
         />
       ))}
     </>
