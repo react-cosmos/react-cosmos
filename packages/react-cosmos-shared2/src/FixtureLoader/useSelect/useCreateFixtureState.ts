c5757fa3ecdd2bcc8e91dfcaf2ef88dda1c10d79
Ovidiu Cherecheș <skidding@gmail.com>
2020-07-03 22:42:17 +0300

    Update useCreateFixtureState.ts

--- packages/react-cosmos-shared2/src/FixtureLoader/useSelect/useCreateFixtureState.ts
+++ packages/react-cosmos-shared2/src/FixtureLoader/useSelect/useCreateFixtureState.ts
@@ -8,28 +8,27 @@ export function useCreateFixtureState<Option extends string>(
   args: UseSelectArgs<Option>
 ) {
   const { setFixtureState } = React.useContext(FixtureContext);
+  const defaultValue = getDefaultSelectValue(args);
   useEffect(() => {
     // The fixture state for this select is (re)created in two situations:
     // 1. Initially: No corresponding fixture state select is found
     // 2: Default value change: Current value is reset to new default value
     setFixtureState(prevFsState => {
-      const defaultValue = getDefaultSelectValue(args);
       const fsSelect = findFixtureStateSelect(prevFsState, selectName);
       if (fsSelect && fsSelect.defaultValue === defaultValue)
         return prevFsState;
 
-      const { options } = args;
       return {
         ...prevFsState,
         selects: {
           ...prevFsState.selects,
           [selectName]: {
-            options,
+            options: args.options,
             defaultValue,
             currentValue: defaultValue,
           },
         },
       };
     });
-  }, [args, selectName, setFixtureState]);
+  }, [args.options, defaultValue, selectName, setFixtureState]);
 }
