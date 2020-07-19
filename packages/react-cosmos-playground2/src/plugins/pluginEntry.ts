d7972b67627cb48dd351b8f39c836567a8e9c08c
Ovidiu Cherecheș <skidding@gmail.com>
2020-07-19 17:28:31 +0300

    Add plugin configs and plugin entry

---
+++ packages/react-cosmos-playground2/src/plugins/pluginEntry.ts
@@ -0,0 +1,85 @@
+// Run "yarn generate-playground-plugin-entry" to update this file. Do not change it by hand!
+import { enablePlugin, resetPlugins } from 'react-plugin';
+
+const disabledPlugins = ['rendererSelect', 'pluginList'];
+
+// Unregistering plugins first makes this file hot-reloadable because a plugin
+// can only be registered once with a given name
+resetPlugins();
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/BuildNotifications/index.ts')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/BuildNotifications/index.ts');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/ClassStatePanel/index.tsx')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/ClassStatePanel/index.tsx');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/ContentOverlay/index.tsx')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/ContentOverlay/index.tsx');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/ControlPanel/index.tsx')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/ControlPanel/index.tsx');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/Core/index.tsx')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/Core/index.tsx');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/EditFixtureButton/index.tsx')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/EditFixtureButton/index.tsx');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/FixtureBookmark/index.tsx')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/FixtureBookmark/index.tsx');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/FixtureSearch/index.tsx')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/FixtureSearch/index.tsx');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/FixtureTree/index.tsx')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/FixtureTree/index.tsx');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/FullScreenButton/index.tsx')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/FullScreenButton/index.tsx');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/MessageHandler/index.tsx')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/MessageHandler/index.tsx');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/Notifications/index.tsx')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/Notifications/index.tsx');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/PluginList/index.tsx')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/PluginList/index.tsx');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/PropsPanel/index.tsx')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/PropsPanel/index.tsx');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/RemoteRenderer/index.tsx')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/RemoteRenderer/index.tsx');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/RendererCore/index.ts')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/RendererCore/index.ts');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/RendererPreview/index.tsx')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/RendererPreview/index.tsx');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/RendererSelect/index.tsx')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/RendererSelect/index.tsx');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/ResponsivePreview/index.tsx')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/ResponsivePreview/index.tsx');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/Root/index.tsx')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/Root/index.tsx');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/Router/index.ts')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/Router/index.ts');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/SelectControl/index.tsx')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/SelectControl/index.tsx');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/StandardControl/index.tsx')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/StandardControl/index.tsx');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/Storage/index.ts')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/Storage/index.ts');
+
+delete require.cache[require.resolve('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/WebpackHmrNotification/index.ts')];
+require('/Users/ovidiu/Work/@react-cosmos/react-cosmos/packages/react-cosmos-playground2/src/plugins/WebpackHmrNotification/index.ts');
+
+disabledPlugins.forEach(disabledPlugin => enablePlugin(disabledPlugin, false));
