a404181360dfc276ab588adec0464c9f4425fd0c
Ovidiu Cherecheș <skidding@gmail.com>
2020-07-31 14:38:18 +0300

    Remove source maps from playground build

--- packages/react-cosmos-playground2/webpack.config.js
+++ packages/react-cosmos-playground2/webpack.config.js
@@ -18,9 +18,7 @@ if (env === 'development') {
 
 module.exports = {
   mode: env,
-  // Besides other advantages, cheap-module-source-map is compatible with
-  // React.componentDidCatch https://github.com/facebook/react/issues/10441
-  devtool: 'cheap-module-source-map',
+  devtool: false,
   entry: src,
   output: {
     libraryTarget: 'umd',
