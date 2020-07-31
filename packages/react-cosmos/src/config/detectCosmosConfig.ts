43d577d81c182cae071bd7c8c4fd86e565c0d348
Ovidiu Cherecheș <skidding@gmail.com>
2020-07-31 16:24:55 +0300

    Make code more readable

--- packages/react-cosmos/src/config/detectCosmosConfig.ts
+++ packages/react-cosmos/src/config/detectCosmosConfig.ts
@@ -16,33 +16,35 @@ export function detectCosmosConfigPath() {
   const rootDir = detectRootDir();
   const cliArgs = getCliArgs();
 
-  if (typeof cliArgs.config !== 'string') {
-    const defaultConfigPath = path.join(rootDir, 'cosmos.config.json');
-    return moduleExists(defaultConfigPath) ? defaultConfigPath : null;
+  // CLI support for --config path/to/cosmos.config.json
+  if (typeof cliArgs.config === 'string') {
+    if (path.extname(cliArgs.config) !== '.json')
+      throw new Error(
+        `[Cosmos] Invalid config file type: ${cliArgs.config} (must be .json)`
+      );
+
+    const absPath = path.resolve(rootDir, cliArgs.config);
+    if (!fileExists(absPath))
+      throw new Error(`[Cosmos] Config not found at path: ${cliArgs.config}`);
+
+    return absPath;
   }
 
-  // CLI support for --config relative/path/to/cosmos.config.json
-  if (path.extname(cliArgs.config) !== '.json')
-    throw new Error(
-      `[Cosmos] Invalid config file type: ${cliArgs.config} (must be .json)`
-    );
-
-  const absPath = path.resolve(rootDir, cliArgs.config);
-  if (!fileExists(absPath))
-    throw new Error(`[Cosmos] Config not found at path: ${cliArgs.config}`);
-
-  return absPath;
+  const defaultConfigPath = path.join(rootDir, 'cosmos.config.json');
+  return moduleExists(defaultConfigPath) ? defaultConfigPath : null;
 }
 
 function detectRootDir() {
   const cliArgs = getCliArgs();
 
-  if (typeof cliArgs.rootDir !== 'string') return getCurrentDir();
+  // CLI support for --root-dir path/to/project
+  if (typeof cliArgs.rootDir === 'string') {
+    const absPath = path.resolve(getCurrentDir(), cliArgs.rootDir);
+    if (!dirExists(absPath))
+      throw new Error(`[Cosmos] Dir not found at path: ${cliArgs.rootDir}`);
 
-  // CLI support for --root-dir relative/path/project
-  const absPath = path.resolve(getCurrentDir(), cliArgs.rootDir);
-  if (!dirExists(absPath))
-    throw new Error(`[Cosmos] Dir not found at path: ${cliArgs.rootDir}`);
+    return absPath;
+  }
 
-  return absPath;
+  return getCurrentDir();
 }
