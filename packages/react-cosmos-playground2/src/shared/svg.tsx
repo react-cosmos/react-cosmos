24e2a50359ceb51f437b47fa7516c55377beb315
Ovidiu Cherecheș <skidding@gmail.com>
2020-08-15 14:06:29 +0300

    Make icons look sharper

--- packages/react-cosmos-playground2/src/shared/svg.tsx
+++ packages/react-cosmos-playground2/src/shared/svg.tsx
@@ -24,7 +24,7 @@ export function Icon({ children }: { children: SvgChildren }) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="2"
+      strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
     >
