ce4a962e83a6c91fa1212097362f29f0f61fc792
Ovidiu Cherecheș <skidding@gmail.com>
2021-02-23 15:00:57 +0200

    Remove unnecessary type

--- packages/react-cosmos-shared2/src/@types/react-element-to-jsx-string.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'react-element-to-jsx-string' {
-  import React from 'react';
-
-  export default function reactElementToJSXString(
-    element: React.ReactElement<any>
-  ): string;
-}
