d2f052528d187ef8caf39cd0366766b56139abef
Ovidiu Cherecheș <skidding@gmail.com>
2021-10-11 18:19:30 +0300

    Auto disable Fast Refresh in CRA 4 (#1352)
    * Auto disable Fast Refresh in CRA 4
    
    * Update README.md

--- packages/react-cosmos/bin/cosmos.js
+++ packages/react-cosmos/bin/cosmos.js
@@ -5,7 +5,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 // Auto disable Fast Refresh in CRA 4 until integration is implemented
 // https://github.com/react-cosmos/react-cosmos/issues/1272
-process.env.FAST_REFRESH = process.env.FAST_REFRESH || false
+process.env.FAST_REFRESH = process.env.FAST_REFRESH || false;
 
 require('regenerator-runtime/runtime');
 const { startWebServer } = require('../dist/webServer');
