4071f7cb08c0921015fb19201ea72ddce0caaafc
Ovidiu Cherecheș <skidding@gmail.com>
2022-01-29 16:44:58 +0200

    Update fixtureHelpers.ts

--- packages/react-cosmos-shared2/src/FixtureLoader/fixtureHelpers.ts
+++ packages/react-cosmos-shared2/src/FixtureLoader/fixtureHelpers.ts
@@ -1,22 +1,26 @@
-import React from 'react';
+import { ReactNode } from 'react';
 import { isMultiFixture, ReactFixtureExport, ReactFixtureMap } from '../react';
 
 export function getFixture(
   fixtureExport: ReactFixtureExport,
   fixtureName?: string
-): void | React.ReactNode {
-  if (!isMultiFixture(fixtureExport)) {
+): void | ReactNode {
+  if (fixtureName === undefined) {
+    if (isMultiFixture(fixtureExport)) {
+      // Fixture name missing in multi fixture
+      return;
+    }
+
     return fixtureExport;
+  }
+
+  if (!isMultiFixture(fixtureExport)) {
+    // Fixture name not found in single fixture
+    return;
   }
 
   // FIXME: Why does fixtureExport need to be cast as ReactFixtureMap when
   // the type predicate returned by isMultiFixture already ensures it?
   const multiFixtureExport: ReactFixtureMap = fixtureExport;
-
-  if (fixtureName) {
-    return multiFixtureExport[fixtureName];
-  }
-
-  const fixtureNames = Object.keys(multiFixtureExport);
-  return multiFixtureExport[fixtureNames[0]];
+  return multiFixtureExport[fixtureName];
 }
