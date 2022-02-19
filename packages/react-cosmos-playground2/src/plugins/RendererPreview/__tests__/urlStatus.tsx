ef1994b1f82fe5ae2773e22d1e6fc7f73fa15966
Ovidiu Cherecheș <skidding@gmail.com>
2022-02-19 18:26:20 +0200

    Fix tests

--- packages/react-cosmos-playground2/src/plugins/RendererPreview/__tests__/urlStatus.tsx
+++ packages/react-cosmos-playground2/src/plugins/RendererPreview/__tests__/urlStatus.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import { waitFor } from '@testing-library/dom';
 import { render } from '@testing-library/react';
-import { loadPlugins, Slot, resetPlugins } from 'react-plugin';
+import React from 'react';
+import { loadPlugins, resetPlugins, Slot } from 'react-plugin';
 import {
-  mockCore,
   getRendererPreviewMethods,
+  mockCore,
+  mockRouter,
 } from '../../../testHelpers/pluginMocks';
 import { fakeFetchResponseStatus } from '../testHelpers/fetch';
 
@@ -16,6 +17,7 @@ function registerTestPlugins() {
   mockCore({
     getWebRendererUrl: () => 'mockRendererUrl',
   });
+  mockRouter();
 }
 
 function loadTestPlugins(status: number) {
