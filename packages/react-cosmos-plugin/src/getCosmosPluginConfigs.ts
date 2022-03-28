f65963995db4463ab2979cbb09f8f353dbe3c012
Ovidiu Cherecheș <skidding@gmail.com>
2022-03-28 21:55:10 +0300

    Update getCosmosPluginConfigs.ts

--- packages/react-cosmos-plugin/src/getCosmosPluginConfigs.ts
+++ packages/react-cosmos-plugin/src/getCosmosPluginConfigs.ts
@@ -3,21 +3,25 @@ import path from 'path';
 import resolveFrom from 'resolve-from';
 
 // TODO: Validate config schema on config import
+// TODO: Allow ui and devServer to be [true] for default paths?
 type RawCosmosPluginConfig = {
   name: string;
   ui?: string;
+  devServer?: string;
 };
 
 export type CosmosPluginConfig = {
   name: string;
   rootDir: string;
   ui?: string;
+  devServer?: string;
 };
 
 type Args = {
   rootDir: string;
   ignore?: string[];
 };
+// TODO: Rename to findCosmosPluginConfigs
 export function getCosmosPluginConfigs({ rootDir, ignore }: Args) {
   const configPaths = getCosmosPluginConfigPaths(rootDir, ignore);
   return configPaths.map(configPath =>
