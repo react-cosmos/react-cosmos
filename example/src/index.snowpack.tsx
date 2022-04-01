e3c256c07afa997b512f2765f72d74e63a79b201
Ovidiu Cherecheș <skidding@gmail.com>
2022-04-01 23:21:04 +0300

    Fix dependencies

--- example/src/index.snowpack.tsx
+++ example/src/index.snowpack.tsx
@@ -1,20 +1,9 @@
-import React from 'react';
-import {
-  createPostMessageConnect,
-  FixtureLoader,
-} from 'react-cosmos-shared2/FixtureLoader';
-import { render } from 'react-dom';
+import { mountDomRenderer } from 'react-cosmos/dom';
 // @ts-ignore
 import { decorators, fixtures } from './cosmos.userdeps.js';
 
-render(
-  <FixtureLoader
-    rendererId="snowpack-renderer"
-    rendererConnect={createPostMessageConnect()}
-    fixtures={fixtures}
-    selectedFixtureId={null}
-    systemDecorators={[]}
-    userDecorators={decorators}
-  />,
-  document.getElementById('root')
-);
+mountDomRenderer({
+  rendererConfig: { containerQuerySelector: '#root' },
+  decorators,
+  fixtures,
+});
