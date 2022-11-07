00f19ce82f3f7d066820e9bea240ddec7bcbf171
Ovidiu Cherecheș <hello@ovidiu.ch>
2022-11-07 22:20:30 +0200

    Update pluginConfigs.ts

--- packages/react-cosmos/src/server/cosmosPlugin/pluginConfigs.ts
+++ packages/react-cosmos/src/server/cosmosPlugin/pluginConfigs.ts
@@ -3,9 +3,9 @@ import { findCosmosPluginConfigs } from './findCosmosPluginConfigs';
 import { readCosmosPluginConfig } from './readCosmosPluginConfig';
 import { CosmosPluginConfig } from './types';
 
-// TODO: Refactor
 export function getPluginConfigs(cosmosConfig: CosmosConfig) {
   const { rootDir, disablePlugins, plugins, exportPath } = cosmosConfig;
+
   if (disablePlugins) return [];
 
   const moduleConfigs = plugins.reduce(
