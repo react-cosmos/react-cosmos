b5df67a91444531969ee068fb4a46b7a6a39f0c8
Ovidiu Cherecheș <hello@ovidiu.ch>
2022-11-18 15:48:16 +0200

    Update clearBuild.ts

--- scripts/clearBuild.ts
+++ scripts/clearBuild.ts
@@ -1,7 +1,10 @@
-import { done, globAsync, rimrafAsync } from './shared.js';
+import fs from 'fs/promises';
+import { done, globAsync } from './shared.js';
 
 (async () => {
   const distPaths = (await globAsync(`./packages/*/dist`)) as string[];
-  await Promise.all(distPaths.map(p => rimrafAsync(p)));
+  await Promise.all(
+    distPaths.map(p => fs.rm(p, { recursive: true, force: true }))
+  );
   console.log(done(`Cleared all build files.`));
 })();
