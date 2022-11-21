75408e68aaedc906ade77a1adf2fa3cb05192a03
Ovidiu Cherecheș <hello@ovidiu.ch>
2022-11-21 13:20:10 +0200

    Update requireFromSilent.ts

--- packages/react-cosmos/src/server/utils/requireFromSilent.ts
+++ packages/react-cosmos/src/server/utils/requireFromSilent.ts
@@ -1,8 +1,10 @@
+import { isNodeError } from './isNodeError.js';
 import { requireFrom } from './requireModule.js';
 
 export function requireFromSilent(fromDirectory: string, moduleId: string) {
   try {
     return requireFrom(fromDirectory, moduleId);
   } catch (err) {
+    if (!isNodeError(err) || err.code !== 'MODULE_NOT_FOUND') console.log(err);
   }
 }
