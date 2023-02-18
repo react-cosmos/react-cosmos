b68a3ec916ca9ffe1d445f72cf2e65f9aa216118
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-02-18 21:45:28 +0200

    Update comment

--- packages/react-cosmos-plugin-vite/src/createViteRendererIndex.ts
+++ packages/react-cosmos-plugin-vite/src/createViteRendererIndex.ts
@@ -5,7 +5,7 @@ import { mountDomRenderer } from 'react-cosmos-dom';
 mount();
 
 async function mount() {
-  // Use dynamic import to load updated modules upon hot reloading
+  // Use dynamic import to reload updated modules upon hot reloading
   const args = await import('${userDepsModuleId}');
   mountDomRenderer(args);
 }
