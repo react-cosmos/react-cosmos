524093094b6cc7daf9619cf761ec0a8156fc6ff6
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-02-18 14:59:17 +0200

    Update resolveUrlPath.ts

--- packages/react-cosmos/src/server/shared/resolveUrlPath.ts
+++ packages/react-cosmos/src/server/shared/resolveUrlPath.ts
@@ -1,6 +1,7 @@
 // resolveUrlPath('/', '_renderer.html') => '/_renderer.html'
 // resolveUrlPath('./', '_renderer.html') => '/_renderer.html'
 // resolveUrlPath('http://localhost:5000', '_renderer.html') => 'http://localhost:5000/_renderer.html'
+
 export function resolveUrlPath(base: string, urlPath: string) {
   // The 'resolve://' protocol is only added when base is a relative URL.
   const resolvedUrl = new URL(urlPath, new URL(base, 'resolve://'));
