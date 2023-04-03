7f1e47ebb2350c0b642e8f518085b5e9fcf27634
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-04-03 16:18:32 +0300

    Add Babel plugin in webpack override

--- examples/webpack/babel.config.js
+++ examples/webpack/babel.config.js
@@ -1,5 +1,3 @@
 export default {
   presets: ['@babel/preset-react', '@babel/preset-typescript'],
-  plugins:
-    process.env.NODE_ENV === 'development' ? ['react-refresh/babel'] : [],
 };
--- examples/webpack/webpack.override.js
+++ examples/webpack/webpack.override.js
@@ -1,10 +1,10 @@
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 
 export default function getWebpackConfig(webpackConfig, env) {
-  if (env !== 'development') return webpackConfig;
+  if (env === 'development') {
+    webpackConfig.module.rules[0].use.options.plugins = ['react-refresh/babel'];
+    webpackConfig.plugins.push(new ReactRefreshWebpackPlugin());
+  }
 
-  return {
-    ...webpackConfig,
-    plugins: [...webpackConfig.plugins, new ReactRefreshWebpackPlugin()],
-  };
+  return webpackConfig;
 }
