b99839ace97d097e9f62810459b8bd27c2a053ac
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-04-05 12:10:32 +0300

    Automatically enable webpack top level await

--- packages/react-cosmos-plugin-webpack/src/server/webpackConfig/getDevWebpackConfig.ts
+++ packages/react-cosmos-plugin-webpack/src/server/webpackConfig/getDevWebpackConfig.ts
@@ -9,6 +9,7 @@ import { getWebpackConfigResolve } from './getWebpackConfigResolve.js';
 import { ensureHtmlWebackPlugin } from './htmlPlugin.js';
 import { getGlobalsPlugin, hasPlugin } from './plugins.js';
 import { resolveWebpackClientPath } from './resolveWebpackClientPath.js';
+import { ensureWebpackConfigTopLevelAwait } from './webpackConfigTopLevelAwait.js';
 
 export async function getDevWebpackConfig(
   cosmosConfig: CosmosConfig,
@@ -26,6 +27,7 @@ export async function getDevWebpackConfig(
     module: getWebpackConfigModule(baseWebpackConfig),
     resolve: getWebpackConfigResolve(cosmosConfig, baseWebpackConfig),
     plugins: getPlugins(cosmosConfig, baseWebpackConfig, userWebpack),
+    experiments: getExperiments(baseWebpackConfig),
   };
 
   // optimization.splitChunks.name = false breaks auto fixture file discovery.
@@ -91,4 +93,8 @@ function getPlugins(
 function getHotMiddlewareEntry(reloadOnFail: boolean) {
   const clientPath = resolve('webpack-hot-middleware/client');
   return `${clientPath}?reload=${reloadOnFail}&overlay=false`;
+}
+
+function getExperiments(baseWebpackConfig: webpack.Configuration) {
+  return ensureWebpackConfigTopLevelAwait(baseWebpackConfig);
 }
