73a2b4480d738ae51f4f082315465c21038cbe54
Alex Bolenok <quassnoi@gmail.com>
2023-04-06 00:28:37 -0400

    Increase awareness of the ability to override Webpack config

--- packages/react-cosmos-plugin-webpack/src/server/webpackConfig/__tests__/customExportConfig.ts
+++ packages/react-cosmos-plugin-webpack/src/server/webpackConfig/__tests__/customExportConfig.ts
@@ -31,6 +31,7 @@ const MY_PLUGIN = {};
 async function getCustomExportWebpackConfig() {
   return mockConsole(async ({ expectLog }) => {
     expectLog('[Cosmos] Using webpack config found at mywebpack.config.js');
+    expectLog('[Cosmos] Learn how to override webpack config for cosmos: https://github.com/react-cosmos/react-cosmos/tree/main/docs#webpack-config-override');
     const cosmosConfig = createCosmosConfig(process.cwd(), {
       webpack: {
         configPath: 'mywebpack.config.js',
