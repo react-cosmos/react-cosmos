9ad5216558da6265cf304964e91466a699f541fa
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-04-07 15:51:22 +0300

    Update setup.ts

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -3,7 +3,7 @@ import 'jest-styled-components';
 
 jest.setTimeout(20000);
 
-// This become necessary since using fetch with { method: 'HEAD' } in
+// This became necessary since using fetch with { method: 'HEAD' } in
 // dev server tests.
 // Copied from https://github.com/prisma/prisma/issues/8558#issuecomment-1129055580
 global.setImmediate = jest.useRealTimers as unknown as typeof setImmediate;
