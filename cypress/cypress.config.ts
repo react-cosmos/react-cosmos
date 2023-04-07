a92ceac71240120957bcd58a085551a39ab52f8d
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-04-08 02:12:17 +0300

    Update cypress.config.ts

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -4,12 +4,12 @@ export default defineConfig({
   projectId: '7gsfa8',
   chromeWebSecurity: false,
   modifyObstructiveCode: false,
-  // Disabled test isoation to try to fix flakiness of domDev test on Windows.
-  // Every now and then it would fail before running any test with the message:
-  // "This page was cleared by navigating to about:blank."
-  // Found it here: https://stackoverflow.com/a/74762655
-  testIsolation: false,
   e2e: {
     specPattern: 'cypress/tests/**/*.{js,jsx,ts,tsx}',
+    // Disabled test isoation to try to fix flakiness of domDev test on Windows.
+    // Every now and then it would fail before running any test with the message:
+    // "This page was cleared by navigating to about:blank."
+    // Found it here: https://stackoverflow.com/a/74762655
+    testIsolation: false,
   },
 });
