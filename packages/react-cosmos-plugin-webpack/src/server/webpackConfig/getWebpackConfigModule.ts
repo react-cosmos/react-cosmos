06e62c232815e8bed88352cce54534d332d3ee9b
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-04-18 00:08:30 +0300

    Pass Cosmos config updated by plugins to webpack loader

--- packages/react-cosmos-plugin-webpack/src/server/webpackConfig/getWebpackConfigModule.ts
+++ packages/react-cosmos-plugin-webpack/src/server/webpackConfig/getWebpackConfigModule.ts
@@ -1,24 +1,32 @@
+import { CosmosConfig } from 'react-cosmos/dist/index.js';
 import webpack from 'webpack';
 import { resolveWebpackClientPath } from './resolveWebpackClientPath.js';
 import { resolveWebpackLoaderPath } from './resolveWebpackLoaderPath.js';
 
 export function getWebpackConfigModule(
+  cosmosConfig: CosmosConfig,
   webpackConfig: webpack.Configuration
 ): webpack.ModuleOptions {
   return {
     ...webpackConfig.module,
-    rules: getRules(webpackConfig),
+    rules: getRules(cosmosConfig, webpackConfig),
   };
 }
 
-function getRules({ module }: webpack.Configuration) {
+function getRules(
+  cosmosConfig: CosmosConfig,
+  { module }: webpack.Configuration
+) {
   const existingRules = (module && module.rules) || [];
-  return [...existingRules, getUserDepsLoaderRule()];
+  return [...existingRules, getUserDepsLoaderRule(cosmosConfig)];
 }
 
-function getUserDepsLoaderRule(): webpack.RuleSetRule {
+function getUserDepsLoaderRule(
+  cosmosConfig: CosmosConfig
+): webpack.RuleSetRule {
   return {
     loader: resolveWebpackLoaderPath(),
     include: resolveWebpackClientPath('userDeps'),
+    options: { cosmosConfig },
   };
 }
