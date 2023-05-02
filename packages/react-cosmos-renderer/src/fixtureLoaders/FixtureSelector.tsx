99d81bd2ec8326ea5aa05bdae4e21e01fb98f2c1
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-05-03 00:47:23 +0300

    Fix ServerFixtureLoader

--- packages/react-cosmos-renderer/src/fixtureLoaders/FixtureSelector.tsx
+++ packages/react-cosmos-renderer/src/fixtureLoaders/FixtureSelector.tsx
@@ -4,7 +4,7 @@ import {
   UserModuleWrappers,
   getFixtureListFromWrappers,
 } from 'react-cosmos-core';
-import { RendererContext } from '../rendererConnect/RendererContext.js';
+import { FixtureSelectorConnect } from './FixtureSelectorConnect.js';
 import { FixtureLoaderSelection } from './useFixtureLoaderState.js';
 
 type Props = {
@@ -28,49 +28,27 @@ export function FixtureSelector({
     [moduleWrappers]
   );
 
-  const { rendererId, rendererConnect } = React.useContext(RendererContext);
-
-  const readyRef = React.useRef(false);
-  React.useEffect(() => {
-    if (readyRef.current) {
-      rendererConnect.postMessage({
-        type: 'fixtureListUpdate',
-        payload: { rendererId, fixtures },
-      });
-    } else {
-      rendererConnect.postMessage({
-        type: 'rendererReady',
-        payload: initialFixtureId
-          ? { rendererId, fixtures, initialFixtureId }
-          : { rendererId, fixtures },
-      });
-      readyRef.current = true;
+  function renderInner() {
+    if (!selection) {
+      return renderNoFixtureSelected
+        ? renderMessage('No fixture selected.')
+        : null;
     }
-  }, [fixtures, initialFixtureId, rendererConnect, rendererId]);
 
-  React.useEffect(
-    () =>
-      rendererConnect.onMessage(msg => {
-        if (msg.type === 'pingRenderers') {
-          rendererConnect.postMessage({
-            type: 'rendererReady',
-            payload: { rendererId, fixtures },
-          });
-        }
-      }),
-    [fixtures, rendererConnect, rendererId]
+    const { fixtureId } = selection;
+    if (!fixtures[fixtureId.path]) {
+      return renderMessage(`Fixture path not found: ${fixtureId.path}`);
+    }
+
+    return renderFixture(selection);
+  }
+
+  return (
+    <FixtureSelectorConnect
+      fixtures={fixtures}
+      initialFixtureId={initialFixtureId}
+    >
+      {renderInner()}
+    </FixtureSelectorConnect>
   );
-
-  if (!selection) {
-    return renderNoFixtureSelected
-      ? renderMessage('No fixture selected.')
-      : null;
-  }
-
-  const { fixtureId } = selection;
-  if (!fixtures[fixtureId.path]) {
-    return renderMessage(`Fixture path not found: ${fixtureId.path}`);
-  }
-
-  return renderFixture(selection);
 }
