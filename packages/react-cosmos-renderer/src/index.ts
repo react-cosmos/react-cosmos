4718cb303f7cafc64d9a5edc7588aa5f837fb62c
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-05-02 20:12:28 +0300

    Remove server entry point

--- packages/react-cosmos-renderer/src/index.ts
+++ packages/react-cosmos-renderer/src/index.ts
@@ -1,4 +1,5 @@
 export * from './fixtureLoaders/ClientFixtureLoader.js';
+export * from './fixtureLoaders/ServerFixtureLoader.js';
 export * from './rendererConnect/createPostMessageConnect.js';
 export * from './rendererConnect/createWebSocketsConnect.js';
 export * from './selectedFixture/decorateFixture.js';
--- packages/react-cosmos-renderer/src/server.ts
+++
@@ -1,1 +0,0 @@
-export * from './fixtureLoaders/ServerFixtureLoader.js';
