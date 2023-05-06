b800218b29fd2fa6eceed32c8a4a085ea222d5a7
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-05-06 18:24:21 +0300

    Update playgroundConfig.ts

--- packages/react-cosmos-ui/src/playgroundConfig.ts
+++ packages/react-cosmos-ui/src/playgroundConfig.ts
@@ -1,9 +1,11 @@
 import { CosmosPluginConfig } from 'react-cosmos-core';
 import { CoreSpec } from './plugins/Core/spec.js';
+import { RendererCoreSpec } from './plugins/RendererCore/spec.js';
 
 // Config can also contain keys for 3rd party plugins
 export type PlaygroundConfig = {
   core: CoreSpec['config'];
+  rendererCore: RendererCoreSpec['config'];
   [pluginName: string]: {};
 };
 
