9c9406096b1533c6a6142600ba03821b31658a9a
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-05-07 13:08:27 +0300

    Update ServerFixtureLoader.tsx

--- packages/react-cosmos-renderer/src/fixtureLoaders/ServerFixtureLoader.tsx
+++ packages/react-cosmos-renderer/src/fixtureLoaders/ServerFixtureLoader.tsx
@@ -13,28 +13,14 @@ import { defaultRenderMessage } from './defaultRenderMessage.js';
 
 // This fixture loader is designed for React Server Components setups.
 // Although server components are stateless, this fixture loader still
-// communicates with the Cosmos UI through Client components (via postMessage
-// or WebSocket messages). The main distinction from the client fixture loader
+// communicates with the Cosmos UI through Client components via postMessage
+// or WebSocket messages. The main distinction from the client fixture loader
 // is that the fixture modules here are loaded on the server. This means that
-// the fixture loader cannot receive fixtureSelect messages from the Cosmos UI.
-// The fixture is selected on the server and a full page reload is required to
-// change the fixture.
-
-// Example usage (src/app/cosmos/page.tsx):
-// import { parseRendererUrlQuery } from 'react-cosmos-core';
-// import { ServerFixtureLoader } from 'react-cosmos-renderer';
-// import { moduleWrappers, rendererConfig } from '../../../cosmos.imports';
-
-// export default ({ searchParams }: { searchParams: {} }) => {
-//   return (
-//     <ServerFixtureLoader
-//       rendererConfig={rendererConfig}
-//       moduleWrappers={moduleWrappers}
-//       searchParams={searchParams}
-//     />
-//   );
-// };
-
+// this fixture loader cannot respond directly to 'fixtureSelect' client
+// requests. Instead, here the fixture is selected from the server-side HTTP
+// request search params. Fixture change requests are then received on the
+// client, which triggers a page reload by changing the URL's search params,
+// which in turn triggers a new fixture selection on the server.
 type Props = {
   searchParams: StringRendererSearchParams;
   moduleWrappers: UserModuleWrappers;
