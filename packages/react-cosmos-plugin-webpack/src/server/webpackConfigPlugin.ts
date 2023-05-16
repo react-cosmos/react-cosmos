f366073cc6c8ae8810a3a05b614b00a66dc56036
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-05-17 00:25:59 +0300

    Prefix renderer URL with public URL in webpack export

--- packages/react-cosmos-plugin-webpack/src/server/webpackConfigPlugin.ts
+++ packages/react-cosmos-plugin-webpack/src/server/webpackConfigPlugin.ts
@@ -1,17 +1,16 @@
+import path from 'node:path';
 import { CosmosConfig, CosmosConfigPluginArgs } from 'react-cosmos';
 import { RENDERER_FILENAME } from './webpackConfig/constants.js';
 
 export async function webpackConfigPlugin({
   cosmosConfig,
 }: CosmosConfigPluginArgs): Promise<CosmosConfig> {
-  const { rendererUrl } = cosmosConfig;
-  if (rendererUrl) {
+  if (cosmosConfig.rendererUrl) {
     return cosmosConfig;
   }
 
   return {
     ...cosmosConfig,
-    // TODO: Should cosmosConfig.publicUrl be used here?
-    rendererUrl: `./${RENDERER_FILENAME}`,
+    rendererUrl: path.join(cosmosConfig.publicUrl, RENDERER_FILENAME),
   };
 }
