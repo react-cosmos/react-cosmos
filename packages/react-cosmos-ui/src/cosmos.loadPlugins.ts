761ecc1395cb1be5457a367990ac3f5735dc8f63
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-05-16 23:04:15 +0300

    Fix inception fixture

--- packages/react-cosmos-ui/src/cosmos.loadPlugins.ts
+++ packages/react-cosmos-ui/src/cosmos.loadPlugins.ts
@@ -1,18 +1,24 @@
 import { loadPlugins } from 'react-plugin';
+import { PlaygroundConfig } from './playgroundConfig.js';
 import './plugins/pluginEntry.js';
 import { DEFAULT_PLUGIN_CONFIG } from './shared/defaultPluginConfig.js';
+
+const rendererConfig: PlaygroundConfig = {
+  ...DEFAULT_PLUGIN_CONFIG,
+  core: {
+    projectId: 'testProjectId',
+    fixturesDir: '__fixtures__',
+    fixtureFileSuffix: 'fixture',
+    devServerOn: true,
+  },
+  rendererCore: {
+    fixtures: {},
+    rendererUrl: '/renderer.html',
+  },
+};
 
 // This file is required to run globally with side effects for the "inception"
 // fixture to work.
 loadPlugins({
-  config: {
-    ...DEFAULT_PLUGIN_CONFIG,
-    core: {
-      projectId: 'testProjectId',
-      fixturesDir: '__fixtures__',
-      fixtureFileSuffix: 'fixture',
-      devServerOn: true,
-      rendererUrl: '/_renderer.html',
-    },
-  },
+  config: rendererConfig,
 });
