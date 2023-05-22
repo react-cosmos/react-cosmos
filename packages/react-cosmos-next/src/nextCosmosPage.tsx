42810b9c8e0cb7bed84b445b737fc1e6fffca919
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-05-22 16:51:02 +0300

    Update nextCosmosPage.tsx

--- packages/react-cosmos-next/src/nextCosmosPage.tsx
+++ packages/react-cosmos-next/src/nextCosmosPage.tsx
@@ -3,16 +3,12 @@ import { RendererConfig, UserModuleWrappers } from 'react-cosmos-core';
 import { NextFixtureLoader } from './NextFixtureLoader.js';
 import { NextCosmosParams } from './nextTypes.js';
 
-type NextCosmosPageProps = {
-  params: NextCosmosParams;
-};
-
 type Args = {
   rendererConfig: RendererConfig;
   moduleWrappers: UserModuleWrappers;
 };
 export function nextCosmosPage({ rendererConfig, moduleWrappers }: Args) {
-  return function NextCosmosPage({ params }: NextCosmosPageProps) {
+  return function NextCosmosPage({ params }: { params: NextCosmosParams }) {
     return (
       <NextFixtureLoader
         rendererConfig={rendererConfig}
