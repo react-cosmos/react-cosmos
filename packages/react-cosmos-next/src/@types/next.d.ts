d9fdd1a7d55c9fd65044e5c31afc4bdce4c89707
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-05-23 16:42:45 +0300

    Don't create new history entries in Next.js renderer iframe (#1503)

--- packages/react-cosmos-next/src/@types/next.d.ts
+++ packages/react-cosmos-next/src/@types/next.d.ts
@@ -2,6 +2,7 @@ declare module 'next/navigation' {
   export function usePathname(): string;
   export function useRouter(): {
     push(href: string): void;
+    replace(href: string): void;
   };
   export function useSearchParams(): URLSearchParams;
 }
