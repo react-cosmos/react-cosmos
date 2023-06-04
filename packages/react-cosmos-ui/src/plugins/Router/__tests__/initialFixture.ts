bd362efdd15172286e8766e20ccfcd5246d255e3
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-06-04 22:02:47 +0300

    Add initial fixture

---
+++ packages/react-cosmos-ui/src/plugins/Router/__tests__/initialFixture.ts
@@ -0,0 +1,63 @@
+import { waitFor } from '@testing-library/dom';
+import { loadPlugins, resetPlugins } from 'react-plugin';
+import { register } from '..';
+import {
+  getRouterMethods,
+  onRouter,
+} from '../../../testHelpers/pluginMocks.js';
+import {
+  getUrlParams,
+  resetUrlParams,
+} from '../../../testHelpers/urlParams.js';
+
+beforeEach(register);
+
+afterEach(() => {
+  resetPlugins();
+  resetUrlParams();
+});
+
+const fixtureId = { path: 'zwei.js' };
+
+it('updates selected fixture ID', async () => {
+  loadPlugins({
+    config: {
+      router: {
+        initialFixtureId: fixtureId,
+      },
+    },
+  });
+  const router = getRouterMethods();
+
+  await waitFor(() => expect(router.getSelectedFixtureId()).toBe(fixtureId));
+});
+
+it('sets URL params', async () => {
+  loadPlugins({
+    config: {
+      router: {
+        initialFixtureId: fixtureId,
+      },
+    },
+  });
+
+  await waitFor(() =>
+    expect(getUrlParams()).toEqual({ fixtureId: JSON.stringify(fixtureId) })
+  );
+});
+
+it('emits "fixtureSelect" event', async () => {
+  const { fixtureSelect } = onRouter();
+
+  loadPlugins({
+    config: {
+      router: {
+        initialFixtureId: fixtureId,
+      },
+    },
+  });
+
+  await waitFor(() =>
+    expect(fixtureSelect).toBeCalledWith(expect.any(Object), fixtureId)
+  );
+});
