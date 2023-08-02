e62a76a6ead6d7ef021393ff585c0ff383767eb2
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-08-03 00:15:50 +0300

    Config svgr and clean up docs index

--- docs-next/next.config.js
+++ docs-next/next.config.js
@@ -7,4 +7,19 @@ const withNextra = nextra({
   staticImage: true,
 });
 
-export default withNextra();
+export default withNextra({
+  webpack(config) {
+    const allowedSvgRegex = /components\/icons\/.+\.svg$/;
+
+    const fileLoaderRule = config.module.rules.find(rule =>
+      rule.test?.test('.svg')
+    );
+    fileLoaderRule.exclude = allowedSvgRegex;
+
+    config.module.rules.push({
+      test: allowedSvgRegex,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+});
