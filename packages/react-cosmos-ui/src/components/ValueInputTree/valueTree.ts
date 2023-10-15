b479ec9cf460be2f6249255c628d30b390b598b4
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-10-15 23:19:24 +0300

    Show Array length instead of keys in Control Panel (#1556)
    * Show Array length instead of keys in Control Panel
    
    * Rename TodoApp

--- packages/react-cosmos-ui/src/components/ValueInputTree/valueTree.ts
+++ packages/react-cosmos-ui/src/components/ValueInputTree/valueTree.ts
@@ -1,23 +1,31 @@
 import { FixtureStateValues } from 'react-cosmos-core';
 import { ValueNode } from './shared.js';
 
-export function createValueTree(values: FixtureStateValues): ValueNode {
+export function createValueTree(
+  values: FixtureStateValues,
+  isArray = false
+): ValueNode {
   const children: Record<string, ValueNode> = {};
 
   Object.keys(values).forEach(key => {
     const value = values[key];
     if (value.type === 'object') {
-      children[key] = createValueTree(value.values);
+      children[key] = createValueTree(value.values, false);
     } else if (value.type === 'array') {
       const objValues: FixtureStateValues = {};
       value.values.forEach((v, idx) => {
         objValues[idx] = v;
       });
-      children[key] = createValueTree(objValues);
+      children[key] = createValueTree(objValues, true);
     } else {
-      children[key] = { data: { type: 'item', value } };
+      children[key] = {
+        data: { type: 'item', value },
+      };
     }
   });
 
-  return { data: { type: 'collection' }, children };
+  return {
+    data: { type: 'collection', isArray },
+    children,
+  };
 }
