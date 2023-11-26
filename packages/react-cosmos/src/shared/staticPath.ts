ebfa6c622b153ccb87d63742621b3041d07d0c9d
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-11-26 17:24:48 +0200

    Update staticPath.ts

--- packages/react-cosmos/src/shared/staticPath.ts
+++ packages/react-cosmos/src/shared/staticPath.ts
@@ -2,6 +2,9 @@ import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 export function getStaticPath(relPath: string) {
+  // For some reason `new URL('.', import.meta.url)` returns a relative path
+  // that starts with "/packages/react-cosmos" instead of the absolute path.
+  // At least in Vitest this happens, when the module URL has the .ts extension.
   const currentDir = dirname(fileURLToPath(new URL(import.meta.url)));
   return resolve(currentDir, '../static', relPath);
 }
