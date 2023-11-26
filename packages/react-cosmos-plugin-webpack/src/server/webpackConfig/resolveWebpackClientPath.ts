79a67d3f219a999a05ed4f1a262a303d66f1222f
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-11-26 23:22:08 +0200

    Replace Jest

--- packages/react-cosmos-plugin-webpack/src/server/webpackConfig/resolveWebpackClientPath.ts
+++ packages/react-cosmos-plugin-webpack/src/server/webpackConfig/resolveWebpackClientPath.ts
@@ -1,9 +1,6 @@
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-// NOTE: This is ESM code that can't run in Jest yet, which means that this
-// module always needs to be mocked in tests until Jest adds ESM support.
-
 export function resolveWebpackClientPath(relPath: string) {
   const currentDir = dirname(fileURLToPath(import.meta.url));
   return resolve(currentDir, '../../client', relPath);
--- packages/react-cosmos/src/utils/requireModule.ts
+++ packages/react-cosmos/src/utils/requireModule.ts
@@ -1,9 +1,6 @@
 import { createRequire } from 'node:module';
 import path from 'path';
 
-// NOTE: This is ESM code that can't run in Jest yet, which means that this
-// module always needs to be mocked in tests until Jest adds ESM support.
-
 export function requireModule(moduleId: string) {
   const require = createRequire(import.meta.url);
   return require(moduleId);
