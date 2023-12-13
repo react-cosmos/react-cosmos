1b83d155284bf21eabf1020f9e0e5524f2f3ff00
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-12-13 15:40:20 +0200

    Update useFixtureState.ts

--- packages/react-cosmos-renderer/src/fixture/useFixtureState.ts
+++ packages/react-cosmos-renderer/src/fixture/useFixtureState.ts
@@ -1,8 +1,11 @@
 import { useContext } from 'react';
-import { FixtureState } from 'react-cosmos-core';
+import { FixtureState, StateUpdater } from 'react-cosmos-core';
 import { FixtureContext } from './FixtureContext.js';
 
-export function useFixtureState<T extends object>() {
-  const { fixtureState } = useContext(FixtureContext);
-  return fixtureState as FixtureState & T;
+export function useFixtureState<T extends object = {}>() {
+  const { fixtureState, setFixtureState } = useContext(FixtureContext);
+  return [
+    fixtureState as FixtureState & T,
+    setFixtureState as (update: StateUpdater<FixtureState & T>) => unknown,
+  ] as const;
 }
