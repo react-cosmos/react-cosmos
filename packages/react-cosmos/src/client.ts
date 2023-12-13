5ba8390b4e572e8e347cdc373d7ed54ab7dc3c08
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-12-13 14:43:41 +0200

    Export useFixtureState from react-cosmos/client

--- packages/react-cosmos/src/client.ts
+++ packages/react-cosmos/src/client.ts
@@ -1,1 +1,6 @@
-export { Viewport, useSelect, useValue } from 'react-cosmos-renderer/client';
+export {
+  Viewport,
+  useFixtureState,
+  useSelect,
+  useValue,
+} from 'react-cosmos-renderer/client';
