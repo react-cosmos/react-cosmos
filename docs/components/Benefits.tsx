9c6b9c8d06dc607af4ad856fde341e6812488f95
Ovidiu Cherecheș <hello@ovidiu.ch>
2024-01-15 00:18:37 +0200

    Add rocket animation from old website

--- docs/components/Benefits.tsx
+++ docs/components/Benefits.tsx
@@ -1,12 +1,24 @@
 import { ReactNode } from 'react';
 import styles from './Benefits.module.css';
+import { Rocket } from './Rocket';
 
 export function Benefits() {
   return (
     <div className={styles.root}>
       <ul className={styles.benefits}>
-        <li>Prototype quickly, debug easily and maintain quality at scale.</li>
-        <li>Stay organized with a well-designed component library.</li>
+        <li className={styles.text}>
+          Prototype quickly, debug easily
+          <br />
+          and maintain quality at scale.
+        </li>
+        <li className={styles.icon}>
+          <Rocket />
+        </li>
+        <li className={styles.text}>
+          Stay organized with a well-
+          <br />
+          designed component library.
+        </li>
       </ul>
     </div>
   );
