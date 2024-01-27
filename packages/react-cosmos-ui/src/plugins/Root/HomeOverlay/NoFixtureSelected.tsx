b96b127dfbd4c41750b7ae2524bbde95c1bff35b
Ovidiu Cherecheș <hello@ovidiu.ch>
2024-01-27 17:49:16 +0200

    Improve styling of screenshots in home screen

--- packages/react-cosmos-ui/src/plugins/Root/HomeOverlay/NoFixtureSelected.tsx
+++ packages/react-cosmos-ui/src/plugins/Root/HomeOverlay/NoFixtureSelected.tsx
@@ -21,7 +21,10 @@ export function NoFixtureSelected({ onShowWelcome }: Props) {
       <OverlayBody>
         <TextContainer>
           <KeyShortcut keys={['⌘', 'K']} label="Search fixtures" />
-          <Subtitle>FIXTURE SELECTED</Subtitle>
+          <SubtitleContainer>
+            <div style={{ flex: 1 }} />
+            <Subtitle>ON SELECTED FIXTURE</Subtitle>
+          </SubtitleContainer>
           <KeyShortcut keys={['L']} label="Toggle fixture list" />
           <KeyShortcut keys={['P']} label="Toggle control panel" />
           <KeyShortcut keys={['F']} label="Go full screen" />
@@ -38,13 +41,20 @@ export function NoFixtureSelected({ onShowWelcome }: Props) {
   );
 }
 
-const Subtitle = styled.div`
+const SubtitleContainer = styled.div`
   margin: 40px 0 24px 0;
+  display: flex;
+  flex-direction: row;
+`;
+
+const Subtitle = styled.div`
+  flex: 1.4;
   color: ${screenGrey3};
   font-size: 14px;
   font-weight: 500;
   line-height: 14px;
-  text-align: center;
+  text-align: left;
+  text-indent: -26px;
   letter-spacing: 0.5px;
 `;
 
