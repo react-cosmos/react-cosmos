378128ae748bf469133dd6042916744cdaad9484
Ovidiu Cherecheș <hello@ovidiu.ch>
2024-02-15 22:07:56 +0200

    Update useSetValue.ts

--- packages/react-cosmos-renderer/src/fixture/useValue/useSetValue.ts
+++ packages/react-cosmos-renderer/src/fixture/useValue/useSetValue.ts
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
   ControlsFixtureState,
-  FixtureStateData,
   createValue,
   extendWithValue,
 } from 'react-cosmos-core';
@@ -18,16 +17,10 @@ export function useSetValue<T>(
         // Types of fixture state values cannot be guaranteed at read
         // time, which means that tampering with the fixture state can
         // cause runtime errors
-        // TODO: Explain the type coercion better
         function getNewState() {
-          if (typeof stateChange !== 'function') {
-            return stateChange as FixtureStateData;
-          }
+          if (typeof stateChange !== 'function') return stateChange;
 
-          const stateUpdater = stateChange as (
-            prevState: unknown
-          ) => FixtureStateData;
-
+          const stateUpdater = stateChange as (prevState: unknown) => unknown;
           return stateUpdater(
             getCurrentValueFromFixtureState(prevFs, inputName, defaultValue)
           );
