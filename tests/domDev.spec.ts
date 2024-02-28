f5132c55a2996a3d7cebb145365a6552b03467cb
Ovidiu Cherecheș <hello@ovidiu.ch>
2024-02-28 10:48:42 +0200

    Try different strategy to wait for dev renderer to build

--- tests/domDev.spec.ts
+++ tests/domDev.spec.ts
@@ -2,16 +2,7 @@ import { test } from '@playwright/test';
 import { webTests } from './helpers/webTests.js';
 
 const url = 'http://localhost:5000';
-const rendererUrl = 'http://localhost:5000/renderer.html';
-const initialLoadTimeout = 30000;
 
 test.describe('DOM dev', () => {
-  test.beforeEach(async ({ page }) => {
-    // NOTE: It seems that the webpack plugin can sometimes slow the Cosmos
-    // server while it's compiling
-    await page.waitForURL(url, { timeout: initialLoadTimeout });
-    await page.waitForURL(rendererUrl, { timeout: initialLoadTimeout });
-  });
-
   webTests(url);
 });
