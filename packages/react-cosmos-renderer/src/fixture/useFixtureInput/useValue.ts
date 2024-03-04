3d8b079a0ef1cb2c29ee045873edc4496da8fb22
Ovidiu Cherecheș <hello@ovidiu.ch>
2024-03-04 02:26:05 +0200

    Update comments

--- packages/react-cosmos-renderer/src/fixture/useFixtureInput/useValue.ts
+++ packages/react-cosmos-renderer/src/fixture/useFixtureInput/useValue.ts
@@ -4,8 +4,8 @@ type Opts<T> = {
   defaultValue: T;
 };
 
-// NOTE: This is an alias for useInput kept for backwards compatibility with
-// Cosmos versions older than 6.1
+// NOTE: This is an alias for useFixtureInput kept for backwards compatibility
+// with Cosmos versions older than 6.1
 export function useValue<T>(
   inputName: string,
   opts: Opts<T>
--- packages/react-cosmos-renderer/src/fixture/useFixtureSelect/useSelect.ts
+++ packages/react-cosmos-renderer/src/fixture/useFixtureSelect/useSelect.ts
@@ -1,6 +1,8 @@
 import { SetSelectValue, UseSelectArgs } from './shared.js';
 import { useFixtureSelect } from './useFixtureSelect.js';
 
+// NOTE: This is an alias for useFixtureSelect kept for backwards compatibility
+// with Cosmos versions older than 6.1
 export function useSelect<Option extends string>(
   selectName: string,
   args: UseSelectArgs<Option>
