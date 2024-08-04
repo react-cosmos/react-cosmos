2bf7adaaed22e181a59be9df269e49344873e061
Ovidiu Cherecheș <hello@ovidiu.ch>
2024-08-04 14:15:29 +0300

    Improve error handling in tests

--- packages/react-cosmos/src/export/__tests__/generateExport.ts
+++ packages/react-cosmos/src/export/__tests__/generateExport.ts
@@ -106,8 +106,9 @@ async function ensureFile(atPath: string) {
   try {
     await fs.mkdir(path.dirname(atPath), { recursive: true });
     await fs.writeFile(atPath, '', { flag: 'wx' });
-  } catch (err) {
+  } catch (err: any) {
     // Nothing to do if file already exists
+    if (err.code !== 'EEXIST') throw err;
   }
 }
 
