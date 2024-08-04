644dac5b3abcbe7d2e8eb7e2a22bac6b2721454f
Ovidiu Cherecheș <hello@ovidiu.ch>
2024-08-04 15:36:10 +0300

    Update resolveWebpackClientPath.ts

--- packages/react-cosmos-plugin-webpack/src/server/webpackConfig/resolveWebpackClientPath.ts
+++ packages/react-cosmos-plugin-webpack/src/server/webpackConfig/resolveWebpackClientPath.ts
@@ -1,7 +1,5 @@
-import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 export function resolveWebpackClientPath(relPath: string) {
-  const currentDir = dirname(fileURLToPath(import.meta.url));
-  return resolve(currentDir, '../../client', relPath);
+  return fileURLToPath(new URL(`../../client/${relPath}`, import.meta.url));
 }
