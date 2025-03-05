6566c3978011b3b2d579c0d900f6f95533e398d1
Ovidiu Cherecheș <hello@ovidiu.ch>
2025-03-05 21:35:10 +0200

    Update panelsLocked.ts

--- packages/react-cosmos-ui/src/plugins/Root/persistentState/panelsLocked.ts
+++ packages/react-cosmos-ui/src/plugins/Root/persistentState/panelsLocked.ts
@@ -2,7 +2,7 @@ import { StorageSpec } from '../../Storage/spec.js';
 import { RootContext } from '../shared.js';
 
 const STORAGE_KEY = 'panelsLocked';
-const DEFAULT_VALUE = false;
+const DEFAULT_VALUE = window.innerWidth >= 640;
 
 export function arePanelsLocked(context: RootContext) {
   const storage = context.getMethodsOf<StorageSpec>('storage');
