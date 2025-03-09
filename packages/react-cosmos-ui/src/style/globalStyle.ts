3fd38ae7e23a3f37362e38ae0c95f14cfcdbae66
Ovidiu Cherecheș <hello@ovidiu.ch>
2025-03-09 20:53:55 +0200

    Update globalStyle.ts

--- packages/react-cosmos-ui/src/style/globalStyle.ts
+++ packages/react-cosmos-ui/src/style/globalStyle.ts
@@ -8,7 +8,7 @@ export const GlobalStyle = createGlobalStyle`
     font-family: ${fontFamily};
     font-size: 14px;
     -webkit-font-smoothing: antialiased;
-    -webkit-text-size-adjust: 100%;
+    -webkit-text-size-adjust: none;
   }
 
   body, p, ul, ol, li, h1, h2, h3, button, input, textarea, select {
