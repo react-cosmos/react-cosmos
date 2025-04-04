f252bc1ef2d7530b47db223b5cea8b30db099062
Ovidiu Cherecheș <hello@ovidiu.ch>
2025-04-04 01:11:17 +0100

    Update WebpackRendererError.tsx

--- packages/react-cosmos-plugin-webpack/src/ui/WebpackRendererError.tsx
+++ packages/react-cosmos-plugin-webpack/src/ui/WebpackRendererError.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { createPlugin } from 'react-plugin';
 import { WebpackRendererErrorSpec } from './WebpackRendererErrorSpec.js';
 
-// Delete this UI plugin because it isn't doing anything?
-// How could a renderer error surface it this index.html is missing...?
 const { register, plug } = createPlugin<WebpackRendererErrorSpec>({
   name: 'webpackRendererError',
 });
