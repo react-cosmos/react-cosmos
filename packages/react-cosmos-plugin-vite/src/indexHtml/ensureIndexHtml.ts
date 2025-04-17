cef7cbe596eb592a241ce8de9a8787363cdbcb67
Ovidiu Cherecheș <hello@ovidiu.ch>
2025-04-18 00:05:57 +0300

    Update ensureIndexHtml.ts

--- packages/react-cosmos-plugin-vite/src/indexHtml/ensureIndexHtml.ts
+++ packages/react-cosmos-plugin-vite/src/indexHtml/ensureIndexHtml.ts
@@ -4,16 +4,10 @@ import { generateViteIndexHtml } from './generateViteIndexHtml.js';
 
 export function ensureIndexHtml(rootDir: string) {
   const htmlPath = path.resolve(rootDir, 'index.html');
-  if (!fs.existsSync(htmlPath)) {
-    console.log(
-      `[Cosmos] Vite index.html not found, creating a default one...`
-    );
-    const html = generateViteIndexHtml();
-    fs.writeFileSync(htmlPath, html);
-    return html;
-  }
+  if (fs.existsSync(htmlPath)) return fs.readFileSync(htmlPath, 'utf-8');
 
-  const html = fs.readFileSync(htmlPath, 'utf-8');
-  // TODO: Add a default script if the user doesn't have one
+  console.log(`[Cosmos] Vite index.html not found, creating a default one...`);
+  const html = generateViteIndexHtml();
+  fs.writeFileSync(htmlPath, html);
   return html;
 }
