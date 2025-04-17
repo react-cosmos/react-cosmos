e7f3a2350e3b6af6895a90986c1153c5936435e0
Ovidiu Cherecheș <hello@ovidiu.ch>
2025-04-17 23:06:11 +0300

    Fix tests on Windows

--- packages/react-cosmos-plugin-vite/src/indexHtml/ensureIndexHtml.test.ts
+++ packages/react-cosmos-plugin-vite/src/indexHtml/ensureIndexHtml.test.ts
@@ -1,4 +1,5 @@
 import fs from 'node:fs';
+import path from 'node:path';
 import { mockConsole } from 'react-cosmos/vitest.js';
 import { Mock } from 'vitest';
 import { ensureIndexHtml } from './ensureIndexHtml.js';
@@ -15,7 +16,11 @@ beforeEach(() => {
   vi.resetAllMocks();
 });
 
-const defaultHtml = `<!DOCTYPE html>
+it('creates default index.html when none exists and returns it', async () => {
+  const rootDir = path.normalize('/my/root/path');
+  const indexPath = path.join(rootDir, 'index.html');
+
+  const defaultHtml = `<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8" />
@@ -28,26 +33,34 @@ const defaultHtml = `<!DOCTYPE html>
   </body>
 </html>\n`;
 
-const customHtml = defaultHtml.replace('React Cosmos Vite Renderer', 'My App');
-
-it('creates and returns default index.html when none exists', async () => {
   (fs.existsSync as Mock).mockReturnValue(false);
 
   await mockConsole(async ({ expectLog }) => {
     expectLog('[Cosmos] Vite index.html not found, creating a default one...');
-    expect(ensureIndexHtml('/my/root/path')).toEqual(defaultHtml);
+    expect(ensureIndexHtml(rootDir)).toEqual(defaultHtml);
   });
-  expect(fs.writeFileSync).toHaveBeenCalledWith(
-    '/my/root/path/index.html',
-    defaultHtml
-  );
+  expect(fs.writeFileSync).toHaveBeenCalledWith(indexPath, defaultHtml);
 });
 
 it('returns existing index.html when none exists', async () => {
-  (fs.existsSync as Mock).mockReturnValue(
-    (path: string) => path === '/my/root/path/index.html'
-  );
+  const rootDir = path.normalize('/my/root/path');
+  const indexPath = path.join(rootDir, 'index.html');
+
+  const customHtml = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>My App</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="/src/main.tsx"></script>
+  </body>
+</html>\n`;
+
+  (fs.existsSync as Mock).mockReturnValue((p: string) => p === indexPath);
   (fs.readFileSync as Mock).mockReturnValue(customHtml);
 
-  expect(ensureIndexHtml('/my/root/path')).toEqual(customHtml);
+  expect(ensureIndexHtml(rootDir)).toEqual(customHtml);
 });
