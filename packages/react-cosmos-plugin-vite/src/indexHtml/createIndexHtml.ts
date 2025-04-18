0d6ad2972f66064fe385e4fb5eb6d9bd1aa50049
Ovidiu Cherecheș <hello@ovidiu.ch>
2025-04-18 17:46:32 +0300

    Improve Vite index.html generation

--- packages/react-cosmos-plugin-vite/src/indexHtml/createIndexHtml.ts
+++ packages/react-cosmos-plugin-vite/src/indexHtml/createIndexHtml.ts
@@ -1,11 +1,7 @@
 import fs from 'node:fs';
-import path from 'node:path';
 import { defaultMainScriptUrl } from './defaultMainScriptUrl.js';
 
-export function ensureIndexHtml(rootDir: string) {
-  const htmlPath = path.resolve(rootDir, 'index.html');
-  if (fs.existsSync(htmlPath)) return fs.readFileSync(htmlPath, 'utf-8');
-
+export function createIndexHtml(htmlPath: string) {
   console.log(`[Cosmos] Vite index.html not found, creating a default one...`);
   const html = generateViteIndexHtml();
   fs.writeFileSync(htmlPath, html);
