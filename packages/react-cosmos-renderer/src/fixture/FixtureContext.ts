0977767f582d5764fb7799982d3a7816de1ad11a
Ovidiu Cherecheș <hello@ovidiu.ch>
2025-06-01 20:32:49 +0300

    Add fixture API for changing fixture

--- packages/react-cosmos-renderer/src/fixture/FixtureContext.ts
+++ packages/react-cosmos-renderer/src/fixture/FixtureContext.ts
@@ -4,9 +4,11 @@ import { FixtureState, SetFixtureState } from 'react-cosmos-core';
 export type FixtureContextValue = {
   fixtureState: FixtureState;
   setFixtureState: SetFixtureState;
+  changeFixture: (path: string, name?: string) => void;
 };
 
 export const FixtureContext = React.createContext<FixtureContextValue>({
   fixtureState: {},
   setFixtureState: () => {},
+  changeFixture: () => {},
 });
