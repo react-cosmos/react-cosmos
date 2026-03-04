4be82eed7a6d82c2b3b7bb5c3f300b0f0e63ff9e
Ovidiu Cherecheș <hello@ovidiu.ch>
2026-03-04 21:37:29 +0200

    Fix static prerendering broken by searchParams access

--- packages/react-cosmos-next/src/nextCosmosPage.tsx
+++ packages/react-cosmos-next/src/nextCosmosPage.tsx
@@ -6,26 +6,25 @@ import { NextCosmosParams } from './nextTypes.js';
 export function nextCosmosPage(args: {
   rendererConfig: RendererConfig;
   moduleWrappers: UserModuleWrappers;
+  readSearchParams?: boolean;
 }) {
   return async function NextCosmosPage(props: {
     params: Promise<NextCosmosParams>;
     searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
   }) {
-    // Default to empty search param in static Next.js builds
-    async function getSearchParams() {
-      try {
-        return await props.searchParams;
-      } catch (err) {
-        return {};
-      }
-    }
+    // Accessing searchParams opts the route into dynamic rendering in Next.js.
+    // Only read them when explicitly opted in.
+    // https://github.com/react-cosmos/react-cosmos/issues/1750
+    const searchParams = args.readSearchParams
+      ? await props.searchParams
+      : {};
 
     return (
       <NextFixtureLoader
         rendererConfig={args.rendererConfig}
         moduleWrappers={args.moduleWrappers}
         params={await props.params}
-        searchParams={await getSearchParams()}
+        searchParams={searchParams}
       />
     );
   };
